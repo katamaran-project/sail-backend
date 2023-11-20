1cfe7a35e16e3addff658a50e2455b410a1d6c99
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-11-20 10:26:53 +0100

    updated codebase for sail 0.17

--- src/nanosail/sail_to_nanosail.ml
+++ src/nanosail/sail_to_nanosail.ml
@@ -29,7 +29,7 @@ let ty_id_of_typ_id (Id_aux (aux, _)) =
   | _ -> Id_nys
 
 let rec ty_of_typ (Typ_aux (typ, _)) =
-  let ty_of_arg (A_aux (aux, _)) = 
+  let ty_of_arg (A_aux (aux, _)) =
     match aux with
     | A_typ typ -> ty_of_typ typ
     | _ -> Ty_nys in
@@ -41,7 +41,7 @@ let rec ty_of_typ (Typ_aux (typ, _)) =
       | "atom_bool" -> Ty_id Bool
       | _           -> Ty_app (ty_id_of_typ_id id, List.map ty_of_arg args)
     )
-  | Typ_tup (h_typ :: typs) ->
+  | Typ_tuple (h_typ :: typs) ->
       let h_ty = ty_of_typ h_typ in
       let f ty1 typ2 =
         let ty2 = ty_of_typ typ2 in
@@ -67,12 +67,12 @@ let rec binds_of_pat (P_aux (aux, a)) =
       let x = string_of_id id in
       let ty = ty_of_typ (Type_check.typ_of_annot a) in
       [(x, ty)]
-  | P_tup pats ->
+  | P_tuple pats ->
       List.concat (List.map binds_of_pat pats)
   | _ ->
       [("PATTERN_NOT_YET_SUPPORTED", Ty_nys)]
 
-let binds_of_pexp (Pat_aux (aux, _)) = 
+let binds_of_pexp (Pat_aux (aux, _)) =
   match aux with
   | Pat_exp (pat, _) -> binds_of_pat pat
   | Pat_when _ ->
@@ -109,7 +109,7 @@ let rec statement_of_aexp (AE_aux (aux, _, _)) =
   match aux with
   | AE_val aval ->
       Stm_exp (expression_of_aval aval)
-  | AE_app (id, avals, _) -> 
+  | AE_app (id, avals, _) ->
       let x = string_of_id id in (
         match avals with
         | [aval1; aval2] when x = "sail_cons" ->
@@ -129,7 +129,7 @@ let rec statement_of_aexp (AE_aux (aux, _, _)) =
       let s1 = statement_of_aexp aexp1 in
       let s2 = statement_of_aexp aexp2 in
       Stm_if (s, s1, s2)
-  | AE_case (aval, cases, _) ->
+  | AE_match (aval, cases, _) ->
       statement_of_match aval cases
   | _ -> Stm_nys
 
@@ -160,11 +160,11 @@ and statement_of_match aval = function
         xt       = string_of_id id_t;
         alt_cons = statement_of_aexp aexp1;
       }
-  | [ (AP_aux (AP_tup [
+  | [ (AP_aux (AP_tuple [
         AP_aux ((AP_id (id_l, _)), _, _);
         AP_aux ((AP_id (id_r, _)), _, _);
       ], _, _),_ , aexp)
-    ] -> 
+    ] ->
       Stm_match_prod {
         s   = Stm_exp (expression_of_aval aval);
         xl  = string_of_id id_l;
@@ -181,7 +181,7 @@ let body_of_pexp (Pat_aux (aux, _)) =
 
 (******************************************************************************)
 
-let ir_funcl (FCL_aux (FCL_Funcl (id, pexp), _)) = {
+let ir_funcl (FCL_aux (FCL_funcl (id, pexp), _)) = {
   funName = string_of_id(id);
   funType = {
     arg_types = binds_of_pexp pexp;
@@ -195,11 +195,14 @@ let ir_fundef (FD_aux ((FD_function (_, _, funcls)), _)) =
   | [funcl] -> some (ir_funcl funcl)
   | _       -> none
 
-let ir_def = function
-  | DEF_fundef fd -> join (some (ir_fundef fd))
-  | _             -> none 
+let ir_def def =
+  match def with
+  | DEF_aux (DEF_fundef fd, _) -> join (some (ir_fundef fd))
+  | _                          -> none
 
-let sail_to_nanosail {defs; _} name =  {
-  program_name = name;
-  funDefList   = List.filter_map ir_def defs
-}
+let sail_to_nanosail ast name =
+  let defs = ast.defs in
+  {
+    program_name = name;
+    funDefList   = List.filter_map ir_def defs
+  }
