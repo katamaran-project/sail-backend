98bcf96aaf9b2526d4faf95abc2068aa2e321219
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-11-22 23:50:20 +0100

    updated tests

--- src/nanosail_tests/nanosail_tests.ml
+++ src/nanosail_tests/nanosail_tests.ml
@@ -27,19 +27,25 @@ let numeric_expression_test_suite =
     let test_name =
       Printf.sprintf "Expected %s" expected
     in
-    let actual = string_of_numeric_expression input
-    in
-    test_name >:: fun _ -> assert_equal expected actual
+    test_name >:: fun _ -> (
+        let actual = string_of_numeric_expression input
+        in
+        let message = Printf.sprintf "Expected %s, actual %s" expected actual
+        in
+        assert_equal ~msg:message expected actual
+      )
   in
   let const c = addloc (S.Ast.Nexp_constant (Z.of_int c))
   and add x y = addloc (S.Ast.Nexp_sum (x, y))
-  and _sub x y = addloc (S.Ast.Nexp_minus (x, y))
-  and _mul x y = addloc (S.Ast.Nexp_times (x, y))
+  and sub x y = addloc (S.Ast.Nexp_minus (x, y))
+  and mul x y = addloc (S.Ast.Nexp_times (x, y))
   in
   let inputs = [
     (const 5, "5");
     (const 10, "10");
     (add (const 1) (const 2), "1 + 2");
+    (sub (const 3) (const 1), "3 - 1");
+    (mul (const 2) (const 4), "2 * 4");
   ]
   in
   "string_of_numeric_expression tests" >::: List.map (fun (input, expected) -> test_string_of_numeric_expression input expected) inputs
