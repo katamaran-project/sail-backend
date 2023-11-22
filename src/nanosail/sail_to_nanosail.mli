274b3ed674e7bf237f729e7e71c798af091f82df
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-11-22 22:44:34 +0100

    removed nanosail mli files

--- src/nanosail/sail_to_nanosail.mli
+++
@@ -1,9 +0,0 @@
-(** Sail to NanoSail translation. *)
-
-(** Takes a sail typed ast and a name and returns the corresponding nanosail
-    ast. *)
-val sail_to_nanosail :
-  Libsail.Type_check.tannot Libsail.Ast_defs.ast ->
-  string -> Ast.ir_t
-
-
