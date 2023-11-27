619f1f9257bd12f5625937b3cf15007080b464a5
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-11-27 22:05:43 +0100

    REG_finite

--- src/nanosail/registers.ml
+++ src/nanosail/registers.ml
@@ -2,6 +2,7 @@ open PPrint
 open Ast
 
 module PP = PPrint
+module PU = Pputil
 module Coq = Coq_generation
 
 module S = struct
@@ -38,7 +39,7 @@ let instance_reg_eq_dec register_names =
   let cases =
     let cs =
       List.map (fun register_name ->
-          let rn_str = string register_name in
+          let rn_str = register_name in
           ((rn_str, rn_str), string "left eq_refl"))
         register_names
     and wildcard_case = ((string "_", string "_"), string "right _")
@@ -51,17 +52,44 @@ let instance_reg_eq_dec register_names =
   separate hardline [
       utf8string "#[export,refine] Instance 𝑹𝑬𝑮_eq_dec : EqDec (sigT Reg) :=";
       utf8string "  fun '(existT σ x) '(existT τ y) =>";
-      Pputil.indent' (Coq.match_pair (string "x", string "y") cases) ^^ Coq.eol;
+      PU.indent' (Coq.match_pair (string "x", string "y") cases) ^^ Coq.eol;
       string "Proof. all: transparent_abstract (intros H; depelim H). Defined."
     ]
 
+let reg_finite register_names =
+  let enum_values =
+    let enum_value_of_register_name register_name =
+      separate space [
+        string "existT";
+        underscore;
+        register_name
+      ]
+    in
+    Coq.list (List.map enum_value_of_register_name register_names)
+  in
+  separate hardline (
+    [
+      utf8string "Program Instance 𝑹𝑬𝑮_finite : Finite (sigT Reg) :=";
+      PU.indent (
+        Coq.record_value [
+          (string "enum", enum_values)
+        ]
+      )
+    ]
+  )
+
+
 let pp_register_module (register_definitions : (sail_definition * register_definition) list) : document =
+  let register_names =
+    List.map (fun (_, def) -> string def.identifier) register_definitions
+  in
   let section_contents =
     separate (twice hardline) [
         reg_inductive_type (List.map snd register_definitions);
         no_confusion_for_reg ();
         reg_definition ();
-        instance_reg_eq_dec (List.map (fun (_, def) -> def.identifier) register_definitions)
+        instance_reg_eq_dec register_names;
+        reg_finite register_names
       ]
   in
   Coq.section "RegDeclKit" section_contents
