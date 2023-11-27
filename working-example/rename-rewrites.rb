f9a08dbbe02faf5599090505b019eaa63feb6d5e
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-11-27 09:59:35 +0100

    updated rename-rewrites script

--- working-example/rename-rewrites.rb
+++ working-example/rename-rewrites.rb
@@ -1,7 +1,7 @@
 Dir['*.sail'].each do |filename|
   if /^intermediate_rewrite_(\d+)_(\w+)\.sail$/ =~ filename
     index, description = $1, $2
-    new_filename = "#{index.rjust(2, '0')}-#{description}.sail"
+    new_filename = "rewrite-#{index.rjust(2, '0')}-#{description}.sail"
     File.rename filename, new_filename
   end
 end
