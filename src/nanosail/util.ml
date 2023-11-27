11d575fa4de3f16b958ccbec83fb76e3dfaae53b
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-11-27 11:44:53 +0100

    count_chars

--- src/nanosail/util.ml
+++ src/nanosail/util.ml
@@ -1,3 +1,11 @@
 let uncurry f (x, y) = f x y
 
 let compose_functions f g x = f (g x)
+
+let count_chars string char =
+  let count acc c =
+    if c == char
+    then acc + 1
+    else acc
+  in
+  String.fold_left count 0 string
