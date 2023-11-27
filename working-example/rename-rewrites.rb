659414257d94df00a0cc0487bc53568099add593
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-11-27 09:57:12 +0100

    rename-rewrites script

---
+++ working-example/rename-rewrites.rb
@@ -0,0 +1,7 @@
+Dir['*.sail'].each do |filename|
+  if /^intermediate_rewrite_(\d+)_(\w+)\.sail$/ =~ filename
+    index, description = $1, $2
+    new_filename = "#{index.rjust(2, '0')}-#{description}.sail"
+    File.rename filename, new_filename
+  end
+end
