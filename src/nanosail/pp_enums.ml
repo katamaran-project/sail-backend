df6372bb3a05355530f14165c021a5edf3507cc0
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-11-29 14:32:09 +0100

    xxxConstructor inductive type

--- src/nanosail/pp_enums.ml
+++ src/nanosail/pp_enums.ml
@@ -14,7 +14,7 @@ module PP = struct
 end
 
 
-let pp_enums (enum_definitions : (sail_definition * enum_definition) list) =
+let generate_inductive_type (enum_definitions : (sail_definition * enum_definition) list) =
   let pp_enum sail_definition enum_definition =
     let coq_translation =
       let identifier = string enum_definition.enum_identifier
@@ -31,3 +31,24 @@ let pp_enums (enum_definitions : (sail_definition * enum_definition) list) =
     PP.Coq.annotate_with_original_definition sail_definition coq_translation
   in
   List.map (uncurry pp_enum) enum_definitions
+
+
+let generate_constructors_inductive_type (enum_definitions : (sail_definition * enum_definition) list) =
+  let pp _ enum_definition =
+    let identifier = string (enum_definition.enum_identifier ^ "Constructor")
+    and typ = string "Set"
+    in
+    PP.Coq.build_inductive_type identifier typ (fun add_constructor ->
+        List.iter
+          (fun (case : string) ->
+            add_constructor (string ("K" ^ case)))
+          enum_definition.enum_cases
+      )
+  in
+  List.map (uncurry pp) enum_definitions
+
+let pp_enums (enum_definitions : (sail_definition * enum_definition) list) =
+  Util.build_list (fun { addall; _ } ->
+      addall (generate_inductive_type enum_definitions);
+      addall (generate_constructors_inductive_type enum_definitions);
+    )
