38cf431820ddf5e62d3ef23a951db266ad4f554c
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-11-29 14:58:24 +0100

    Enum inductive type

--- src/nanosail/pp_enums.ml
+++ src/nanosail/pp_enums.ml
@@ -47,8 +47,30 @@ let generate_constructors_inductive_type (enum_definitions : (sail_definition * 
   in
   List.map (uncurry pp) enum_definitions
 
+
+let generate_enum_of_enums (enum_definitions : (sail_definition * enum_definition) list) =
+  let enum_identifiers =
+    let extract_identifier (_, enum_definition) = enum_definition.enum_identifier
+    in
+    List.map extract_identifier enum_definitions
+  and identifier = string "Enums"
+  and typ = string "Set"
+  in
+  PP.Coq.build_inductive_type
+    identifier
+    typ
+    (fun add_constructor ->
+      List.iter
+        (fun enum_identifier ->
+          add_constructor (string enum_identifier)
+        )
+        enum_identifiers
+    )
+
 let pp_enums (enum_definitions : (sail_definition * enum_definition) list) =
-  Util.build_list (fun { addall; _ } ->
+  Util.build_list (fun { add; addall } ->
       addall (generate_inductive_type enum_definitions);
       addall (generate_constructors_inductive_type enum_definitions);
+      if not (List.is_empty enum_definitions)
+      then add (generate_enum_of_enums enum_definitions);
     )
