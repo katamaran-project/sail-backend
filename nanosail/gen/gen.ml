eac426de296b4606f3339fe20c902c70e0d8a5ff
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-12-04 13:44:23 +0100

    gen.ml

--- nanosail/gen/gen.ml
+++ nanosail/gen/gen.ml
@@ -1,7 +1,7 @@
 module Coq = Coq
-(* module Enums = Enums *)
-(* module FunDeclKit = Fundeclkit *)
+module Enums = Enums
+module FunDeclKit = Fundeclkit
 module Katamaran = Katamaran
-(* module Registers = Registers *)
+module Registers = Registers
 module Sail = Sail
 
