54ec2b350c4446d9f841b07ca74a1809df514438
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-12-04 14:14:14 +0100

    cleanup

--- nanosail/gen/enums.ml
+++ nanosail/gen/enums.ml
@@ -3,24 +3,13 @@ open Ast
 open Auxlib
 
 
-module PP = struct
-  include PPrint
-  
-  module Coq = Coq
-
-  module Katamaran = struct
-    module Registers = Registers
-  end
-end
-
-
 let generate_inductive_type (enum_definitions : (sail_definition * enum_definition) list) =
   let pp_enum sail_definition enum_definition =
     let coq_translation =
       let identifier = string enum_definition.identifier
       and typ = string "Set"
       in
-      PP.Coq.build_inductive_type identifier typ (fun add_constructor ->
+      Coq.build_inductive_type identifier typ (fun add_constructor ->
           List.iter
             (fun (case : string) ->
               add_constructor (string case)
@@ -28,7 +17,7 @@ let generate_inductive_type (enum_definitions : (sail_definition * enum_definiti
             enum_definition.cases
         )
     in
-    PP.Coq.annotate_with_original_definition sail_definition coq_translation
+    Coq.annotate_with_original_definition sail_definition coq_translation
   in
   List.map (uncurry pp_enum) enum_definitions
 
@@ -38,7 +27,7 @@ let generate_constructors_inductive_type (enum_definitions : (sail_definition * 
     let identifier = string (enum_definition.identifier ^ "Constructor")
     and typ = string "Set"
     in
-    PP.Coq.build_inductive_type identifier typ (fun add_constructor ->
+    Coq.build_inductive_type identifier typ (fun add_constructor ->
         List.iter
           (fun (case : string) ->
             add_constructor (string ("K" ^ case)))
@@ -57,7 +46,7 @@ let generate_enum_of_enums (enum_definitions : (sail_definition * enum_definitio
   and constructor_of_enum (enum_definition : enum_definition) =
     string ("E" ^ String.lowercase_ascii enum_definition.identifier)
   in
-  PP.Coq.build_inductive_type
+  Coq.build_inductive_type
     identifier
     typ
     (fun add_constructor ->
@@ -89,7 +78,7 @@ let generate_no_confusions (enum_definitions : (sail_definition * enum_definitio
     in
     set_transparent_obligations ^^ twice hardline ^^ derivations
   in
-  PP.Coq.section "TransparentObligations" contents
+  Coq.section "TransparentObligations" contents
 
 
 let generate_eqdecs (enum_definitions : (sail_definition * enum_definition) list) =
