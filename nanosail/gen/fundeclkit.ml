38ed9caaa464ddb4ae551e66938496c7977087d2
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-12-06 14:27:59 +0100

    fundeclkit monadic update

--- nanosail/gen/fundeclkit.ml
+++ nanosail/gen/fundeclkit.ml
@@ -1,49 +1,57 @@
 open PPrint
 open Ast
 open Util
-
+open Monad
 
 
 let generate function_definitions =
   let pp_function_declaration function_definition =
-    let name = string function_definition.funName
-    and function_type =
-      let parameter_types = Coq.list (List.map Sail.pp_bind function_definition.funType.arg_types)
-      and return_type = Sail.pp_ty function_definition.funType.ret_type
+    let name = string function_definition.funName in
+    let* function_type =
+      let* parameter_types =
+        let* ps = seqmap (List.map Sail.pp_bind function_definition.funType.arg_types)
+        in
+        generate (Coq.list ps)
       in
-      concat [
-        string "Fun";
-        space;
-        align (
-          group (
-            concat [
-              parameter_types;
-              break 1;
-              return_type
+      let* return_type = Sail.pp_ty function_definition.funType.ret_type
+      in
+      generate (
+          concat [
+              string "Fun";
+              space;
+              align (
+                  group (
+                      concat [
+                          parameter_types;
+                          break 1;
+                          return_type
+                        ]
+                    )
+                )
             ]
-          )
         )
-      ]
     in
-    (name, function_type)
+    generate (name, function_type)
   in
   let inductive_type_declaration =
     let name = string "Fun"
     and typ = string "PCtx -> Ty -> Set"
     in
-    Coq.build_inductive_type name typ (fun add_constructor ->
-        List.iter
-          (fun function_definition ->
-            let name, typ = pp_function_declaration function_definition
-            in
-            add_constructor ~typ:typ name
-          )
-          function_definitions
-      )
+    let* constructors =
+      seqmap (List.map pp_function_declaration function_definitions)
+    in
+    generate (
+        Coq.build_inductive_type name typ (fun add_constructor ->
+            List.iter
+              (fun (name, function_type) ->
+                add_constructor ~typ:function_type name
+              )
+              constructors
+          ))
   in
   let contents =
     separate small_step [
-        inductive_type_declaration;
+        Coq.annotate inductive_type_declaration;
         separate_map hardline utf8string [
             "Definition 𝑭  : PCtx -> Ty -> Set := Fun.";
             "Definition 𝑭𝑿 : PCtx -> Ty -> Set := fun _ _ => Empty_set.";
