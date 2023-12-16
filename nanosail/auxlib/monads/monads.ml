b2b0ff3f1914dfc5951c7a2bdaeee74749ef72c4
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-12-16 23:09:10 +0100

    Monads.Util

--- nanosail/auxlib/monads/monads.ml
+++ nanosail/auxlib/monads/monads.ml
@@ -1,3 +1,5 @@
 module State     = State
 module Writer    = Writer
 module Notations = Notations
+module Monoid    = Monoid
+module Util      = Util
---
+++ nanosail/auxlib/monads/util.ml
@@ -0,0 +1,25 @@
+module Make (M : Sig.Monad) = struct
+  let rec collect n f =
+    if n <= 0
+    then M.return []
+    else
+      M.bind f (fun r -> M.bind (collect (n-1) f) (fun rs -> M.return @@ r::rs))
+
+  let rec repeat n f =
+    if n <= 0
+    then M.return ()
+    else M.bind f (fun _ -> repeat (n - 1) f)
+
+  let map f xs =
+    let rec aux xs acc =
+      match xs with
+      | []    -> M.return @@ List.rev acc
+      | x::xs -> M.bind (f x) (fun x' -> aux xs (x' :: acc))
+    in
+    aux xs []
+
+  let rec iter f xs =
+    match xs with
+    | []    -> M.return ()
+    | x::xs -> M.bind (f x) (fun _ -> iter f xs)
+end
