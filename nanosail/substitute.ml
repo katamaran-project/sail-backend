6ba626304b1bb0025522ed7ea3fe590a8274460c
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-12-16 10:14:56 +0100

    code layout

--- nanosail/substitute.ml
+++ nanosail/substitute.ml
@@ -8,13 +8,13 @@ let sanitizing_substitution (identifier : Ast.identifier) : Ast.identifier =
 let substitute_identifiers_in_numeric_expression (subst : identifier -> identifier) =
   let rec aux (numeric_expression : numeric_expression) =
     match numeric_expression with
-    | NE_constant _ -> numeric_expression
-    | NE_add (left, right) -> NE_add (aux left, aux right)
-    | NE_minus (left, right) -> NE_minus (aux left, aux right)
-    | NE_times (left, right) -> NE_times (aux left, aux right)
-    | NE_neg operand -> NE_neg (aux operand)
-    | NE_id identifier -> NE_id (subst identifier)
-    | NE_var identifier -> NE_var (subst identifier)
+    | NE_constant _           -> numeric_expression
+    | NE_add (left, right)    -> NE_add (aux left, aux right)
+    | NE_minus (left, right)  -> NE_minus (aux left, aux right)
+    | NE_times (left, right)  -> NE_times (aux left, aux right)
+    | NE_neg operand          -> NE_neg (aux operand)
+    | NE_id identifier        -> NE_id (subst identifier)
+    | NE_var identifier       -> NE_var (subst identifier)
   in
   aux
 
