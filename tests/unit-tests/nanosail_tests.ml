a5248b3a42c4793032d6b00e4bcb4c39be3c3e6a
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-12-16 23:10:11 +0100

    nanosail tests includes monad tests

--- tests/unit-tests/nanosail_tests.ml
+++ tests/unit-tests/nanosail_tests.ml
@@ -5,7 +5,8 @@ let tests =
   "all tests" >::: [
     Conversion_tests.test_suite;
     Substitution_tests.test_suite;
-    Monad_tests.test_suite;
+    State_monad_tests.test_suite;
+    Writer_monad_tests.test_suite;
   ]
 
 
