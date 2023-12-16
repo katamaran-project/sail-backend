d8f10c85e78bc9eb23522a3346014cf1934244a2
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-12-16 10:14:27 +0100

    removed redundant code

--- nanosail/substitute.ml
+++ nanosail/substitute.ml
@@ -4,24 +4,6 @@ open Ast
 let sanitizing_substitution (identifier : Ast.identifier) : Ast.identifier =
   Auxlib.drop_chars_while identifier (fun c -> c = '\'')
 
-(* (\* *)
-(*   Given nanosail identifiers, creates a substitution that converts *)
-(*   each of these into corresponding valid Coq identifiers. *)
-(* *\) *)
-(* let create_sanitizing_substitution (identifiers : Ast.identifier list) : Ast.identifier -> Ast.identifier = *)
-(*   let identity id   = id *)
-(*   and wrap f id id' = fun i -> if i = id then id' else f i *)
-(*   in *)
-(*   List.fold_left *)
-(*     (fun acc identifier -> *)
-(*       match sanitize_identifier identifier with *)
-(*       | Some identifier' -> wrap acc identifier identifier' *)
-(*       | None             -> acc *)
-(*     ) *)
-(*     identity *)
-(*     identifiers *)
-
-
 
 let substitute_identifiers_in_numeric_expression (subst : identifier -> identifier) =
   let rec aux (numeric_expression : numeric_expression) =
@@ -35,3 +17,7 @@ let substitute_identifiers_in_numeric_expression (subst : identifier -> identifi
     | NE_var identifier -> NE_var (subst identifier)
   in
   aux
+
+
+let sanitize_numeric_expression =
+  substitute_identifiers_in_numeric_expression sanitizing_substitution
