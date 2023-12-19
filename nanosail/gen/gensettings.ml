a14df91d75dce985b9c586e609388f6f8036cb18
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-12-19 13:03:33 +0100

    code layout

--- nanosail/gen/gensettings.ml
+++ nanosail/gen/gensettings.ml
@@ -1,5 +1,3 @@
-let opt_list_notations = ref false
-
+let opt_list_notations       = ref false
 let opt_include_untranslated = ref false
-
-let opt_include_ignored = ref false
+let opt_include_ignored      = ref false
