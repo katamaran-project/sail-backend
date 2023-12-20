cee12fd83dd8ae1d0ca28a8d24a64819a490dd31
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-12-20 12:21:37 +0100

    improved variants

--- nanosail/gen/variants.ml
+++ nanosail/gen/variants.ml
@@ -4,6 +4,16 @@ open Monad
 
 
 let generate_inductive_type sail_definition ({ identifier; type_quantifier; constructors } : variant_definition) =
+  let identifier' =
+    Sail.pp_identifier identifier
+  in
+  let pp_constructor_type (nanotype : nanotype) =
+    let* ts = map Sail.pp_nanotype @@ Ast.tuple_to_list nanotype
+    in
+    let ts = ts @ [ identifier' ]
+    in
+    generate @@ separate (string " -> ") ts
+  in
   let inductive_type =
     let* type_quantifier' =
       map (fun (id, kind) ->
@@ -13,13 +23,13 @@ let generate_inductive_type sail_definition ({ identifier; type_quantifier; cons
         ) type_quantifier
     in
     Coq.mbuild_inductive_type
-      (Sail.pp_identifier identifier)
+      identifier'
       ~parameters: type_quantifier'
       (string "Set")
       (fun add_constructor ->
          iter
            (fun (constructor, typ) ->
-              let* typ' = Sail.pp_nanotype typ
+              let* typ' = pp_constructor_type typ
               in
               add_constructor ~typ:typ' (string constructor))
            constructors
