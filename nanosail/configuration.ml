e97c1fe83fafcfc89a3fd4bab6f39c6268cadfd1
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-02 14:08:39 +0100

    added comments

--- nanosail/configuration.ml
+++ nanosail/configuration.ml
@@ -2,7 +2,14 @@ include Auxlib.Settings
 
 type 'a setting = 'a t
 
+(* Use list notations *)
 let use_list_notations               = create false
+
+(* Output definitions for which no translation is available yet *)
 let include_untranslated_definitions = create false
+
+(* Annotate all Microsail definitions with their corresponding Sail definition *)
 let include_original_code            = create false
+
+(* Output ignored definitions *)
 let include_ignored_definitions      = create false
