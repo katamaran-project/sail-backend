321b7387fbcd01856b131a6e09d78decd09e1ac4
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-02 14:48:39 +0100

    Configuation.ignore_pragmas

--- nanosail/configuration.ml
+++ nanosail/configuration.ml
@@ -1,8 +1,11 @@
+open Auxlib
 include Auxlib.Settings
 
 type 'a setting = 'a t
 
-type sail_definition_predicate = Ast.sail_definition -> bool
+type sail_definition = Ast.sail_definition
+
+type sail_definition_predicate = sail_definition -> bool
 
 (* Use list notations *)
 let use_list_notations               = create false
@@ -18,3 +21,12 @@ let include_ignored_definitions      = create false
 
 (* Predicate that determines which Sail definitions to ignore *)
 let ignore_definition                = create (fun (_ : Ast.sail_definition) -> false)
+
+
+let ignore_pragma pragma (Libsail.Ast.DEF_aux (definition, _annotation)) =
+  match definition with
+  | Libsail.Ast.DEF_pragma (p, _argument, _location) -> p = pragma
+  | _                                                -> false
+
+let ignore_pragmas pragmas =
+  List.fold_left (fun acc p -> acc |.| ignore_pragma p) (fun _ -> false) pragmas
