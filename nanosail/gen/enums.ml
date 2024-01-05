7e46108d8926f097aa762cce3e55c347738f7621
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-05 12:53:42 +0100

    base update

--- nanosail/gen/enums.ml
+++ nanosail/gen/enums.ml
@@ -1,3 +1,4 @@
+open Base
 open PPrint
 open Ast
 
@@ -8,7 +9,7 @@ let generate_inductive_type (sail_definition : sail_definition) (enum_definition
     and typ = Sail.pp_identifier "Set"
     in
     Coq.build_inductive_type identifier typ (fun add_constructor ->
-        List.iter add_constructor @@ List.map string enum_definition.cases
+        List.iter ~f:add_constructor @@ List.map ~f:string enum_definition.cases
       )
   in
   Coq.annotate_with_original_definition sail_definition coq_translation
@@ -19,33 +20,33 @@ let generate_constructors_inductive_type (_sail_definition : sail_definition) (e
   in
   Coq.build_inductive_type identifier typ (fun add_constructor ->
       List.iter
-        (fun (case : string) ->
+        ~f:(fun (case : string) ->
           add_constructor @@ string @@ "K" ^ case)
         enum_definition.cases
     )
 
 let generate_enum_of_enums (enum_definitions : (sail_definition * enum_definition) list) =
   let enum_definitions =
-    List.map snd enum_definitions
+    List.map ~f:snd enum_definitions
   in
   let identifier = string "Enums"
   and typ = string "Set"
   and constructor_of_enum (enum_definition : enum_definition) =
-    string @@ "E" ^ String.lowercase_ascii enum_definition.identifier
+    string @@ "E" ^ String.lowercase enum_definition.identifier
   in
   Coq.build_inductive_type
     identifier
     typ
     (fun add_constructor ->
       List.iter
-        (fun enum_identifier ->
+        ~f:(fun enum_identifier ->
           add_constructor (constructor_of_enum enum_identifier)
         )
         enum_definitions
     )
 
 let generate_no_confusions (enum_definitions : (sail_definition * enum_definition) list) =
-  let enum_definitions = List.map snd enum_definitions
+  let enum_definitions = List.map ~f:snd enum_definitions
   in
   let contents =
     let set_transparent_obligations =
@@ -56,7 +57,7 @@ let generate_no_confusions (enum_definitions : (sail_definition * enum_definitio
         string @@ Printf.sprintf "Derive NoConfusion for %s." enum_definition.identifier
       in
       let lines =
-        List.map generate_derivation enum_definitions
+        List.map ~f:generate_derivation enum_definitions
       in
       separate hardline lines
     in
@@ -65,12 +66,12 @@ let generate_no_confusions (enum_definitions : (sail_definition * enum_definitio
   Coq.section "TransparentObligations" contents
 
 let generate_eqdecs (enum_definitions : (sail_definition * enum_definition) list) =
-  let enum_definitions = List.map snd enum_definitions
+  let enum_definitions = List.map ~f:snd enum_definitions
   in
   let generate_eqdec (enum_definition : enum_definition) =
     string @@ Printf.sprintf "Derive EqDec for %s." enum_definition.identifier
   in
   let lines =
-    List.map generate_eqdec enum_definitions
+    List.map ~f:generate_eqdec enum_definitions
   in
   separate hardline lines
