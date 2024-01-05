9c53d175a5fe7956513b3a5e3ebbf1cf7ba7a113
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-05 15:07:35 +0100

    renamed value to t

--- slang/values.ml
+++ slang/values.ml
@@ -1,7 +1,15 @@
-type value =
-  | Cons    of value * value
-  | Integer of int
-  | Symbol  of string
-  | String  of string
-  | Bool    of bool
+type t =
+  | Cons           of t * t
+  | Integer        of int
+  | Symbol         of string
+  | String         of string
+  | Bool           of bool
   | Nil
+  | NativeFunction of (t list -> t)
+
+
+let rec cons_to_list value =
+  match value with
+  | Nil               -> []
+  | Cons (head, tail) -> head :: cons_to_list tail
+  | _                 -> failwith "invalid list"
