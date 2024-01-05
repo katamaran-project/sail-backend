2fc7fcd29bdef8b5f583b099f19f9bf591c23f65
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-05 12:54:28 +0100

    base update

--- nanosail/gen/ignored.ml
+++ nanosail/gen/ignored.ml
@@ -1,4 +1,6 @@
+open Base
+
 let generate (ignored_definitions : Ast.sail_definition list) =
-  let ignored_definitions' = List.map Sail.pp_sail_definition ignored_definitions
+  let ignored_definitions' = List.map ~f:Sail.pp_sail_definition ignored_definitions
   in
   PPrint.(Coq.comment @@ separate (twice hardline) ignored_definitions')
