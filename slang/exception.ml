639f8deb15f0b74946fc0cdd2f850ac0e842314c
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-06 21:53:01 +0100

    added define to slang

---
+++ slang/exception.ml
@@ -0,0 +1,2 @@
+exception SlangError of string
+
--- slang/prelude/functions.ml
+++ slang/prelude/functions.ml
@@ -1,5 +1,6 @@
 open Base
 open Util
+open Exception
 open Evaluation_context
 open Monads.Notations.Star(Evaluation_context)
 
@@ -12,19 +13,40 @@ open T.Notations
 
 let lambda args =
   match args with
-  | []             -> raise T.TypeError
-  | [_]            -> raise T.TypeError
+  | []             -> raise @@ SlangError "ill-formed lambda"
+  | [_]            -> raise @@ SlangError "ill-formed lambda"
   | params :: body -> begin
-      let*  env = current_environment
-      in
+      let*  env    = current_environment in
       let=! params = T.list T.symbol params
-      and=! body = T.map T.value body
+      and=! body   = T.map T.value body
       in    
       return @@ V.Closure (env, params, body)
     end
 
 
+let define args =
+  match args with
+  | []             -> raise @@ SlangError "ill-formed define"
+  | [_]            -> raise @@ SlangError "ill-formed define"
+  | params :: body -> begin
+      let*  env    = current_environment in
+      let=! params = T.list T.symbol params
+      and=! body   = T.map T.value body
+      in
+      match params with
+      | []                          -> raise @@ SlangError "ill-formed define"
+      | function_name :: parameters -> begin
+          let closure = V.Closure (env, parameters, body)
+          in
+          let* () = add_binding function_name closure
+          in
+          return V.Nil
+        end
+    end
+
+
 let library env =
   extend_environment env (fun { native_function; _ } ->
-      native_function "lambda" lambda
+      native_function "lambda" lambda;
+      native_function "define" define;
     )
