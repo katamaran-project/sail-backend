42eb0e3daa0c724c20f4033cf9ff00d983b47888
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-07 11:24:56 +0100

    code layout

--- nanosail/configuration.ml
+++ nanosail/configuration.ml
@@ -8,11 +8,11 @@ type sail_definition = Ast.sail_definition
 
 type sail_definition_predicate = sail_definition -> bool
 
-let use_list_notations               = create false               (* Use list notations *)
-let include_untranslated_definitions = create false               (* Output definitions for which no translation is available yet *)
+let use_list_notations               = create false               (* Use list notations                                                          *)
+let include_untranslated_definitions = create false               (* Output definitions for which no translation is available yet                *)
 let include_original_code            = create false               (* Annotate all Microsail definitions with their corresponding Sail definition *)
-let include_ignored_definitions      = create false               (* Output ignored definitions *)
-let ignore_definition                = create (Fn.const false)    (* Predicate that determines which Sail definitions to ignore *)
+let include_ignored_definitions      = create false               (* Output ignored definitions                                                  *)
+let ignore_definition                = create (Fn.const false)    (* Predicate that determines which Sail definitions to ignore                  *)
 
 
 let ignore_pragma pragma (Libsail.Ast.DEF_aux (definition, _annotation)) =
