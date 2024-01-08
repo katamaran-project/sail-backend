23ed153ed3508b7a0a6c393460e2fcea13abbf29
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-08 19:17:10 +0100

    slang define variable

--- slang/prelude/functions.ml
+++ slang/prelude/functions.ml
@@ -24,22 +24,43 @@ let lambda args =
 
 
 let define args =
+  let define_function form body =
+    match form with
+    | []                          -> raise @@ SlangError "ill-formed define"
+    | function_name :: parameters -> begin
+        let* env = current_environment
+        in
+        let closure = V.Closure (env, parameters, body)
+        in
+        let* () = add_binding function_name closure
+        in
+        return V.Nil
+      end
+
+  and define_variable identifier body =
+    match body with
+    | [ expression ] -> begin
+        let* value = Evaluation.evaluate expression
+        in
+        let* () = add_binding identifier value
+        in
+        return V.Nil
+      end
+    | _     -> raise @@ SlangError "ill-formed define"
+
+  in
   match args with
   | []             -> raise @@ SlangError "ill-formed define"
   | [_]            -> raise @@ SlangError "ill-formed define"
-  | params :: body -> begin
-      let*  env    = current_environment in
-      let=! params = T.list T.symbol params
-      and=! body   = T.map T.value body
+  | form :: body -> begin
+      let=! body = T.map T.value body
       in
-      match params with
-      | []                          -> raise @@ SlangError "ill-formed define"
-      | function_name :: parameters -> begin
-          let closure = V.Closure (env, parameters, body)
-          in
-          let* () = add_binding function_name closure
-          in
-          return V.Nil
+      match T.list T.symbol form with
+      | Some form_symbols -> define_function form_symbols body
+      | None              -> begin
+          match T.symbol form with
+          | Some identifier -> define_variable identifier body
+          | None            -> raise @@ SlangError "ill-formed define"
         end
     end
 
