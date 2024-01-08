32324c5f29bca6d23bc19eea116062cfcd7aabb7
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-08 22:15:04 +0100

    ConfigLib

--- nanosail/configuration.ml
+++ nanosail/configuration.ml
@@ -1,15 +1,15 @@
 open Base
 
-include Configlib.Exported
+include ConfigLib.Exported
 
 
 type sail_definition = Ast.sail_definition
 
-let use_list_notations               = Configlib.bool    "use-list-notations"               (* Use list notations                                                          *)
-let include_untranslated_definitions = Configlib.bool    "include-untranslated-definitions" (* Output definitions for which no translation is available yet                *)
-let include_original_code            = Configlib.bool    "include-original-code"            (* Annotate all Microsail definitions with their corresponding Sail definition *)
-let include_ignored_definitions      = Configlib.bool    "include-ignored-definitions"      (* Output ignored definitions                                                  *)
-let ignore_pragmas                   = Configlib.strings "ignore-pragmas"                   (* Pragmas to be ignored                                                       *)
+let use_list_notations               = ConfigLib.bool    "use-list-notations"               (* Use list notations                                                          *)
+let include_untranslated_definitions = ConfigLib.bool    "include-untranslated-definitions" (* Output definitions for which no translation is available yet                *)
+let include_original_code            = ConfigLib.bool    "include-original-code"            (* Annotate all Microsail definitions with their corresponding Sail definition *)
+let include_ignored_definitions      = ConfigLib.bool    "include-ignored-definitions"      (* Output ignored definitions                                                  *)
+let ignore_pragmas                   = ConfigLib.strings "ignore-pragmas"                   (* Pragmas to be ignored                                                       *)
 
 
 let ignore_definition (Libsail.Ast.DEF_aux (definition, _annotation)) =
