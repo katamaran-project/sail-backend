df0c4f88e3c3c16d607538508fec69c899cf59e4
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-01-08 16:04:27 +0100

    updated minicaps configuration

--- minimal-caps/configuration.lisp
+++ minimal-caps/configuration.lisp
@@ -2,3 +2,9 @@
 (include-untranslated-definitions)
 (include-original-code)
 (include-ignored-definitions)
+
+(ignore-pragmas "file_start"
+                "include_start"
+                "include_end"
+                "sail_internal"
+                "file_end")
