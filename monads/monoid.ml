0fbd7eacd6cdb2424d228f981333e7cd7ffc4f4b
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-08 22:23:46 +0100

    removed files

--- monads/monoid.ml
+++
@@ -1,6 +0,0 @@
-module type S = sig
-  type t
-
-  val empty   : t
-  val ( <+> ) : t -> t -> t
-end
--- monads/sig.ml
+++
@@ -1,6 +0,0 @@
-module type Monad = sig
-  type 'a t
-
-  val return : 'a -> 'a t
-  val bind   : 'a t -> ('a -> 'b t) -> 'b t
-end
