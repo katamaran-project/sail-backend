bc21b91a0921cb71cec61860fe02b0da01274b93
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-01-09 13:46:46 +0100

    ignoring functions

--- nanosail/Configuration.ml
+++ nanosail/Configuration.ml
@@ -1,4 +1,5 @@
 open Base
+open NYI
 
 include ConfigLib.Exported
 
@@ -10,11 +11,15 @@ let include_untranslated_definitions = ConfigLib.bool    "include-untranslated-d
 let include_original_code            = ConfigLib.bool    "include-original-code"            (* Annotate all Microsail definitions with their corresponding Sail definition *)
 let include_ignored_definitions      = ConfigLib.bool    "include-ignored-definitions"      (* Output ignored definitions                                                  *)
 let ignore_pragmas                   = ConfigLib.strings "ignore-pragmas"                   (* Pragmas to be ignored                                                       *)
-
+let ignore_functions                 = ConfigLib.strings "ignore-functions"                 (* Functions to be ignored                                                     *)
 
 let ignore_definition (Libsail.Ast.DEF_aux (definition, _annotation)) =
   match definition with
-  | Libsail.Ast.DEF_pragma (identifier, _, _) -> List.mem (get ignore_pragmas) identifier ~equal:String.equal
-  | _                                         -> false
-
-
+  | Libsail.Ast.DEF_pragma (identifier, _, _) ->
+     List.mem (get ignore_pragmas) identifier ~equal:String.equal
+  | Libsail.Ast.DEF_fundef (FD_aux (FD_function (_, _, x), (location, _))) -> begin
+      match x with
+      | [ FCL_aux (Libsail.Ast.FCL_funcl (Libsail.Ast.Id_aux (Id identifier, _), _), _) ] -> List.mem (get ignore_functions) identifier ~equal:String.equal
+      | _ -> not_yet_implemented [%here] location
+    end
+  | _ -> false
