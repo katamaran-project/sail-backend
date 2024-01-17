28aa0b7674c2cc77b539cf04a15dfce2a946d4c5
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-17 16:04:17 +0100

    Error -> Result

--- monads/Monads.ml
+++ monads/Monads.ml
@@ -7,4 +7,4 @@ module Monoid         = Monoid
 module Util           = Util
 module Continuation   = Continuation
 module Reader         = Reader
-module Error          = Error
+module Result         = Result
