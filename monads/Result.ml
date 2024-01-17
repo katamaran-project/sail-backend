fcf366d3eb106abba8da1804ca225e2126fc9234
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-17 16:45:07 +0100

    Result update

--- monads/Result.ml
+++ monads/Result.ml
@@ -9,12 +9,23 @@ module type S = sig
   val fail   : error -> 'a t
   val bind   : 'a t -> ('a -> 'b t) -> 'b t
 
-  val (<|>)  : ('a -> 'b t) -> ('a -> 'b t) -> ('a -> 'b t)
+  val disjunction : ('a -> 'b t) -> ('a -> 'b t) -> ('a -> 'b t)
+  val force       : 'a t -> 'a
+  val map    : 'a t -> f:('a -> 'b) -> 'b t
+  val all    : 'a t list -> ('a list) t
+
+  module Notations : sig
+    val (<|>)  : ('a -> 'b t) -> ('a -> 'b t) -> ('a -> 'b t)
+    val (!!)   : 'a t -> 'a
+    val (|?>)  : ('a -> 'b t) -> ('b -> 'c) -> 'a -> 'c t
+  end
 end
 
 module Make (E : sig type t end) : (S with type error = E.t) = struct
   type error = E.t
-  
+
+  exception ResultError of error
+
   type 'a t =
     | Success of 'a
     | Failure of error
@@ -28,12 +39,41 @@ module Make (E : sig type t end) : (S with type error = E.t) = struct
     | Success a -> f a
     | Failure e -> Failure e
 
-  let (<|>) f g x =
-    match f x with
+  let map x ~f =
+    match x with
+    | Success x -> Success (f x)
+    | Failure e -> Failure e
+
+  let all xs =
+    let rec aux acc xs =
+      match xs with
+      | []                -> return @@ List.rev acc
+      | (Success a) :: xs -> aux (a :: acc) xs
+      | (Failure e) :: _  -> Failure e
+    in
+    aux [] xs
+
+  let disjunction f g result =
+    match f result with
     | Success x -> Success x
     | Failure _ -> begin
-        match g x with
+        match g result with
         | Success x -> Success x
         | Failure e -> Failure e
       end
+
+  let force  result =
+    match result with
+    | Success x -> x
+    | Failure e -> raise @@ ResultError e
+
+
+  module Notations = struct
+    let (<|>) = disjunction
+
+    let (!!) = force
+
+    let (|?>) f g =
+      fun value -> map (f value) ~f:g
+  end
 end
