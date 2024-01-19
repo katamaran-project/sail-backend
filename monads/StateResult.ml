e47906028c73cea0c38a92a68d6f02e587e5c39e
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-19 12:11:46 +0100

    StateResult

--- monads/StateResult.ml
+++ monads/StateResult.ml
@@ -3,40 +3,53 @@ module type S = sig
 
   type state
   type error
-  type 'a result
+  type 'a accessor = (state -> 'a) * (state -> 'a -> state)
+  type 'a result =
+    | Success of 'a
+    | Failure of error
 
-  val get : state t
-  val put : state -> unit t
-  val run : 'a t -> state -> ('a result * state)
+  val get     : 'a accessor -> 'a t
+  val put     : 'a accessor -> 'a -> unit t
+  val run     : 'a t -> state -> ('a result * state)
+  val fail    : error -> 'a t
+  val recover : 'a t -> (error -> 'a t) -> 'a t
 end
 
 module Make (S : sig type t end) (E : sig type t end) : (S with type state = S.t and type error = E.t) = struct
   module SM = ComponentState.Make(S)
   module RM = Result.Make(E)
-  
-  type    state  =    S.t
-  type    error  =    E.t
-  type 'a result = 'a RM.t
 
-  type 'a t =
-    | State of (state -> ('a RM.t) * state)
+  type 'a t        = 'a RM.t SM.t
+  type    state    =    S.t
+  type    error    =    E.t
+  type 'a result   = 'a RM.t         = Success of 'a | Failure of error   (* needed to prevent result from becoming abstract *)
+  type 'a accessor = 'a SM.accessor
 
-  let run (State f) s =
-    f s
+  let run = SM.run
 
-  let return x =
-    State (fun s -> (RM.Success x, s))
+  let return (x : 'a) : 'a t =
+    Base.Fn.compose SM.return RM.return x
 
-  let bind (State f) g =
-    State (fun s ->
-        match f s with
-        | (RM.Success a, s') -> run (g a) s'
-        | (RM.Failure e, s') -> (RM.Failure e, s')
+  let fail error =
+    SM.return @@ RM.Failure error
+
+  let bind (f : 'a t) (g : 'a -> 'b t) : 'b t =
+    SM.bind f (fun x -> 
+        match x with
+        | RM.Success x -> g x
+        | RM.Failure e -> SM.return @@ RM.Failure e
       )
 
-  let get =
-    State (fun s -> (RM.return s, s))
+  let get (accessor : 'a accessor) : 'a t =
+    SM.bind (SM.get accessor) return
 
-  let put s =
-    State (fun _ -> (RM.return (), s))
+  let put (accessor : 'a accessor) (x : 'a) : unit t =
+    SM.bind (SM.put accessor x) return
+
+  let recover (f : 'a t) (error_handler: error -> 'a t) =
+    SM.bind f (fun result ->
+        match result with
+        | Success _ -> SM.return result
+        | Failure e -> error_handler e
+      )
 end
