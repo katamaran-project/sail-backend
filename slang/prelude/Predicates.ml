76f30915668d0cde700a29f08d639c284d29f370
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-20 17:23:21 +0100

    slang predicates

--- slang/prelude/Predicates.ml
+++ slang/prelude/Predicates.ml
@@ -4,23 +4,29 @@ open Monads.Notations.Star(EvaluationContext)
 
 
 module EV = EvaluationContext
-module M = Multimethods
+module M  = Multimethods
+module P  = Value.Predicate
 
 open Shared
 
 
-let is_cons (args : Value.t list) =
+let mk_predicate pred args =
   let impl args =
     let=? v = M.(map1 value) args
     in
-    match v with
-    | Value.Cons (_, _) -> EC.return @@ Some (Value.Bool true)
-    | _                 -> EC.return @@ Some (Value.Bool false)
+    EC.return @@ Some (Value.Bool (pred v))
   in
   M.mk_multimethod [ impl ] args
 
 
 let library env =
   EnvironmentBuilder.extend_environment env (fun { native_function; _ } ->
-      native_function "cons?" is_cons;
+      native_function "cons?"    @@ mk_predicate P.is_cons;
+      native_function "integer?" @@ mk_predicate P.is_integer;
+      native_function "symbol?"  @@ mk_predicate P.is_symbol;
+      native_function "string?"  @@ mk_predicate P.is_string;
+      native_function "bool?"    @@ mk_predicate P.is_bool;
+      native_function "nil?"     @@ mk_predicate P.is_nil;
+      native_function "closure?" @@ mk_predicate P.is_closure;
+      native_function "native?"  @@ mk_predicate P.is_native;
     )
