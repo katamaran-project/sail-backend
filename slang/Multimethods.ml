b423fe0ae0b918357fe773f4113f1deb455151c6
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-20 02:10:52 +0100

    multimethods

--- slang/Multimethods.ml
+++ slang/Multimethods.ml
@@ -1,87 +1,117 @@
 open! Base
 open! Auxlib
+open Evaluation
+module EC = EvaluationContext
+open Monads.Notations.Star(EC)
 
 
-exception DispatchFailure
-exception ExecutionError
+type 'a converter         = Value.t -> 'a option
+type 'a multiconverter    = Value.t list -> 'a option
+type    method_definition = Value.t list -> Value.t option EvaluationContext.t
 
 
-type 'a converter = Value.t -> 'a
+
+let (let=?) x f = Option.bind x ~f:f
+
+let (and=?) x y = let=? x in let=? y in Some (x, y)
+
+let return x = Some x
+
+let fail = None
 
 
 let value (v : Value.t) =
-  v
+  Some v
 
 
 let integer value =
   match value with
-  | Value.Integer n -> n
-  | _               -> raise DispatchFailure
+  | Value.Integer n -> return n
+  | _               -> fail
 
 
 let tuple2 f1 f2 value =
   match value with
-  | Value.Cons (x1, Cons (x2, Nil)) -> let x1 = f1 x1 and x2 = f2 x2 in (x1, x2)
-  | _                               -> raise DispatchFailure
+  | Value.Cons (x1, Cons (x2, Nil)) -> let=? x1 = f1 x1 and=? x2 = f2 x2 in return (x1, x2)
+  | _                               -> fail
 
 
 let tuple3 f1 f2 f3 value =
   match value with
-  | Value.Cons (x1, Value.Cons (x2, Value.Cons (x3, Nil))) -> (f1 x1, f2 x2, f3 x3)
-  | _                                                      -> raise DispatchFailure
+  | Value.Cons (x1, Value.Cons (x2, Value.Cons (x3, Nil))) -> let=? x1 = f1 x1 and=? x2 = f2 x2 and=? x3 = f3 x3 in return (x1, x2, x3)
+  | _                                                      -> fail
 
 
 let string value =
   match value with
-  | Value.String s -> s
-  | _              -> raise DispatchFailure
+  | Value.String s -> return s
+  | _              -> fail
 
 
 let symbol value =
   match value with
-  | Value.Symbol identifier -> identifier
-  | _                       -> raise DispatchFailure
+  | Value.Symbol identifier -> return identifier
+  | _                       -> fail
 
 
 let cons f g value =
   match value with
-  | Value.Cons (car, cdr) -> (f car, g cdr)
-  | _                     -> raise DispatchFailure
+  | Value.Cons (car, cdr) -> let=? x = f car and=? y = g cdr in return (x, y)
+  | _                     -> fail
 
 
 let nil value =
   match value with
-  | Value.Nil -> ()
-  | _         -> raise DispatchFailure
+  | Value.Nil -> return ()
+  | _         -> fail
 
 
-let rec list f value =
-  match value with
-  | Value.Cons (car, cdr) -> f car :: list f cdr
-  | Value.Nil             -> []
-  | _                     -> raise DispatchFailure
+let list ?(min_length = 0) f value =
+  let rec aux acc value =
+    match value with
+    | Value.Cons (car, cdr) -> let=? x = f car in aux (x :: acc) cdr
+    | Value.Nil             -> return @@ List.rev acc
+    | _                     -> fail
+  in
+  let=? elts = aux [] value
+  in
+  if List.length elts < min_length
+  then fail
+  else return elts
+
+
+(* expects values to contain exactly 1 item that satisfy the given pattern *)
+let map1 (f : 'a converter) (values : Value.t list) =
+  match values with
+  | [v1] -> let=? v1' = f v1 in return v1'
+  | _    -> fail
 
 
 (* expects values to contain exactly 2 items that satisfy the given patterns *)
-let map2 f g values =
+let map2 (f : 'a converter) (g : 'b converter) (values : Value.t list) =
   match values with
-  | [v1; v2] -> (f v1, g v2)
-  | _        -> raise DispatchFailure
+  | [v1; v2] -> let=? v1' = f v1 and=? v2' = g v2 in return (v1', v2')
+  | _        -> fail
 
 
-let binary_combine f g arg =
-  try
-    f arg
-  with
-  | DispatchFailure -> g arg
+let mk_multimacro methods arguments =
+  let rec call_matching_method methods =
+    match methods with
+    | []    -> raise @@ Exception.SlangError "no method found"
+    | m::ms -> begin
+        let* state = EC.current_state
+        in
+        let* result = m arguments
+        in
+        match result with
+        | Some result -> EC.return result
+        | None        -> let* () = EC.set_current_state state in call_matching_method ms
+      end
+  in
+  call_matching_method methods
 
 
-let rec combine methods arg =
-  match methods with
-  | []    -> raise DispatchFailure
-  | m::ms -> (binary_combine m (combine ms)) arg
-
-
-module Notations = struct
-  let (<+>) = binary_combine
-end
+let mk_multimethod (methods : method_definition list) arguments =
+  let* evaluated_arguments = EC.map evaluate arguments
+  in
+  mk_multimacro methods evaluated_arguments
--- slang/Multimethods.mli
+++ slang/Multimethods.mli
@@ -1,21 +1,20 @@
-exception DispatchFailure
-exception ExecutionError
+type 'a converter         = Value.t -> 'a option
+type 'a multiconverter    = Value.t list -> 'a option
+type    method_definition = Value.t list -> Value.t option EvaluationContext.t
 
-type 'a converter = Value.t -> 'a
+val value          : Value.t converter
+val integer        : int converter
+val string         : string converter
+val symbol         : string converter
 
-val value   : Value.t converter
-val integer : int converter
-val string  : string converter
-val tuple2  : 'a converter -> 'b converter -> ('a * 'b) converter
-val tuple3  : 'a converter -> 'b converter -> 'c converter -> ('a * 'b * 'c) converter
-val symbol  : string converter
-val cons    : 'a converter -> 'b converter -> ('a * 'b) converter
-val nil     : unit converter
-val list    : 'a converter -> 'a list converter
+val cons           : 'a converter -> 'b converter -> ('a * 'b) converter
+val nil            : unit converter
 
-val map2    : 'a converter -> 'b converter -> Value.t list -> 'a * 'b
-val combine : ('a -> 'b) list -> 'a -> 'b
+val list           : ?min_length : int -> 'a converter -> 'a list converter
+val tuple2         : 'a converter -> 'b converter -> ('a * 'b) converter
+val tuple3         : 'a converter -> 'b converter -> 'c converter -> ('a * 'b * 'c) converter
 
-module Notations : sig
-  val (<+>) : ('a -> 'b) -> ('a -> 'b) -> 'a -> 'b
-end
+val map1           : 'a converter -> 'a multiconverter
+val map2           : 'a converter -> 'b converter -> ('a * 'b) multiconverter
+val mk_multimacro  : method_definition list -> Value.native_function    
+val mk_multimethod : method_definition list -> Value.native_function
