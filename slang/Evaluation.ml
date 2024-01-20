4ac7c7412c4ab3286ac5c4f83a4b025bfd4b62c4
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-20 22:03:02 +0100

    got rid of EvaluationError

--- slang/Evaluation.ml
+++ slang/Evaluation.ml
@@ -1,5 +1,6 @@
 open Base
 open Auxlib
+open Exception
 
 
 module EC = struct
@@ -7,12 +8,11 @@ module EC = struct
   include Monads.Notations.Star(EvaluationContext)
 end
 
-exception EvaluationError of string
 
 let bind_parameters parameters arguments =
   match List.zip parameters arguments with
   | List.Or_unequal_lengths.Ok pairs        -> EC.iter (uncurry EC.add_binding) pairs
-  | List.Or_unequal_lengths.Unequal_lengths -> raise @@ EvaluationError "wrong number of parameters"
+  | List.Or_unequal_lengths.Unequal_lengths -> raise @@ SlangError "wrong number of parameters"
 
 let with_environment (env : Value.t Environment.t) (func : 'a EC.t) : 'a EC.t =
   let open EC
@@ -34,14 +34,14 @@ let rec evaluate (ast : Value.t) : Value.t EC.t =
       in
       match Value.cons_to_list args with
       | Some vs -> evaluate_call f vs
-      | _       -> raise @@ EvaluationError "invalid call: expected well-formed list"
+      | _       -> raise @@ SlangError "invalid call: expected well-formed list"
     end
   | Value.Symbol identifier -> begin
       let* lookup_result = lookup identifier
       in
       match lookup_result with
       | Some value          -> return value
-      | None                -> raise @@ EvaluationError ("unbound identifier " ^ identifier)
+      | None                -> raise @@ SlangError ("unbound identifier " ^ identifier)
     end
   | Value.Integer _      -> return ast
   | Value.String _       -> return ast
@@ -51,12 +51,12 @@ let rec evaluate (ast : Value.t) : Value.t EC.t =
 
 and evaluate_call func arguments =
   match func with
-  | Value.Cons (_, _)    -> raise @@ EvaluationError "cons are not callable"
-  | Value.Integer _      -> raise @@ EvaluationError "integers are not callable"
-  | Value.Symbol _       -> raise @@ EvaluationError "symbols are not callable"
-  | Value.String _       -> raise @@ EvaluationError "strings are not callable"
-  | Value.Bool _         -> raise @@ EvaluationError "bools are not callable"
-  | Value.Nil            -> raise @@ EvaluationError "nil is not callable"
+  | Value.Cons (_, _)    -> raise @@ SlangError "cons are not callable"
+  | Value.Integer _      -> raise @@ SlangError "integers are not callable"
+  | Value.Symbol _       -> raise @@ SlangError "symbols are not callable"
+  | Value.String _       -> raise @@ SlangError "strings are not callable"
+  | Value.Bool _         -> raise @@ SlangError "bools are not callable"
+  | Value.Nil            -> raise @@ SlangError "nil is not callable"
   | Value.Callable f     -> evaluate_callable f arguments
 
 and evaluate_callable native_function arguments =
