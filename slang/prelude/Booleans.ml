e071acef79b1691c0df9dffed779b9cf454c1061
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-21 00:54:26 +0100

    negation

---
+++ slang/prelude/Booleans.ml
@@ -0,0 +1,49 @@
+open Base
+open Evaluation
+open Monads.Notations.Star(EvaluationContext)
+
+module EV = EvaluationContext
+module M  = Multimethods
+
+open Shared
+    
+
+let rec conjunction args =
+  match args with
+  | []    -> EC.return @@ Value.Bool true
+  | x::xs -> begin
+      let* x' = evaluate x
+      in
+      if Value.truthy x'
+      then conjunction xs
+      else EC.return @@ Value.Bool false
+    end
+
+
+let rec disjunction args =
+  match args with
+  | []    -> EC.return @@ Value.Bool false
+  | x::xs -> begin
+      let* x' = evaluate x
+      in
+      if Value.truthy x'
+      then EC.return @@ Value.Bool true
+      else disjunction xs
+    end
+
+
+let rec negation args =
+  let impl args =
+    let=? b = M.(map1 truthy) args
+    in
+    EC.return @@ Some (Value.Bool (not b))
+  in
+  M.mk_multimethod [ impl ] args
+
+
+let library env =
+  EnvironmentBuilder.extend_environment env (fun { callable; _ } ->
+      callable "and" conjunction;
+      callable "or"  disjunction;
+      callable "not" negation;
+    )
