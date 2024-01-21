a6835eb74a5726a4895415c03213e97ff9fac2c7
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-01-21 23:02:19 +0100

    tests

--- tests/slang-tests/advanced_tests.ml
+++ tests/slang-tests/advanced_tests.ml
@@ -2,6 +2,8 @@ open Base
 open Auxlib
 open OUnit2
 
+open Shared.ListMonadNotations
+
 
 let test_run input expected =
   input >:: fun _ -> begin
@@ -18,31 +20,50 @@ let arithmetic_tests =
   let open Slang.Value
   in
   let test_cases =
-    [
-      (
-        {|
-          (define (mod x y)
-            (if (< x y)
-              x
-              (recurse (- x y) y)))
-          (mod 9 5)
-        |},
-        Integer 4
-      );
-      (
-        {|
-          (define (filter pick? xs)
-            (if (nil? xs)
-                ()
-                (if (pick? (car xs))
-                    (cons (car xs) (recurse pick? (cdr xs)))
-                    (recurse pick? (cdr xs)))))
+    build_list begin fun { addall; _ } ->
+      addall begin
+        let* x = List.range 0 10
+        and* y = List.range 1 10
+        in
+        [
+          Printf.sprintf
+          {|
+            (define (mod x y)
+              (if (< x y)
+                x
+                (recurse (- x y) y)))
+            (mod %d %d)
+          |} x y,
+          Integer (x % y)
+        ];
+      end;
 
-          (filter (lambda (x) (> x 0)) '(-4 9 -2 4))
-        |},
-        list_to_cons [Integer 9; Integer 4]
-      );
-    ]
+      addall begin
+        let* xs, expected = [
+          ("()", []);
+          ("(1)", [1]);
+          ("(-1)", []);
+          ("(1 2)", [1; 2]);
+          ("(-1 2)", [2]);
+          ("(-1 2 -3 4 -5)", [2; 4]);
+        ]
+        in
+        [
+          Printf.sprintf
+          {|
+            (define (filter pick? xs)
+              (if (nil? xs)
+                  ()
+                  (if (pick? (car xs))
+                      (cons (car xs) (recurse pick? (cdr xs)))
+                      (recurse pick? (cdr xs)))))
+
+            (filter (lambda (x) (> x 0)) '%s)
+          |} xs,
+          list_to_cons @@ List.map expected ~f:Mk.integer
+        ];
+      end;
+    end
   in
   "advanced" >::: List.map ~f:(uncurry test_run) test_cases
 
