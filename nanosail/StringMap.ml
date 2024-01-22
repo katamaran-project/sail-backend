52417772b24e7d266ed5f32af62c6cc28e2aeaf8
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-01-22 17:15:37 +0100

    StringMap

--- nanosail/StringMap.ml
+++ nanosail/StringMap.ml
@@ -5,4 +5,8 @@ type 'a t = (string, 'a, String.comparator_witness) Map.t
 
 let empty = Map.empty(module String)
 
-let add = Map.add
+let add  = Map.add
+
+let fold = Map.fold
+
+let to_list = Map.to_alist
