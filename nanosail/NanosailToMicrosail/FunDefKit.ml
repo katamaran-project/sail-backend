a22fac0107a8ec4c259ca77775d12c32b9cbd99b
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-01-23 10:13:26 +0100

    split code into files

---
+++ nanosail/NanosailToMicrosail/FunDefKit.ml
@@ -0,0 +1,107 @@
+open Base
+open PPrint
+open Ast
+open Auxlib
+open Util
+open Statements
+open Monads.Notations.Star(AnnotationContext)
+
+module AC = AnnotationContext
+
+
+let pp_function_definition
+      ((sail_function_definition : sail_definition), (function_definition : function_definition))
+      type_constraint =
+  let identifier = Sail.pp_identifier @@ "fun_" ^ function_definition.function_name in
+  let parameters = [] in
+  let coq_definition =
+    let* result_type =
+      let* bindings =
+        let* docs = AC.map Sail.pp_bind function_definition.function_type.arg_types
+        in
+        AC.return @@ Coq.list docs
+      in
+      let* result_type =
+        Sail.pp_nanotype function_definition.function_type.ret_type
+      in
+      AC.return @@ Some (
+                      pp_hanging_list (PP.string "Stm") [
+                          bindings;
+                          result_type
+                        ]
+                    )
+    in
+    let* body =
+      pp_statement function_definition.function_body
+    in
+    AC.return @@ Coq.definition ~identifier ~parameters ~result_type ~body
+  in
+  let original_sail_code =
+    build_list (fun { add; _ } ->
+        (
+          match type_constraint with
+          | Some (sail_type_constraint, _) -> add sail_type_constraint
+          | None                           -> ()
+        );
+        add sail_function_definition;
+      )
+  in
+  Coq.annotate_with_original_definitions
+    original_sail_code
+    (Coq.annotate coq_definition)
+
+let pp_function_definitions
+      (function_definitions : (sail_definition * function_definition) list)
+      top_level_type_constraint_definitions =
+  let type_and_function_pairs =
+    let find_type_constraint function_name =
+      match
+        List.filter
+          ~f:(fun (_, type_constraint) -> String.equal type_constraint.identifier function_name)
+          top_level_type_constraint_definitions
+      with
+      | [x] -> Some x
+      | []  -> None
+      | _   -> None
+    in
+    List.map ~f:(fun ((_sail_definition, function_definition) as fdef) ->
+        (fdef, find_type_constraint function_definition.function_name))
+      function_definitions
+  in
+  List.map ~f:(uncurry pp_function_definition) type_and_function_pairs
+
+let pp_function_definition_kit
+      function_definitions
+      top_level_type_constraint_definitions =
+  let fundef =
+    let identifier = Sail.pp_identifier "FunDef"
+    and parameters = [
+        utf8string "{Δ τ}";
+        utf8string "(f : Fun Δ τ)"
+      ]
+    and result_type = Some (utf8string "Stm Δ τ")
+    and body =
+      let matched_expression =
+        utf8string "f in Fun Δ τ return Stm Δ τ"
+      and cases =
+        let case_of_function_definition function_definition =
+          (
+            string function_definition.function_name,
+            string (Printf.sprintf "fun_%s" function_definition.function_name)
+          )
+        in
+        List.map ~f:case_of_function_definition (List.map ~f:snd function_definitions)
+      in
+      Coq.match' matched_expression cases
+    in
+    Coq.definition ~identifier ~parameters ~result_type ~body
+  in
+  let contents =
+    separate small_step (
+        build_list (fun { add; addall } ->
+            addall @@ pp_function_definitions function_definitions top_level_type_constraint_definitions;
+            add fundef
+          )
+      )
+  in
+  Coq.section "FunDefKit" contents
---
+++ nanosail/NanosailToMicrosail/Statements.ml
@@ -0,0 +1,114 @@
+open Base
+open PPrint
+open Ast
+open Auxlib
+open Util
+open Monads.Notations.Star(AnnotationContext)
+open Expressions
+
+module AC = AnnotationContext
+
+
+let rec pp_statement statement =
+  match statement with
+  | Stm_exp e ->
+     let* e' = pp_par_expression e
+     in
+     AC.return @@ simple_app [string "stm_exp"; e']
+
+  | Stm_match match_pattern -> begin
+      match match_pattern with
+      | MP_list { matched; when_nil; when_cons } -> begin
+          let id_head, id_tail, when_cons_body = when_cons
+          in
+          let* matched'   = pp_par_statement matched
+          and* when_nil'  = pp_par_statement when_nil
+          and* when_cons' = pp_par_statement when_cons_body
+          in
+          AC.return @@ simple_app [
+            string "stm_match_list";
+            matched';
+            when_nil';
+            dquotes (string id_head);
+            dquotes (string id_tail);
+            when_cons';
+          ]
+        end
+
+      | MP_product { matched; id_fst; id_snd; body } -> begin
+          let* matched' = pp_par_statement matched
+          and* body'    = pp_par_statement body
+          in
+          AC.return @@ simple_app [
+            string "stm_match_prod";
+            matched';
+            dquotes (string id_fst);
+            dquotes (string id_snd);
+            body';
+          ]
+        end
+
+      | MP_bool { condition; when_true; when_false } -> begin
+          let* condition'  = pp_par_statement condition
+          and* when_true'  = pp_par_statement when_true
+          and* when_false' = pp_par_statement when_false
+          in
+          AC.return @@ simple_app [
+            string "stm_if";
+            condition';
+            when_true';
+            when_false'
+          ]
+        end
+
+      | MP_enum { matched; cases } -> begin
+          let translate_case ~(key:string) ~(data:statement) (acc : document list AC.t) =
+            let* acc
+            and* pattern = AC.return @@ string key
+            and* clause = pp_statement data
+            in
+            AC.return @@ separate space [
+              string "|";
+              pattern;
+              string " => ";
+              clause
+            ] :: acc
+          in
+          let* matched' = pp_par_statement matched
+          and* cases' = StringMap.fold cases ~init:(AC.return []) ~f:translate_case
+          in
+          AC.return @@ separate hardline @@ build_list @@ fun { add; addall } -> begin
+            add @@ Coq.comment @@ string "TODO Fix this";
+            add @@ separate space [ string "match"; matched'; string "with" ];
+            addall cases'
+          end
+        end
+    end
+
+  | Stm_call (f, arg_list) ->
+     let* arg_list' = AC.map pp_par_expression arg_list
+     in
+     AC.return @@ simple_app @@ string "call" :: string f :: arg_list'
+
+  | Stm_let (v, s1, s2) ->
+     let* s1' = pp_statement s1
+     and* s2' = pp_statement s2
+     in
+     AC.return @@
+         simple_app [
+             string ("let: \"" ^ v ^ "\" :=");
+             s1';
+             string "in";
+             s2'
+           ]
+
+  | Stm_seq (s1, s2) ->
+     let* s1' = pp_par_statement s1
+     and* s2' = pp_par_statement s2
+     in
+     AC.return @@ simple_app [ string "stm_seq"; s1'; s2' ]
+
+and pp_par_statement s =
+  let* s' = pp_statement s
+  in
+  AC.return @@ parens s'
