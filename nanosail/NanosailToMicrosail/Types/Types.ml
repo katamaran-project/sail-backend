022527c6944037ef90b1774dbc358c4600fc4ef6
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-01-30 16:09:12 +0100

    TypeAbbreviations file

--- nanosail/NanosailToMicrosail/Types/Types.ml
+++ nanosail/NanosailToMicrosail/Types/Types.ml
@@ -8,39 +8,8 @@ module AC = AnnotationContext
 module PP = PPrint
 
 module Variants = Variants
+module TypeAbbreviations = TypeAbbreviations
 
-module TypeAbbreviations = struct
-  let generate (type_abbreviation : type_abbreviation_definition) : AC.annotation AC.t =
-    let { identifier; abbreviation } = type_abbreviation
-    in
-    match abbreviation with
-    | TA_numeric_expression (quantifier, numexpr) -> begin
-        let  identifier  = pp_identifier identifier
-        and  result_type = None in
-        let* body        = Numeric.pp_numeric_expression numexpr
-        and* parameters  = Sail.pp_type_quantifier quantifier
-        in
-        AC.return @@ Coq.definition ~identifier ~parameters ~result_type ~body
-      end
-
-    | TA_numeric_constraint (quantifier, numconstraint) -> begin
-        let  identifier  = pp_identifier identifier
-        and  result_type = None in
-        let* body        = Numeric.pp_numeric_constraint numconstraint
-        and* parameters  = Sail.pp_type_quantifier quantifier
-        in
-        AC.return @@ Coq.definition ~identifier ~parameters ~result_type ~body
-      end
-
-    | TA_alias (quantifier, typ) -> begin
-        let  identifier  = pp_identifier identifier
-        and  result_type = None in
-        let* body        = Nanotype.pp_nanotype typ
-        and* parameters  = Sail.pp_type_quantifier quantifier
-        in
-        AC.return @@ Coq.definition ~identifier ~parameters ~result_type ~body;
-      end
-end
 
 
 module Enums = struct
