f18e246fe1d6be0fd71f00ca76cf501081ef62e9
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-02-12 12:10:02 +0100

    cleanup

--- slang/prelude/Lists.ml
+++ slang/prelude/Lists.ml
@@ -10,37 +10,37 @@ open Shared
 
 
 (* (cons x y) creates a pair with values x and y *)
-let cons (args : Value.t list) =
+let cons =
   let impl args =
     let=? car, cdr = C.(map2 value value) args
     in
     EC.return @@ Option.some @@ Value.Cons (car, cdr)
   in
-  mk_multimethod [ impl ] args
+  ("cons", mk_multimethod [ impl ])
 
 
 (* (car pair) returns the first element of the given pair *)
-let car args =
+let car =
   let impl args =
     let=? (car, _) = C.(map1 (cons value value)) args
     in
     EC.return @@ Some car
   in
-  mk_multimethod [ impl ] args
+  ("car", mk_multimethod [ impl ])
 
 
 (* (cdr pair) returns the second element of the given pair *)
-let cdr args =
+let cdr =
   let impl args =
     let=? (_, cdr) = C.(map1 (cons value value)) args
     in
     EC.return @@ Some cdr
   in
-  mk_multimethod [ impl ] args
+  ("cdr", mk_multimethod [ impl ])
 
 
 (* (any? predicate list) *)
-let any args =
+let any =
   let impl args =
     let=? predicate, items = C.(map2 callable (list value)) args
     in
@@ -48,11 +48,11 @@ let any args =
     in
     EC.lift ~f:(Fn.compose Option.some Value.Mk.bool) @@ EC.exists ~f:predicate items
   in
-  mk_multimethod [ impl ] args
+  ("any?", mk_multimethod [ impl ])
 
 
 (* (all? predicate list) *)
-let all args =
+let all =
   let impl args =
     let=? predicate, items = C.(map2 callable (list value)) args
     in
@@ -60,25 +60,22 @@ let all args =
     in
     EC.lift ~f:(Fn.compose Option.some Value.Mk.bool) @@ EC.forall ~f:predicate items
   in
-  mk_multimethod [ impl ] args
+  ("all?", mk_multimethod [ impl ])
 
 
 (* (contains? list value) *)
-let contains args =
+let contains =
   let impl args =
     let=? list, value = C.(map2 (list value) value) args
     in
     EC.return @@ Option.some @@ Value.Mk.bool @@ List.exists ~f:(Value.equal value) list
   in
-  mk_multimethod [ impl ] args
+  ("contains?", mk_multimethod [ impl ])
 
 
 let library env =
   EnvironmentBuilder.extend_environment env (fun { callable; _ } ->
-      callable "cons" cons;
-      callable "car" car;
-      callable "cdr" cdr;
-      callable "any?" any;
-      callable "all?" all;
-      callable "contains?" contains
+      List.iter
+        ~f:(Auxlib.uncurry callable)
+        [ cons; car; cdr; any; all; contains ]
     )
