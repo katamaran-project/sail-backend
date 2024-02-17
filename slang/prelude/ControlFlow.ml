76b0938df27602257fdaba83ec6ae2956b23244d
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-02-17 21:01:46 +0100

    slang cond

--- slang/prelude/ControlFlow.ml
+++ slang/prelude/ControlFlow.ml
@@ -33,10 +33,30 @@ let if_then_else =
   ("if", M.mk_multimacro [ if_then_else; if_then ])
 
 
+let cond =
+  let impl (args : Value.t list) =
+    let=?? clauses = List.map ~f:C.(tuple2 value value) args
+    in
+    let rec evaluate_cond (clauses : (Value.t * Value.t) list) : Value.t Option.t EC.t =
+      match clauses with
+      | [] -> EV.return @@ Some (Value.Nil)
+      | (condition, expression) :: clauses -> begin
+          let* evaluated_condition = evaluate condition
+          in
+          if Value.truthy evaluated_condition
+          then EV.lift ~f:Option.some @@ evaluate expression
+          else evaluate_cond clauses
+        end
+    in
+    evaluate_cond clauses
+  in
+  ("cond", M.mk_multimacro [ impl ])
+
 
 let library env =
   EnvironmentBuilder.extend_environment env @@ fun { callable; _ } -> begin
     List.iter ~f:(Auxlib.uncurry callable) [
-      if_then_else
+      if_then_else;
+      cond;
     ] 
   end
