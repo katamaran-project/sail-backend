615038b94c1ee65653f5a404e92e86ef0118870f
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-02-17 21:20:22 +0100

    comments

--- slang/Multimethods.ml
+++ slang/Multimethods.ml
@@ -9,6 +9,15 @@ open Monads.Notations.Star(EC)
 type method_definition = Value.t list -> Value.t option EvaluationContext.t
 
 
+(*
+   A special form receives its arguments in unevaluated form, i.e.
+
+     (special-form (+ 1 2))
+
+   receives the list (+ 1 2) as argument, not 3.
+   A special form is not a macro: the result is considered the end result, whereas
+   a macro 
+ *)
 let mk_multi_special_form methods arguments =
   let rec call_matching_method methods =
     match methods with
@@ -26,6 +35,10 @@ let mk_multi_special_form methods arguments =
   call_matching_method methods
 
 
+(*
+   A multimethod is a function that receives its arguments after they have been evaluated,
+   i.e., strict evaluation.   
+ *)
 let mk_multimethod (methods : method_definition list) arguments =
   let* evaluated_arguments = EC.map ~f:evaluate arguments
   in
