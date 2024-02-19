1632f0c718197a5549d0edd4c5a8d2ccadc6015a
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-02-19 14:07:35 +0100

    cleaned up StringMap

--- nanosail/StringMap.ml
+++ nanosail/StringMap.ml
@@ -1,17 +1,12 @@
 open Base
 
+include Map
 
 type 'a t = (string, 'a, String.comparator_witness) Map.t
 
 let of_alist_exn (pairs : (string * 'a) list) = Map.of_alist_exn (module String) pairs
 
-let empty    = Map.empty(module String)
-let add      = Map.add
-let add_exn  = Map.add_exn
-let fold     = Map.fold
-let to_list  = Map.to_alist
-let find     = Map.find
-let find_exn = Map.find_exn
+let empty = Map.empty(module String)
 
 let overwrite map ~key ~data =
   Map.update map key ~f:(fun _ -> data)
