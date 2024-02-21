05c2578202eaf93de9bdc44b8385b93310248562
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-02-21 14:05:11 +0100

    FunctionCalls

--- nanosail/NanosailToMicrosail/FunctionCalls.ml
+++ nanosail/NanosailToMicrosail/FunctionCalls.ml
@@ -6,7 +6,7 @@ open Monads.Notations.Star(AnnotationContext)
 module AC = AnnotationContext
 
 
-let default_translation 
+let default_translation
     (function_identifier : string       )
     (arguments           : document list) : document AC.t
   =
@@ -18,7 +18,7 @@ let default_translation
     end
   in
   AC.return @@ simple_app terms
-  
+
 
 let translate
     (function_identifier : string       )
@@ -28,6 +28,14 @@ let translate
   | "add_bits_int" -> begin
       match arguments with
       | [x; y] -> AC.return @@ PP.parens (x ^^ string "+" ^^ y)
-      | _      -> default_translation function_identifier arguments
+      | _      -> begin
+          let message =
+            Printf.sprintf "%s should receive 2 arguments but instead received %d; falling back on default translation for function calls" function_identifier (List.length arguments)
+          in
+          let* annotation_index = AC.create_annotation @@ string message
+          and* translation      = default_translation function_identifier arguments
+          in
+          AC.return @@ PP.separate space [ translation; Coq.inline_comment (string @@ Int.to_string annotation_index) ]
+        end
     end
   | _ -> default_translation function_identifier arguments
