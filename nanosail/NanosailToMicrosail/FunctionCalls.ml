479f7c9b3f609f445d57eb9a48bacc4c7ce8d0fc
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-02-21 14:09:46 +0100

    FunctionCalls

--- nanosail/NanosailToMicrosail/FunctionCalls.ml
+++ nanosail/NanosailToMicrosail/FunctionCalls.ml
@@ -20,22 +20,29 @@ let default_translation
   AC.return @@ simple_app terms
 
 
+let translate_as_binary_operator
+    (original_function_name : string       )
+    (operator               : string       )
+    (operands               : document list) : document AC.t
+  =
+  match operands with
+  | [x; y] -> AC.return @@ PP.parens @@ PP.separate space [x; string operator; y]
+  | _      -> begin
+      let message =
+        Printf.sprintf "%s should receive 2 arguments but instead received %d; falling back on default translation for function calls" original_function_name (List.length operands)
+      in
+      let* annotation_index = AC.create_annotation @@ string message
+      and* translation      = default_translation original_function_name operands
+      in
+      AC.return @@ PP.separate space [ translation; Coq.inline_comment (string @@ Int.to_string annotation_index) ]
+    end
+  
+
+
 let translate
     (function_identifier : string       )
     (arguments           : document list) : document AC.t
   =
   match function_identifier with
-  | "add_bits_int" -> begin
-      match arguments with
-      | [x; y] -> AC.return @@ PP.parens (x ^^ string "+" ^^ y)
-      | _      -> begin
-          let message =
-            Printf.sprintf "%s should receive 2 arguments but instead received %d; falling back on default translation for function calls" function_identifier (List.length arguments)
-          in
-          let* annotation_index = AC.create_annotation @@ string message
-          and* translation      = default_translation function_identifier arguments
-          in
-          AC.return @@ PP.separate space [ translation; Coq.inline_comment (string @@ Int.to_string annotation_index) ]
-        end
-    end
+  | "add_bits_int" -> translate_as_binary_operator function_identifier "+" arguments
   | _ -> default_translation function_identifier arguments
