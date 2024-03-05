de05bd7821a29b1630f57bd3834a88a3f1f6609b
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-03-05 16:52:38 +0100

    refactoring

--- nanosail/Templates.ml
+++ nanosail/Templates.ml
@@ -3,10 +3,13 @@ open Base
 module EC = Slang.EvaluationContext
 
 
+let process_template input_file output_file =
+  Stdio.printf "%s => %s\n" input_file output_file
+
+
 let process
       (_translation : Ast.program)
   =
   let templates = Configuration.(get template_files)
   in
-  Stdio.printf "PROCESSING TEMPLATES\n";
-  List.iter ~f:(fun (x, y) -> Stdio.printf "**************** %s -> %s\n" x y) templates
+  List.iter ~f:(Auxlib.uncurry process_template) templates
