8088e35e61ae511975735f88d5b01a2817ce856d
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-03-07 11:28:31 +0100

    template test 001

---
+++ tests/template-tests/001/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+SAIL=sail
+
+SAIL_SOURCES=model.sail
+TESTED_FILE=microsail.v
+
+
+$SAIL -katamaran -katamaran_config configuration.lisp $SAIL_SOURCES
+DIFF=$(diff $TESTED_FILE expected/$TESTED_FILE)
+if [ "$DIFF" ]; then
+    echo "TEMPLATE TEST $(basename `pwd`) FAILED"
+    exit -1;
+fi
