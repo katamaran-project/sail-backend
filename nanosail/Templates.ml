a526637db8914054d3c24797c0ea93d4b2e56328
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-03-07 11:57:53 +0100

    templates

--- nanosail/Templates.ml
+++ nanosail/Templates.ml
@@ -3,13 +3,68 @@ open Base
 module EC = Slang.EvaluationContext
 
 
-let process_template input_file output_file =
-  Stdio.printf "%s => %s\n" input_file output_file
+let is_template_block_start line =
+  String.equal (String.rstrip line) "(*<"
 
 
-let process
-      (_translation : Ast.program)
+let is_template_block_end line =
+  String.equal (String.rstrip line) ">*)"
+
+
+let process_template_streams
+    (_translation   : Ast.program        )
+    (input_stream  : Stdio.In_channel.t )
+    (output_stream : Stdio.Out_channel.t)
   =
+  let inside_template_block = ref false
+  and block_acc = ref []
+  in
+  let accumulate_line line =
+    block_acc := line :: !block_acc
+  and output_line line =
+    Stdio.Out_channel.output_lines output_stream [line]
+  and process_block () =
+    () (* todo *)
+  in
+  let start_new_block () =
+    if !inside_template_block
+    then failwith "Nested template blocks are not allowed"
+    else inside_template_block := true
+  and finish_block () =
+    if !inside_template_block
+    then begin
+      process_block ();
+      inside_template_block := false;
+      block_acc := []
+    end
+    else failwith "Unexpected end of template block"
+  and process_line line =
+    if !inside_template_block
+    then accumulate_line line
+    else output_line line
+  in
+  Stdio.In_channel.iter_lines input_stream ~f:begin fun line ->
+    if is_template_block_start line
+    then start_new_block ()
+    else if is_template_block_end line
+    then finish_block ()
+    else process_line line
+  end
+
+
+let process_template
+    (translation : Ast.program)
+    (input_file  : string     )
+    (output_file : string     )
+  =
+  Stdio.In_channel.with_file ~binary:false input_file ~f:begin fun input_stream ->
+    Stdio.Out_channel.with_file output_file ~f:begin fun output_stream ->
+      process_template_streams translation input_stream output_stream
+    end
+  end
+
+
+let process (translation : Ast.program) =
   let templates = Configuration.(get template_files)
   in
-  List.iter ~f:(Auxlib.uncurry process_template) templates
+  List.iter ~f:(fun (i, o) -> process_template translation i o) templates
