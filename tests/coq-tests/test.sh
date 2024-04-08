00dbed4fe21e917bfccea74c3c7562334fb3d599
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-04-08 12:48:40 +0200

    coq-tests/test.sh

---
+++ tests/coq-tests/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+for dir in `find -mindepth 1 -maxdepth 1 -type d | sort`; do
+    (
+        cd $dir;
+        echo "Testing $(basename `pwd`)";
+        ./test.sh;
+        if [ $? != 0 ]; then
+            echo "FAILED $?";
+            exit -1
+        fi
+    )
+done
