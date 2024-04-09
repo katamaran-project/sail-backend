44a1ca12aa799ed3c8d3abc4a8075e9f7f3e1a84
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-04-09 16:51:55 +0200

    no eqdec for regname if no register definitions

--- nanosail/NanosailToMicrosail/EqDec.ml
+++ nanosail/NanosailToMicrosail/EqDec.ml
@@ -6,13 +6,17 @@ module PP = PPrint
 
 
 let generate (definitions : (sail_definition * definition) list) =
-  let _ =
-    definitions
+  let has_registers =
+    let register_definitions =
+      select Extract.register_definition definitions
+    in
+    not @@ List.is_empty register_definitions
   in
   let eqdec_derivations =
     build_list (fun { add; _ } ->
-        add @@ Coq.derive_eqdec_for (Id.mk "RegName")
+        if has_registers
+        then add @@ Coq.derive_eqdec_for (Id.mk "RegName");
       )
   in
   Some PP.(separate hardline eqdec_derivations)
-     
+
