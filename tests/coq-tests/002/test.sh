e69d03b97e79b18b77839df98e1a653564385bb9
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-04-09 15:27:19 +0200

    coq-tests/002

---
+++ tests/coq-tests/002/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+SAIL=sail
+COQ=coqc
+
+SAIL_SOURCES=model.sail
+TESTED_FILE=microsail.v
+
+# $SAIL -ddump_rewrite_ast intermediate -katamaran -katamaran_config configuration.lisp $SAIL_SOURCES
+$SAIL -katamaran -katamaran_config configuration.lisp $SAIL_SOURCES
+$COQ microsail.v
