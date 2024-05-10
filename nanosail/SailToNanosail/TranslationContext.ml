a95e2b34d26c3f2e1dcc4e685f5f24e940b819ca
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-05-10 23:17:23 +0200

    updated TranslationContext

--- nanosail/SailToNanosail/TranslationContext.ml
+++ nanosail/SailToNanosail/TranslationContext.ml
@@ -1,50 +1,33 @@
 open Base
 open Ast
-open Exception
 open Basics
-open Monads.OptionNotation
 
 
 module Context = struct
-  type type_map = Ast.type_definition IdentifierMap.t
-
-  type register_map = nanotype IdentifierMap.t
-
   type t =
     {
-      types         : type_map;     (* maps identifiers to type definitions         *)
-      next_id_index : int;          (* counter used to generate unique identifiers  *)
-      registers     : register_map; (* list of registers                            *)
+      definitions   : definition list;  (* list of definitions                          *)
+      next_id_index : int;              (* counter used to generate unique identifiers  *)
     }
 
   let empty : t =
     {
-      types         = IdentifierMap.empty;
+      definitions = [];
       next_id_index = 0;
-      registers     = IdentifierMap.empty;
     }
-
-  let lookup_type (type_map : type_map) identifier =
-    StringMap.find type_map identifier
 
   (*
      Accessors
   *)
-  let types =
-    let get (context : t) : type_map = context.types
-    and set (context : t) (types : type_map) : t = { context with types }
+  let definitions =
+    let get (context : t) : definition list = context.definitions
+    and set (context : t) (definitions : definition list) : t = { context with definitions }
     in
     (get, set)
-
+  
   let next_id_index =
     let get (context : t) : int = context.next_id_index
     and set (context : t) (next_id_index : int) : t = { context with next_id_index }
-    in
-    (get, set)
-
-  let registers =
-    let get (context : t) : register_map = context.registers
-    and set (context : t) (registers : register_map) : t = { context with registers }
     in
     (get, set)
 end
@@ -91,43 +74,40 @@ let fail ocaml_position message =
   Monad.fail @@ AssertionFailure (ocaml_position, message)
 
 
-let register_type (type_definition : type_definition) =
-  let* types = Monad.get Context.types
-  in
-  let updated_types =
-    let key = type_identifier type_definition
-    and data = type_definition
-    in
-    match IdentifierMap.add types ~key ~data with
-    | `Duplicate -> raise @@ TranslationError (Printf.sprintf "type %s defined multiple times" (Id.string_of key))
-    | `Ok result -> result
-  in
-  Monad.put Context.types updated_types
+(* let register_type (type_definition : type_definition) = *)
+(*   let* types = Monad.get Context.types *)
+(*   in *)
+(*   let updated_types = *)
+(*     let key = type_identifier type_definition *)
+(*     and data = type_definition *)
+(*     in *)
+(*     match IdentifierMap.add types ~key ~data with *)
+(*     | `Duplicate -> raise @@ TranslationError (Printf.sprintf "type %s defined multiple times" (Id.string_of key)) *)
+(*     | `Ok result -> result *)
+(*   in *)
+(*   Monad.put Context.types updated_types *)
 
 
-let register_register (register_definition : register_definition) =
-  let* register_map = Monad.get Context.registers
-  in
-  let updated_register_map =
-    let key = register_definition.identifier
-    and data = register_definition.typ
-    in
-    match IdentifierMap.add register_map ~key ~data with
-    | `Duplicate -> raise @@ TranslationError (Printf.sprintf "register %s defined multiple times" (Id.string_of key))
-    | `Ok result -> result
-  in
-  Monad.put Context.registers updated_register_map
+(* let register_register (register_definition : register_definition) = *)
+(*   let* register_map = Monad.get Context.registers *)
+(*   in *)
+(*   let updated_register_map = *)
+(*     let key = register_definition.identifier *)
+(*     and data = register_definition.typ *)
+(*     in *)
+(*     match IdentifierMap.add register_map ~key ~data with *)
+(*     | `Duplicate -> raise @@ TranslationError (Printf.sprintf "register %s defined multiple times" (Id.string_of key)) *)
+(*     | `Ok result -> result *)
+(*   in *)
+(*   Monad.put Context.registers updated_register_map *)
 
 
 let register (definition : definition) =
-  match definition with
-  | TypeDefinition type_definition         -> register_type type_definition
-  | RegisterDefinition register_definition -> register_register register_definition
-  | TopLevelTypeConstraintDefinition _     -> return ()
-  | FunctionDefinition _                   -> return ()
-  | UntranslatedDefinition _               -> return ()
-  | IgnoredDefinition                      -> return ()
-  | ValueDefinition _                      -> return ()
+  let* old_definitions = Monad.get Context.definitions
+  in
+  let new_definitions = definition :: old_definitions
+  in
+  Monad.put Context.definitions new_definitions
 
 
 (*
@@ -138,20 +118,35 @@ let register (definition : definition) =
 let lookup_type
       (extractor  : type_definition -> 'a option)
       (identifier : identifier                  ) : 'a option t =
-  let* types = Monad.get Context.types
+  let predicate (definition : definition) : type_definition option =
+    match definition with
+    | TypeDefinition type_definition ->
+      if Id.equal identifier (type_identifier type_definition)
+      then Some type_definition
+      else None
+    | _ -> None
   in
-  return @@ begin
-    let=? type_definition = Context.lookup_type types identifier
-    in
-    extractor type_definition
-  end
+  let* definitions = Monad.get Context.definitions
+  in
+  let type_definition = List.find_map definitions ~f:predicate
+  in
+  return @@ Option.bind type_definition ~f:extractor
+
 
 
 (* Looks up type of register with given name *)
 let lookup_register_type (identifier : identifier) : nanotype option t =
-  let* registers = Monad.get Context.registers
+  let predicate (definition : definition) : register_definition option =
+    match definition with
+    | RegisterDefinition register_definition ->
+      if Id.equal register_definition.identifier identifier
+      then Some register_definition
+      else None
+    | _ -> None
   in
-  return @@ IdentifierMap.find registers identifier
+  let* definitions = Monad.get Context.definitions
+  in
+  return @@ Option.map (List.find_map definitions ~f:predicate) ~f:(fun r -> r.typ)
 
 
 let is_register (identifier : identifier) : bool t =
