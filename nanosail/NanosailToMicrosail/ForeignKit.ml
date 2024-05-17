77b147af0c46976831507ed34f1ad931c36f7336
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-05-17 15:42:41 +0200

    cleanup

--- nanosail/NanosailToMicrosail/ForeignKit.ml
+++ nanosail/NanosailToMicrosail/ForeignKit.ml
@@ -1,19 +1,18 @@
-open PPrint
 open Monads.Notations.Star(AnnotationContext)
 
 module AC = AnnotationContext
 
 
-let pp_foreignKit =
+let pp_foreignKit = (* todo *)
   let title = Id.mk "ForeignKit"
   and contents =
-    separate_map hardline utf8string [
+    PP.(separate_map hardline utf8string [
       "Definition Memory : Set := unit.";
       "Definition ForeignCall {σs σ} (f : 𝑭𝑿 σs σ) (args : NamedEnv Val σs)";
       "  (res : string + Val σ) (γ γ' : RegStore) (μ μ' : Memory) : Prop := False.";
       "Lemma ForeignProgress {σs σ} (f : 𝑭𝑿 σs σ) (args : NamedEnv Val σs) γ μ :";
       "  exists γ' μ' res, ForeignCall f args res γ γ' μ μ'.";
       "Proof. destruct f. Qed."
-    ]
+    ])
   in
   Coq.section title contents
