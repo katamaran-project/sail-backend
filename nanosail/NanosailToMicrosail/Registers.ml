fcc691f00c35cc0dc074ae836431627564675884
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-05-17 15:48:38 +0200

    cleanup

--- nanosail/NanosailToMicrosail/Registers.ml
+++ nanosail/NanosailToMicrosail/Registers.ml
@@ -1,5 +1,4 @@
 open Base
-open PPrint
 open Ast
 open Monads.Notations.Star(AnnotationContext)
 open Identifier
@@ -8,8 +7,8 @@ module AC = AnnotationContext
 
 
 let reg_inductive_type register_definitions =
-  let identifier = string "Reg"
-  and typ = string "Ty -> Set"
+  let identifier = PP.string "Reg"
+  and typ = PP.string "Ty -> Set"
   in
   let inductive_type =
     Coq.mbuild_inductive_type identifier typ (fun add_constructor ->
@@ -18,7 +17,7 @@ let reg_inductive_type register_definitions =
           in
           let* register_type = Nanotype.pp_nanotype register_definition.typ
           in
-          let typ = separate space [ string "Reg"; register_type  ]
+          let typ = PP.(separate space [ string "Reg"; register_type  ])
           in
           add_constructor ~typ:typ identifier
         in
@@ -30,15 +29,15 @@ let reg_inductive_type register_definitions =
 
 let no_confusion_for_reg () =
   Coq.section (Id.mk "TransparentObligations") (
-      separate hardline [
+      PP.(separate hardline [
           string "Local Set Transparent Obligations.";
           string "Derive Signature NoConfusion NoConfusionHom EqDec for Reg."
-        ]
+        ])
     )
 
 
 let reg_definition () =
-  utf8string "Definition 𝑹𝑬𝑮 : Ty -> Set := Reg."
+  PP.utf8string "Definition 𝑹𝑬𝑮 : Ty -> Set := Reg."
 
 
 let translate_regname (register_identifier : identifier) : identifier =
@@ -64,8 +63,8 @@ let regnames (register_definitions : (sail_definition * register_definition) lis
       let register_names =
         List.map ~f:(fun (_, def) -> def.identifier) register_definitions
       in
-      let type_name = string "RegName"
-      and typ = string "Set"
+      let type_name = PP.string "RegName"
+      and typ = PP.string "Set"
       in
       let inductive_type =
         Coq.mbuild_inductive_type type_name typ (fun add_constructor ->
@@ -80,9 +79,9 @@ let instance_reg_eq_dec register_names =
     let cs =
       List.map ~f:(fun register_name ->
           let rn_str = register_name in
-          ((rn_str, rn_str), string "left eq_refl"))
+          ((rn_str, rn_str), PP.string "left eq_refl"))
         register_names
-    and wildcard_case = ((string "_", string "_"), string "right _")
+    and wildcard_case = PP.((string "_", string "_"), string "right _")
     in
     Auxlib.build_list (fun { add; addall; _ } ->
         addall cs;
@@ -90,42 +89,50 @@ let instance_reg_eq_dec register_names =
         then add wildcard_case;
       )
   in
-  separate hardline [
+  PP.(
+    separate hardline [
       utf8string "#[export,refine] Instance 𝑹𝑬𝑮_eq_dec : EqDec (sigT Reg) :=";
       utf8string "  fun '(existT σ x) '(existT τ y) =>";
-      PP.indent' (Coq.match_pair (string "x", string "y") cases) ^^ Coq.eol;
+      indent' (Coq.match_pair (string "x", string "y") cases) ^^ Coq.eol;
       string "Proof. all: transparent_abstract (intros H; depelim H). Defined."
     ]
+  )
 
 let reg_finite register_names =
   let enum_values =
     let enum_value_of_register_name register_name =
-      separate space [
-        string "existT";
-        underscore;
-        register_name
-      ]
+      PP.(
+        separate space [
+          string "existT";
+          underscore;
+          register_name
+        ]
+      )
     in
     Coq.list (List.map ~f:enum_value_of_register_name register_names)
   in
-  separate hardline (
+  PP.(
+    separate hardline (
       [
         utf8string "Program Instance 𝑹𝑬𝑮_finite : Finite (sigT Reg) :=";
         PP.indent' (
-            Coq.record_value [
-                (string "enum", enum_values)
+          Coq.record_value [
+            (string "enum", enum_values)
               ]
-          )
+        )
       ]
     )
+  )
 
 let obligation_tactic =
-  separate hardline [
-    string "Local Obligation Tactic :=";
-    string "  finite_from_eqdec."
-  ]
+  PP.(
+    separate hardline [
+      string "Local Obligation Tactic :=";
+      string "  finite_from_eqdec."
+    ]
+  )
 
-let generate (register_definitions : (sail_definition * register_definition) list) : document =
+let generate (register_definitions : (sail_definition * register_definition) list) : PP.document =
   let register_names =
     let extract_identifier (pair : sail_definition * register_definition) =
       pp_identifier (snd pair).identifier
@@ -133,13 +140,13 @@ let generate (register_definitions : (sail_definition * register_definition) lis
     List.map ~f:extract_identifier register_definitions
   in
   let section_contents =
-    Coq.line @@ separate (twice hardline) [
+    Coq.line @@ PP.(separate (twice hardline) [
       reg_inductive_type @@ List.map ~f:snd register_definitions;
       no_confusion_for_reg ();
       reg_definition ();
       instance_reg_eq_dec register_names;
       obligation_tactic;
       reg_finite register_names
-    ]
+    ])
   in
   Coq.section (Id.mk "RegDeclKit") section_contents
