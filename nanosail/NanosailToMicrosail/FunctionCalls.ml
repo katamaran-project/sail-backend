2dd3ee83846488fc55339428f6b9969be27d063a
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-05-17 15:45:55 +0200

    cleanup

--- nanosail/NanosailToMicrosail/FunctionCalls.ml
+++ nanosail/NanosailToMicrosail/FunctionCalls.ml
@@ -1,5 +1,4 @@
 open Base
-open PP
 open Auxlib
 open Identifier
 open Monads.Notations.Star(AnnotationContext)
@@ -8,26 +7,26 @@ module AC = AnnotationContext
 
 
 let default_translation
-    (function_identifier : Ast.identifier)
-    (arguments           : document list ) : document AC.t
+    (function_identifier : Ast.identifier  )
+    (arguments           : PP.document list) : PP.document AC.t
   =
   let terms =
     build_list @@ fun { add; addall; _ } -> begin
-      add @@ string "call";
+      add @@ PP.string "call";
       add @@ pp_identifier function_identifier;
       addall @@ arguments
     end
   in
-  AC.return @@ simple_app terms
+  AC.return @@ PP.simple_app terms
 
 
 let translate_as_binary_operator
-    (original_function_name : Ast.identifier)
-    (operator               : string        )
-    (operands               : document list ) : document AC.t
+    (original_function_name : Ast.identifier   )
+    (operator               : string           )
+    (operands               : PP.document list ) : PP.document AC.t
   =
   match operands with
-  | [x; y] -> AC.return @@ PP.parens @@ PP.separate space [x; string operator; y]
+  | [x; y] -> AC.return @@ PP.(parens @@ separate space [x; string operator; y])
   | _      -> begin
       let message =
         Printf.sprintf
@@ -35,16 +34,16 @@ let translate_as_binary_operator
           (Id.string_of original_function_name)
           (List.length operands)
       in
-      let* annotation_index = AC.create_annotation @@ string message
+      let* annotation_index = AC.create_annotation @@ PP.string message
       and* translation      = default_translation original_function_name operands
       in
-      AC.return @@ PP.separate space [ translation; Coq.inline_comment (string @@ Int.to_string annotation_index) ]
+      AC.return @@ PP.(separate space [ translation; Coq.inline_comment (string @@ Int.to_string annotation_index) ])
     end
 
 
 let translate
-    (function_identifier : Ast.identifier)
-    (arguments           : document list ) : document AC.t
+    (function_identifier : Ast.identifier   )
+    (arguments           : PP.document list ) : PP.document AC.t
   =
   match Id.string_of function_identifier with
   | "add_bits_int" -> translate_as_binary_operator function_identifier "+" arguments
