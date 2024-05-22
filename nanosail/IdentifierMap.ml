7f951cc30f0d0c9e8521850c40d2c68046ac23b8
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-05-22 17:09:31 +0200

    removed redundant code

--- nanosail/IdentifierMap.ml
+++ nanosail/IdentifierMap.ml
@@ -2,8 +2,6 @@ open Base
 
 include Map
 
-let x = Map.of_alist (module Id) [Id.Id "x", 1; Id.Id "y", 2]
-
 type 'a t = (Id.t, 'a, Id.comparator_witness) Map.t
 
 let of_alist_exn (pairs : (Id.t * 'a) list) = Map.of_alist_exn (module Id) pairs
