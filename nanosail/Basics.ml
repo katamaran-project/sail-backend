b62fa63b8c3f87105024c549610a8dc70753fabb
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-05-27 14:11:48 +0200

    code layout

--- nanosail/Basics.ml
+++ nanosail/Basics.ml
@@ -3,8 +3,10 @@ open Base
 
 type ocaml_source_location = Lexing.position
 
+
 let string_of_position ({ pos_fname; pos_lnum; pos_bol; pos_cnum } : ocaml_source_location) =
   Printf.sprintf "Pos(%s:%d:%d:%d)" pos_fname pos_lnum pos_bol pos_cnum
+
 
 let rec string_of_location (location : Libsail.Parse_ast.l) =
   match location with
@@ -13,6 +15,7 @@ let rec string_of_location (location : Libsail.Parse_ast.l) =
   | Generated loc           -> Printf.sprintf "GeneratedLocation(%s)" (string_of_location loc)
   | Hint (hint, loc1, loc2) -> Printf.sprintf "HintLocation(%s, %s, %s)" hint (string_of_location loc1) (string_of_location loc2)
   | Range (pos1, pos2)      -> Printf.sprintf "Range(%s-%s)" (string_of_position pos1) (string_of_position pos2)
+
 
 let string_of_sail_definition
       ?(buffer_initial_size = 1000)
