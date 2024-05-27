d14f5e0676f1b03bf695f9b6496a3692724ea78a
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-05-27 14:11:30 +0200

    unwrap_identifier

--- nanosail/SailToNanosail/Translate/Identifier.ml
+++ nanosail/SailToNanosail/Translate/Identifier.ml
@@ -13,6 +13,17 @@ module TC = TranslationContext
 open Monads.Notations.Star(TC)
 
 
+let unwrap_identifier
+      (ocaml_location : Lexing.position)
+      (id             : Libsail.Ast.id ) : string TC.t
+  =
+  let S.Id_aux (unwrapped_id, id_location) = id
+  in
+  match unwrapped_id with
+  | S.Id string -> TC.return string
+  | S.Operator _ -> TC.not_yet_implemented ocaml_location id_location
+
+
 let translate_identifier
       (ocaml_location : Lexing.position)
       (identifier     : S.id           ) : N.identifier TC.t
