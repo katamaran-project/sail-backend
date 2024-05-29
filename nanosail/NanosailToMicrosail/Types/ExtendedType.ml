9e4936376a3dc3990c6b0afb428ab618eb8831c6
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-05-29 18:57:38 +0200

    extended type stuff

--- nanosail/NanosailToMicrosail/Types/ExtendedType.ml
+++ nanosail/NanosailToMicrosail/Types/ExtendedType.ml
@@ -2,18 +2,48 @@ open Base
 open Monads.Notations.Star(AnnotationContext)
 
 module AC = AnnotationContext
+module Big_int = Nat_big_num
 
 
-let rec pp_extended_type (extended_type : Ast.ExtendedType.t) : PP.document AC.t =
+let rec pp_extended_parameter_type (extended_type : Ast.ExtendedType.Parameter.t) : PP.document AC.t =
+  let open Ast.ExtendedType.Parameter
+  in
   match extended_type with
-  | Ast.ExtendedType.Int k    -> AC.return @@ PP.(string "int" ^^ space ^^ string k)
-  | Ast.ExtendedType.Bool k   -> AC.return @@ PP.(string "bool" ^^ space ^^ string k)
-  | Ast.ExtendedType.Other s  -> AC.return @@ PP.string s
-  | Ast.ExtendedType.Tuple ts -> begin
-      let* ts' = AC.map ~f:pp_extended_type ts (* add parentheses around each t of ts *)
+  | Int k    -> AC.return @@ PP.(string "int" ^^ space ^^ string k)
+  | Bool k   -> AC.return @@ PP.(string "bool" ^^ space ^^ string k)
+  | Other s  -> AC.return @@ PP.string s
+  | Tuple ts -> begin
+      let* ts' = AC.map ~f:pp_extended_parameter_type ts (* add parentheses around each t of ts *)
       in
       AC.return @@ PP.(separate (string " * ") ts')
     end
+
+
+let rec pp_int_expression (integer_expression : Ast.ExtendedType.IntExpression.t) : PP.document AC.t =
+  let pp_unary_operation operator operand =
+    let* operand' = pp_int_expression operand
+    in
+    AC.return @@ PP.(string operator ^^ parens operand')
+  and pp_binary_operation left operator right =
+    let* left'  = pp_int_expression left
+    and* right' = pp_int_expression right
+    in
+    AC.return @@ PP.(separate space [parens left'; string operator; parens right'])
+  in
+  match integer_expression with
+   | Ast.ExtendedType.IntExpression.Var identifier -> AC.return @@ PP.string identifier
+   | Ast.ExtendedType.IntExpression.Constant k -> AC.return @@ PP.string @@ Z.to_string k
+   | Ast.ExtendedType.IntExpression.Add (left, right) -> pp_binary_operation left "+" right
+   | Ast.ExtendedType.IntExpression.Sub (left, right) -> pp_binary_operation left "-" right
+   | Ast.ExtendedType.IntExpression.Mul (left, right) -> pp_binary_operation left "*" right
+   | Ast.ExtendedType.IntExpression.Neg operand -> pp_unary_operation "-" operand
+
+
+let pp_extended_return_value_type (extended_type : Ast.ExtendedType.ReturnValue.t) : PP.document AC.t =
+  let open Ast.ExtendedType.ReturnValue
+  in
+  match extended_type with
+  | Int expression -> pp_int_expression expression
 
 
 let pp_extended_function_type
@@ -30,13 +60,13 @@ let pp_extended_function_type
   let* pp_parameter_names =
     AC.return @@ List.map ~f:(fun name -> PP.(string "parameter " ^^ PP.string name)) parameter_names
   and* pp_parameter_extended_types =
-    AC.map ~f:pp_extended_type parameter_extended_types
+    AC.map ~f:pp_extended_parameter_type parameter_extended_types
   in
   let pp_parameter_pairs =
     List.zip_exn pp_parameter_names pp_parameter_extended_types
   in
   let* pp_return_value_pair =
-    let* ret = pp_extended_type return_extended_type
+    let* ret = pp_extended_return_value_type return_extended_type
     in
     AC.return (PP.string "return value", ret)
   in
--- nanosail/SailToNanosail/Translate/ExtendedType.ml
+++ nanosail/SailToNanosail/Translate/ExtendedType.ml
@@ -33,7 +33,7 @@ let unpack_parameter_types (parameter_bindings : N.type_annotation Libsail.Ast.p
   Focuses on parameter types.
   Atoms must only contain a single identifier as their type argument.
 *)
-let extended_parameter_type_of_sail_type (sail_type : S.typ) : N.ExtendedType.t TC.t =
+let extended_parameter_type_of_sail_type (sail_type : S.typ) : N.ExtendedType.Parameter.t TC.t =
   let S.Typ_aux (unwrapped_sail_type, sail_type_location) = sail_type
   in
   match unwrapped_sail_type with
@@ -71,7 +71,7 @@ let extended_parameter_type_of_sail_type (sail_type : S.typ) : N.ExtendedType.t 
                      | S.Nexp_var kid      -> begin
                          let S.Kid_aux (Var unwrapped_kid, _kid_location) = kid
                          in
-                         TC.return @@ N.ExtendedType.Int unwrapped_kid
+                         TC.return @@ N.ExtendedType.Parameter.Int unwrapped_kid
                        end
                   end
              end
@@ -87,7 +87,32 @@ let extended_parameter_type_of_sail_type (sail_type : S.typ) : N.ExtendedType.t 
      end
 
 
-let extended_return_type_of_sail_type (sail_type : S.typ) : N.ExtendedType.t TC.t =
+
+let rec int_expression_of_sail_numeric_expression (numeric_expression : S.nexp) : N.ExtendedType.IntExpression.t TC.t =
+  let S.Nexp_aux (unwrapped_numeric_expression, numeric_expression_location) = numeric_expression
+  in
+  match unwrapped_numeric_expression with
+   | S.Nexp_id _         -> TC.not_yet_implemented [%here] numeric_expression_location
+   | S.Nexp_app (_, _)   -> TC.not_yet_implemented [%here] numeric_expression_location
+   | S.Nexp_sum (_, _)   -> TC.not_yet_implemented [%here] numeric_expression_location
+   | S.Nexp_minus (_, _) -> TC.not_yet_implemented [%here] numeric_expression_location
+   | S.Nexp_exp _        -> TC.not_yet_implemented [%here] numeric_expression_location
+   | S.Nexp_neg _        -> TC.not_yet_implemented [%here] numeric_expression_location
+   | S.Nexp_constant n   -> TC.return @@ N.ExtendedType.IntExpression.Constant n
+   | S.Nexp_var id       -> begin
+       let S.Kid_aux (Var unwrapped_id, _id_location) = id
+       in
+       TC.return @@ N.ExtendedType.IntExpression.Var unwrapped_id
+     end
+   | S.Nexp_times (left, right) -> begin
+       let* left'  = int_expression_of_sail_numeric_expression left
+       and* right' = int_expression_of_sail_numeric_expression right
+       in
+       TC.return @@ N.ExtendedType.IntExpression.Mul (left', right')
+     end
+
+
+let extended_return_type_of_sail_type (sail_type : S.typ) : N.ExtendedType.ReturnValue.t TC.t =
   let S.Typ_aux (unwrapped_sail_type, sail_type_location) = sail_type
   in
   match unwrapped_sail_type with
@@ -108,25 +133,12 @@ let extended_return_type_of_sail_type (sail_type : S.typ) : N.ExtendedType.t TC.
                let S.A_aux (unwrapped_type_argument, type_argument_location) = type_argument
                in
                match unwrapped_type_argument with
-                | S.A_typ _                     -> TC.not_yet_implemented [%here] type_argument_location
-                | S.A_bool _                    -> TC.not_yet_implemented [%here] type_argument_location
-                | S.A_nexp numerical_expression -> begin
-                    let S.Nexp_aux (unwrapped_numerical_expression, numerical_expression_location) = numerical_expression
+                | S.A_typ _                   -> TC.not_yet_implemented [%here] type_argument_location
+                | S.A_bool _                  -> TC.not_yet_implemented [%here] type_argument_location
+                | S.A_nexp numeric_expression -> begin
+                    let* int_expression = int_expression_of_sail_numeric_expression numeric_expression
                     in
-                    match unwrapped_numerical_expression with
-                     | S.Nexp_id _         -> TC.not_yet_implemented [%here] numerical_expression_location
-                     | S.Nexp_constant _   -> TC.not_yet_implemented [%here] numerical_expression_location
-                     | S.Nexp_app (_, _)   -> TC.not_yet_implemented [%here] numerical_expression_location
-                     | S.Nexp_times (_, _) -> TC.not_yet_implemented [%here] numerical_expression_location
-                     | S.Nexp_sum (_, _)   -> TC.not_yet_implemented [%here] numerical_expression_location
-                     | S.Nexp_minus (_, _) -> TC.not_yet_implemented [%here] numerical_expression_location
-                     | S.Nexp_exp _        -> TC.not_yet_implemented [%here] numerical_expression_location
-                     | S.Nexp_neg _        -> TC.not_yet_implemented [%here] numerical_expression_location
-                     | S.Nexp_var kid      -> begin
-                         let S.Kid_aux (Var unwrapped_kid, _kid_location) = kid
-                         in
-                         TC.return @@ N.ExtendedType.Int unwrapped_kid
-                       end
+                    TC.return @@ N.ExtendedType.ReturnValue.Int int_expression
                   end
              end
            | _ -> TC.not_yet_implemented ~message:"Unexpected number of type arguments (should be exactly one)" [%here] sail_type_location
