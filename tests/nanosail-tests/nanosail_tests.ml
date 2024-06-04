5498a65992e67f4d4445e6dadf94a2153eef7170
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-06-04 16:17:05 +0200

    rename

--- tests/nanosail-tests/nanosail_tests.ml
+++ tests/nanosail-tests/nanosail_tests.ml
@@ -5,7 +5,7 @@ let tests =
   "all tests" >::: [
     ConversionTests.test_suite;
     SubstitutionTests.test_suite;
-    PrecedenceTests.test_suite;
+    PrecedenceFormatterTests.test_suite;
     ExtendedIntegerTests.test_suite;
   ]
 
