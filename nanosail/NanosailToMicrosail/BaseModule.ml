f5959de99f237280c189ae941264169ecbbac5f1
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-06-11 17:38:16 +0200

    refactoring

--- nanosail/NanosailToMicrosail/BaseModule.ml
+++ nanosail/NanosailToMicrosail/BaseModule.ml
@@ -1,12 +1,18 @@
-let pp_base_module () =
-  let base_module_name = "UntitledBase"
-  in
+let pp_typedeclkit () =
   PP.(separate hardline [
-          string "Module Export " ^^ string base_module_name ^^ string " <: Base.";
           string "#[export] Instance typedeclkit : TypeDeclKit :=";
           string "  {| enumi   := Enums;";
           string "     unioni  := Unions;";
           string "     recordi := Records;";
           string "  |}.";
-          string "End " ^^ string base_module_name ^^ dot;
-  ])
+        ]
+  )
+
+
+let pp_base_module () =
+  let base_module_name = "UntitledBase"
+  and flag = Coq.Export
+  and includes = [ "Base" ]
+  and contents = PP.(separate hardline [ pp_typedeclkit () ])
+  in
+  Coq.module' ~flag ~includes base_module_name contents
