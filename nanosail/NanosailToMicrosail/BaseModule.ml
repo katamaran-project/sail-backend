f2d7e746e569be9e68768f853ad7853f69f91a13
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-06-12 11:25:14 +0200

    enum_denote generation

--- nanosail/NanosailToMicrosail/BaseModule.ml
+++ nanosail/NanosailToMicrosail/BaseModule.ml
@@ -13,17 +13,28 @@ let pp_typedeclkit () =
 
 
 let pp_enum_denote (definitions : (Sail.sail_definition * Ast.definition) list) : PP.document =
-  let _enum_definitions =
+  let enum_definitions =
     List.map ~f:snd Ast.(select Extract.(type_definition of_enum) definitions)
   in
+  let parameter_identifier = "e"
+  in
   let identifier  = PP.(string "enum_denote")
-  and parameters  = [ (PP.string "e", Some (PP.string "Enums")) ]
+  and parameters  = [ (PP.string parameter_identifier, Some (PP.string "Enums")) ]
   and result_type = Some (PP.string "Set")
   and body =
-    PP.string "test"
+    let matched_expression = PP.string parameter_identifier
+    and cases =
+      let process_case identifier =
+        (
+          PP.string @@ Ast.Identifier.string_of @@ TranslationSettings.convert_enum_name_to_tag identifier,
+          PP.string @@ Ast.Identifier.string_of identifier
+        )
+      in
+      List.map ~f:process_case @@ List.map ~f:(fun enum_definition -> enum_definition.identifier) enum_definitions
+    in
+    Coq.match' matched_expression cases
   in
   Coq.definition ~identifier ~parameters ~result_type body
-
 
 
 let pp_base_module (definitions : (Sail.sail_definition * Ast.definition) list) : PP.document
