9e0646f356907aa4d74e019e317559c12b3c1558
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-06-20 12:02:36 +0200

    NumericExpression.t

--- nanosail/Ast/NumericExpression.ml
+++ nanosail/Ast/NumericExpression.ml
@@ -16,3 +16,15 @@ let rec to_string (numeric_expression : t) =
   | Neg e          -> Printf.sprintf "-%s" (to_string e)
   | Id id          -> Identifier.string_of id
   | Var id         -> Identifier.string_of id
+
+
+let rec equal (t1 : t) (t2 : t) : bool =
+  match t1, t2 with
+   | Constant n1     , Constant n2      -> Z.equal n1 n2
+   | Add (t1a, t1b)  , Add (t2a, t2b)   -> equal t1a t2a && equal t1b t2b
+   | Minus (t1a, t1b), Minus (t2a, t2b) -> equal t1a t2a && equal t1b t2b
+   | Times (t1a, t1b), Times (t2a, t2b) -> equal t1a t2a && equal t1b t2b
+   | Neg t1          , Neg t2           -> equal t1 t2
+   | Id id1          , Id id2           -> Identifier.equal id1 id2
+   | Var id1         , Var id2          -> Identifier.equal id1 id2
+   | _               , _                -> false
