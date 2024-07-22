50333ac443d001afefd634aa9fc29ab9db307cdd
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-07-22 21:51:17 +0200

    cleanup

--- nanosail/NanosailToMicrosail/Types/Variants.ml
+++ nanosail/NanosailToMicrosail/Types/Variants.ml
@@ -1,6 +1,5 @@
 open Base
 open PPrint
-open Identifier
 open Monads.Notations.Star(AnnotationContext)
 
 module AC = AnnotationContext
@@ -16,7 +15,7 @@ let generate_inductive_type (variant_definition : Ast.Definition.Type.Variant.t)
   in
   let inductive_type =
     let identifier' =
-      pp_identifier identifier
+      Identifier.pp_identifier identifier
     in
     let pp_constructor_types (field_nanotypes : Ast.Type.t list) =
       let* ts = AC.map ~f:Nanotype.coq_type_of_nanotype field_nanotypes
@@ -29,7 +28,7 @@ let generate_inductive_type (variant_definition : Ast.Definition.Type.Variant.t)
       AC.map type_quantifier ~f:(fun (id, kind) ->
           let* kind' = PPSail.pp_kind kind
           in
-          AC.return (pp_identifier id, kind')
+          AC.return (Identifier.pp_identifier id, kind')
         )
     in
     Coq.build_inductive_type
@@ -41,20 +40,20 @@ let generate_inductive_type (variant_definition : Ast.Definition.Type.Variant.t)
         AC.iter constructors ~f:(fun (constructor, typ) ->
             let* typ' = pp_constructor_types typ
             in
-            add_constructor ~typ:typ' (pp_identifier constructor))
+            add_constructor ~typ:typ' (Identifier.pp_identifier constructor))
       end
   in
   inductive_type
 
 
 let generate_constructors_inductive_type (variant_definition  : Ast.Definition.Type.Variant.t) =
-  let identifier = pp_identifier @@ TranslationSettings.derive_variant_constructor_type variant_definition.identifier
-  and typ = pp_identifier @@ Ast.Identifier.mk "Set"
+  let identifier = Identifier.pp_identifier @@ TranslationSettings.derive_variant_constructor_type variant_definition.identifier
+  and typ = Identifier.pp_identifier @@ Ast.Identifier.mk "Set"
   and constructor_names = List.map ~f:fst variant_definition.constructors
   in
   Coq.build_inductive_type identifier typ (fun add_constructor ->
       AC.iter constructor_names
-        ~f:(fun case -> add_constructor @@ pp_identifier @@ TranslationSettings.convert_constructor_name_to_tag case)
+        ~f:(fun case -> add_constructor @@ Identifier.pp_identifier @@ TranslationSettings.convert_constructor_name_to_tag case)
     )
 
 
@@ -77,7 +76,7 @@ let generate_tags (variant_definitions : (Sail.sail_definition * Ast.Definition.
   and tag_of_variant (variant_definition : Ast.Definition.Type.Variant.t) =
     let id = TranslationSettings.convert_variant_name_to_tag variant_definition.identifier
     in
-    pp_identifier id
+    Identifier.pp_identifier id
   in
   let inductive_type =
     Coq.build_inductive_type
