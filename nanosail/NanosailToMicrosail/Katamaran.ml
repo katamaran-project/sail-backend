026ddc2ef816b01fd0ce215a5d6a5b79222e2288
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-07-24 15:25:30 +0200

    refactoring

--- nanosail/NanosailToMicrosail/Katamaran.ml
+++ nanosail/NanosailToMicrosail/Katamaran.ml
@@ -35,8 +35,9 @@ let pp_program_module
     contents
 
 
-let pretty_print ir =
-  let type_definitions     = Ast.(select Extract.(type_definition of_anything) ir.definitions)
+let pretty_print (ir : Ast.program) =
+  let definitions          = ir.definitions
+  and type_definitions     = Ast.(select Extract.(type_definition of_anything) ir.definitions)
   and enum_definitions     = Ast.(select Extract.(type_definition of_enum    ) ir.definitions)
   and record_definitions   = Ast.(select Extract.(type_definition of_record  ) ir.definitions)
   and variant_definitions  = Ast.(select Extract.(type_definition of_variant ) ir.definitions)
@@ -104,6 +105,36 @@ let pretty_print ir =
   let pp_finite =
     block [%here] "Finite" begin
       Finite.generate ir.definitions
+    end
+  in
+
+  let pp_finite =
+    let finite_enums =
+      let enum_definitions = Ast.(select Extract.(type_definition of_enum) definitions)
+      in
+      List.map ~f:(Auxlib.uncurry Types.Enums.generate_enum_finiteness) enum_definitions
+    and finite_registers =
+      let register_definitions = Ast.(select Extract.register_definition definitions)
+      in
+      Registers.generate_register_finiteness register_definitions
+    in
+    let finite_definitions =
+      Auxlib.build_list @@
+      fun { addall; add; _ } -> begin
+        addall finite_enums;
+        add    finite_registers;
+      end
+    in
+    let parts =
+      Auxlib.build_list @@
+      fun { add; addall; _ } -> begin
+        add    @@ Coq.imports [ "stdpp.finite" ];
+        add    @@ Coq.local_obligation_tactic (Ast.Identifier.mk "finite_from_eqdec");
+        addall @@ finite_definitions;
+      end
+    in
+    block [%here] "Finite" begin
+      Coq.section (Ast.Identifier.mk "Finite") @@ PP.(separate (twice hardline) parts)
     end
   in
 
