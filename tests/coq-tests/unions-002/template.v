da78900ff36b2339b6b5c77a9f311e7c7cc43152
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-07-25 20:51:32 +0200

    unions-002

--- tests/coq-tests/unions-002/template.v
+++ tests/coq-tests/unions-002/template.v
@@ -2,6 +2,11 @@
   (full-translation)
 >*)
 
+Theorem test : forall instr : instruction, exists x : Z, exists y : Z, instr = Pop x y.
+Proof.
+  destruct instr as [a b]; exists a; exists b; trivial.
+Qed.
+
 (*
 
 (*<
