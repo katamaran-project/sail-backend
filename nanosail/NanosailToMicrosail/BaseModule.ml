58c28ae385d7687bfd3e6d3d6930af222b70dd77
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-07-29 19:32:33 +0200

    improved generation

--- nanosail/NanosailToMicrosail/BaseModule.ml
+++ nanosail/NanosailToMicrosail/BaseModule.ml
@@ -429,7 +429,7 @@ let pp_union_fold (variant_definitions : Ast.Definition.Type.Variant.t list) : P
         in
         let field_variables =
           let generate_identifier index =
-            PP.string @@ Printf.sprintf "x%d" index
+            Identifier.pp @@ Configuration.tag_as_generated @@ Ast.Identifier.mk @@ Int.to_string index
           and indices =
             let n_fields = List.length constructor_field_types
             in
@@ -517,7 +517,7 @@ let pp_union_unfold (variant_definitions : Ast.Definition.Type.Variant.t list) :
       let constructor_case_handler (constructor_identifier, field_types) : (PP.document * PP.document) AC.t =
         let field_names =
           let generate_identifier index =
-            PP.string @@ Printf.sprintf "x%d" index
+            Identifier.pp @@ Configuration.tag_as_generated @@ Ast.Identifier.mk @@ Int.to_string index
           and indices =
             let n_fields = List.length field_types
             in
