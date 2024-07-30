bc4b7e958cbac657f467e3777187c8795bf705a6
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-07-30 18:46:00 +0200

    fixed bug in StringOf.Nanosail.nanotype

--- nanosail/StringOf/Nanosail.ml
+++ nanosail/StringOf/Nanosail.ml
@@ -1,5 +1,5 @@
 let identifier         = Ast.Identifier.string_of
-let nanotype           = Ast.Identifier.string_of
+let nanotype           = Ast.Type.to_string
 let type_argument      = Ast.TypeArgument.to_string
 let numeric_expression = Ast.Numeric.Expression.to_string
 let numeric_constraint = Ast.Numeric.Constraint.to_string
