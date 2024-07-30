7bfc5dfbb741c01aa9c86eed1e9fc1c4cc32921c
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-07-30 13:10:36 +0200

    coq-test/type-alias-of-union

---
+++ tests/coq-tests/type-alias-of-union/template.v
@@ -0,0 +1,14 @@
+(*<
+  (generate (full-translation))
+
+  (if (untranslated-definitions?)
+    (generate "\n\nUNTRANSLATED DEFINITIONS"))
+>*)
+
+(*
+
+(*<
+  (generate (untranslated-definitions))
+>*)
+
+*)
