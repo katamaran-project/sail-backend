507883f588e9a18eb8e65424012d998a01907bb5
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-07-31 10:13:30 +0200

    rename

--- nanosail/Configuration.ml
+++ nanosail/Configuration.ml
@@ -169,5 +169,5 @@ let should_ignore_definition (definition : Libsail.Type_check.tannot Libsail.Ast
 let tag_as_generated (id : Ast.Identifier.t) =
   Ast.Identifier.add_prefix "ж" id
 
-let tag_as_type_alias (id : Ast.Identifier.t) =
+let tag_as_reified (id : Ast.Identifier.t) =
   Ast.Identifier.add_prefix "ty_" id
--- nanosail/NanosailToMicrosail/Types/TypeAbbreviations.ml
+++ nanosail/NanosailToMicrosail/Types/TypeAbbreviations.ml
@@ -35,7 +35,7 @@ let generate (type_abbreviation : Ast.Definition.Type.Abbreviation.t) : PP.docum
         in
         AC.return @@ Coq.definition ~identifier ~parameters body;
       and* musail_alias =
-        let tagged_identifier = Configuration.tag_as_type_alias identifier
+        let tagged_identifier = Configuration.tag_as_reified identifier
         in
         let  identifier = Identifier.pp tagged_identifier
         in
