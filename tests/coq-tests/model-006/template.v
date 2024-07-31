7321a2033a90531cefd29a674ea88cabde0ea985
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-07-31 18:08:16 +0200

    model-006

---
+++ tests/coq-tests/model-006/template.v
@@ -0,0 +1,16 @@
+(*<
+  (generate (full-translation))
+
+  (if (untranslated-definitions?)
+    (generate "\n\nUNTRANSLATED DEFINITIONS"))
+>*)
+
+(*
+
+  UNTRANSLATED DEFINITIONS
+
+(*<
+  (generate (untranslated-definitions))
+>*)
+
+*)
