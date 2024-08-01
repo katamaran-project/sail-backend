f15ea2bff435446f94319839a34d698c54515304
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-01 17:23:51 +0200

    fix

--- tests/coq-tests/enums-001/template.v
+++ tests/coq-tests/enums-001/template.v
@@ -2,7 +2,7 @@
   (generate (full-translation))
 
   (if (untranslated-definitions?)
-    (generate "\n\nTheorem UNTRANSLATED_DEFINITIONS : False. Qed.")
+    (generate "\n\nTheorem UNTRANSLATED_DEFINITIONS : False. Qed."))
 >*)
 
 (*
