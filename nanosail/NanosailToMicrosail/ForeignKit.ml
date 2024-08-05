dbd57eab40176f8b5f98d6e0c333cd31816555d6
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-05 18:49:31 +0200

    refactoring

--- nanosail/NanosailToMicrosail/ForeignKit.ml
+++ nanosail/NanosailToMicrosail/ForeignKit.ml
@@ -6,13 +6,13 @@ module GC = GenerationContext
 let pp_foreign_kit () : PP.document GC.t =
   let title = Ast.Identifier.mk "ForeignKit"
   and contents =
-    PP.(separate_map hardline utf8string [
+    PP.vertical_strings [
       "Definition Memory : Set := unit.";
       "Definition ForeignCall {σs σ} (f : 𝑭𝑿 σs σ) (args : NamedEnv Val σs)";
       "  (res : string + Val σ) (γ γ' : RegStore) (μ μ' : Memory) : Prop := False.";
       "Lemma ForeignProgress {σs σ} (f : 𝑭𝑿 σs σ) (args : NamedEnv Val σs) γ μ :";
       "  exists γ' μ' res, ForeignCall f args res γ γ' μ μ'.";
       "Proof. destruct f. Qed."
-    ])
+    ]
   in
   GC.return @@ Coq.section title contents
