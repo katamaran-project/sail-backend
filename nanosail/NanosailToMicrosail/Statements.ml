d62f9d98dd12e9256f103852dd6afbbebba292f8
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-05 17:47:53 +0200

    GC Statements

--- nanosail/NanosailToMicrosail/Statements.ml
+++ nanosail/NanosailToMicrosail/Statements.ml
@@ -1,16 +1,16 @@
 open Base
-open Monads.Notations.Star(AnnotationContext)
+open Monads.Notations.Star(GenerationContext)
 
-module AC = AnnotationContext
+module GC = GenerationContext
 
 
-let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
-  let pp_expression_statement (expression : Ast.Expression.t) : PPrint.document AC.t =
+let rec pp_statement (statement : Ast.Statement.t) : PPrint.document GC.t =
+  let pp_expression_statement (expression : Ast.Expression.t) : PPrint.document GC.t =
     let* expression' = Expressions.pp_par_expression expression
     in
-    AC.return @@ PP.(simple_app [string "stm_exp"; expression'])
+    GC.return @@ PP.(simple_app [string "stm_exp"; expression'])
 
-  and pp_match_statement (match_pattern : Ast.Statement.match_pattern) : PPrint.document AC.t =
+  and pp_match_statement (match_pattern : Ast.Statement.match_pattern) : PPrint.document GC.t =
     match match_pattern with
     | List { matched; when_nil; when_cons } -> begin
         let id_head, id_tail, when_cons_body = when_cons
@@ -19,7 +19,7 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
         and* when_nil'  = pp_par_statement when_nil
         and* when_cons' = pp_par_statement when_cons_body
         in
-        AC.return @@ PP.(simple_app [
+        GC.return @@ PP.(simple_app [
             string "stm_match_list";
             matched';
             when_nil';
@@ -33,7 +33,7 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
         let* matched' = pp_par_statement matched
         and* body'    = pp_par_statement body
         in
-        AC.return @@ PP.(simple_app [
+        GC.return @@ PP.(simple_app [
             string "stm_match_prod";
             matched';
             dquotes (Identifier.pp id_fst);
@@ -47,7 +47,7 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
         and* when_true'  = pp_par_statement when_true
         and* when_false' = pp_par_statement when_false
         in
-        AC.return @@ PP.(simple_app [
+        GC.return @@ PP.(simple_app [
             string "stm_if";
             condition';
             when_true';
@@ -56,12 +56,12 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
       end
 
     | Enum { matched; matched_type; cases } -> begin
-        let translate_case ~(key : Ast.Identifier.t) ~(data : Ast.Statement.t) (acc : PP.document list AC.t) =
+        let translate_case ~(key : Ast.Identifier.t) ~(data : Ast.Statement.t) (acc : PP.document list GC.t) =
           let* acc
-          and* pattern = AC.return @@ Identifier.pp key
+          and* pattern = GC.return @@ Identifier.pp key
           and* clause = pp_statement data
           in
-          AC.return @@ PP.(separate space [
+          GC.return @@ PP.(separate space [
               string "|";
               pattern;
               string " => ";
@@ -69,12 +69,12 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
             ] :: acc)
         in
         let* matched' = pp_par_statement matched
-        and* cases' = Ast.Identifier.Map.fold cases ~init:(AC.return []) ~f:translate_case
+        and* cases' = Ast.Identifier.Map.fold cases ~init:(GC.return []) ~f:translate_case
         in
         let matched_type =
           Identifier.pp @@ Identifier.reified_enum_name matched_type
         in
-        AC.return @@ PP.separate PP.hardline @@ Auxlib.build_list @@ fun { add; addall; _ } -> begin
+        GC.return @@ PP.separate PP.hardline @@ Auxlib.build_list @@ fun { add; addall; _ } -> begin
           add @@ PP.(separate space [ string "match:"; matched'; string "in"; matched_type; string "with" ]);
           addall cases'
         end
@@ -84,18 +84,18 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
         let _ = matched
         and _ = cases
         in
-        AC.not_yet_implemented [%here]
+        GC.not_yet_implemented [%here]
       end
 
   and pp_call_statement
       (function_identifier : Ast.Identifier.t     )
-      (arguments           : Ast.Expression.t list) : PPrint.document AC.t
+      (arguments           : Ast.Expression.t list) : PPrint.document GC.t
     =
-    let* pretty_printed_arguments = AC.map ~f:Expressions.pp_par_expression arguments
+    let* pretty_printed_arguments = GC.map ~f:Expressions.pp_par_expression arguments
     in
     FunctionCalls.translate function_identifier pretty_printed_arguments
 
-  and pp_let_statement (let_data : Ast.Statement.let_data) : PPrint.document AC.t
+  and pp_let_statement (let_data : Ast.Statement.let_data) : PPrint.document GC.t
     =
     let {
         variable_identifier;
@@ -106,13 +106,13 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
     in
     let  variable_identifier'    = Identifier.pp variable_identifier in
     let* binding_statement'      = pp_statement binding_statement
-    and* binding_statement_type' = Nanotype.pp_nanotype binding_statement_type
+    and* binding_statement_type' = Nanotype.pp_nanotype' binding_statement_type
     and* body_statement'         = pp_statement body_statement
     in
     if
       Configuration.(get pretty_print_let)
     then
-      AC.return @@ PP.(
+      GC.return @@ PP.(
         simple_app [
             separate space [
                 string "let:";
@@ -126,7 +126,7 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
           ]
         )
     else
-      AC.return @@ PP.(
+      GC.return @@ PP.(
         simple_app [
             string "stm_let";
             dquotes variable_identifier';
@@ -139,29 +139,29 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
 
   and pp_sequence_statement
       (left  : Ast.Statement.t)
-      (right : Ast.Statement.t) : PPrint.document AC.t
+      (right : Ast.Statement.t) : PPrint.document GC.t
     =
       let* left'  = pp_par_statement left
       and* right' = pp_par_statement right
       in
-      AC.return @@ PP.(simple_app [ string "stm_seq"; left'; right' ])
+      GC.return @@ PP.(simple_app [ string "stm_seq"; left'; right' ])
 
-  and pp_read_register_statement (register_identifier : Ast.Identifier.t) : PPrint.document AC.t =
-    AC.return @@ PP.(simple_app [ string "stm_read_register"; Identifier.pp register_identifier ])
+  and pp_read_register_statement (register_identifier : Ast.Identifier.t) : PPrint.document GC.t =
+    GC.return @@ PP.(simple_app [ string "stm_read_register"; Identifier.pp register_identifier ])
 
   and pp_write_register_statement
       (register_identifier : Ast.Identifier.t)
-      (rhs                 : Ast.Statement.t ) : PPrint.document AC.t
+      (rhs                 : Ast.Statement.t ) : PPrint.document GC.t
     =
     let* rhs' = pp_statement rhs
     in
-    AC.return @@ PP.simple_app [
+    GC.return @@ PP.simple_app [
       Identifier.pp @@ Ast.Identifier.mk "stm_write_register";
       Identifier.pp register_identifier;
       rhs'
     ]
 
-  and pp_destructure_record_statement (destructure_record : Ast.Statement.destructure_record) : PPrint.document AC.t =
+  and pp_destructure_record_statement (destructure_record : Ast.Statement.destructure_record) : PPrint.document GC.t =
     let {
       record_type_identifier;
       field_identifiers;
@@ -186,7 +186,7 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
     let* destructured_record' = pp_statement destructured_record
     and* body' = pp_statement body
     in
-    AC.return @@ PP.simple_app [
+    GC.return @@ PP.simple_app [
       Identifier.pp @@ Ast.Identifier.mk "stm_match_record";
       Identifier.pp record_type_identifier;
       PP.parens destructured_record';
@@ -196,13 +196,13 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
 
   and pp_cast_statement
       (statement_to_be_cast : Ast.Statement.t)
-      (_target_type         : Ast.Type.t     ) : PPrint.document AC.t
+      (_target_type         : Ast.Type.t     ) : PPrint.document GC.t
     =
     Stdio.printf "Warning: ignored cast\n";
     pp_statement statement_to_be_cast
 
-  and pp_fail_statement (message : string) : PPrint.document AC.t =
-    AC.return @@ PP.simple_app [ Identifier.pp @@ Ast.Identifier.mk "fail"; PP.string message ]
+  and pp_fail_statement (message : string) : PPrint.document GC.t =
+    GC.return @@ PP.simple_app [ Identifier.pp @@ Ast.Identifier.mk "fail"; PP.string message ]
 
   in
   match statement with
@@ -220,4 +220,4 @@ let rec pp_statement (statement : Ast.Statement.t) : PPrint.document AC.t =
 and pp_par_statement s =
   let* s' = pp_statement s
   in
-  AC.return @@ PP.parens s'
+  GC.return @@ PP.parens s'
