5465b3e27822093c6960d732d7f661896ebc097e
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-05 21:27:24 +0200

    refactoring

--- nanosail/NanosailToMicrosail/PPSail.ml
+++ nanosail/NanosailToMicrosail/PPSail.ml
@@ -1,9 +1,7 @@
 open Base
 open Monads.Notations.Star(GenerationContext)
 
-module GC = GenerationContext
 
 
-(* todo move this *)
 let pp_bind (arg, t) =
-  GC.return @@ PP.(utf8string ("\"" ^ (Ast.Identifier.string_of arg) ^ "\" ∷ " ) ^^ t)
+  PP.(utf8string ("\"" ^ (Ast.Identifier.string_of arg) ^ "\" ∷ " ) ^^ t)
