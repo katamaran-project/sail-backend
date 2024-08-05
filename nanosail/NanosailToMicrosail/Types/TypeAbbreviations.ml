3479fac78eaeae659cd9a303a12418485394239e
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-05 15:54:54 +0200

    GC TypeAbbreviation

--- nanosail/NanosailToMicrosail/Types/TypeAbbreviations.ml
+++ nanosail/NanosailToMicrosail/Types/TypeAbbreviations.ml
@@ -1,39 +1,39 @@
 open Base
-open Monads.Notations.Star(AnnotationContext)
+open Monads.Notations.Star(GenerationContext)
 
-module AC = AnnotationContext
+module GC = GenerationContext
 
 
-let generate (type_abbreviation : Ast.Definition.Type.Abbreviation.t) : PP.document AC.t =
+let generate (type_abbreviation : Ast.Definition.Type.Abbreviation.t) : PP.document GC.t =
   let { identifier; abbreviation } : Ast.Definition.Type.Abbreviation.t = type_abbreviation
   in
   match abbreviation with
   | NumericExpression (quantifier, numexpr) -> begin
       let  identifier  = Identifier.pp identifier
       in
-      let* body        = Numeric.Expression.pp numexpr
-      and* parameters  = PPSail.pp_type_quantifier quantifier
+      let* body        = Numeric.Expression.pp' numexpr
+      and* parameters  = PPSail.pp_type_quantifier' quantifier
       in
-      AC.return @@ Coq.definition ~identifier ~parameters body
+      GC.return @@ Coq.definition ~identifier ~parameters body
     end
 
   | NumericConstraint (quantifier, numconstraint) -> begin
       let  identifier  = Identifier.pp identifier
       in
-      let* body        = Numeric.Constraint.pp numconstraint
-      and* parameters  = PPSail.pp_type_quantifier quantifier
+      let* body        = Numeric.Constraint.pp' numconstraint
+      and* parameters  = PPSail.pp_type_quantifier' quantifier
       in
-      AC.return @@ Coq.definition ~identifier ~parameters body
+      GC.return @@ Coq.definition ~identifier ~parameters body
     end
 
   | Alias (quantifier, typ) -> begin
       let* coq_alias =
         let  identifier  = Identifier.pp identifier
         in
-        let* body        = Nanotype.coq_type_of_nanotype typ
-        and* parameters  = PPSail.pp_type_quantifier quantifier
+        let* body        = Nanotype.coq_type_of_nanotype' typ
+        and* parameters  = PPSail.pp_type_quantifier' quantifier
         in
-        AC.return @@ Coq.definition ~identifier ~parameters body;
+        GC.return @@ Coq.definition ~identifier ~parameters body;
       in
-      AC.return coq_alias
+      GC.return coq_alias
     end
