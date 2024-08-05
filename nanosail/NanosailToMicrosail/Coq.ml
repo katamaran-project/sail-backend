ab5a5b04832f82d7dcb14001033fbbdeb15b90b5
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-05 10:36:57 +0200

    todo

--- nanosail/NanosailToMicrosail/Coq.ml
+++ nanosail/NanosailToMicrosail/Coq.ml
@@ -142,6 +142,7 @@ let list (items : PP.document list) : PP.document =
     PP.(delimited_sequence (lbracket ^^ space) (space ^^ rbracket) semi items)
 
 
+(* todo rename to pp_product *)
 let product (v1 : PP.document) (v2 : PP.document) : PP.document =
   PP.(soft_surround 1 0 lparen (v1 ^^ comma ^^ break 1 ^^ v2) rparen)
 
