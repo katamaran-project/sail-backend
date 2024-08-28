d9d0b8af8b9ef7e4100aed2d322ccb8f87202e35
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-28 19:56:17 +0200

    comments

--- slang/Helpers.ml
+++ slang/Helpers.ml
@@ -2,15 +2,34 @@ open Monads.Notations.Star(EvaluationContext)
 
 
 module Function = struct
+  (*
+
+     Given an OCaml function func with type
+     
+       unit -> 'a EC.t
+     
+     convert it to an equivalent Slang function that takes no parameters and returns
+     the result of func () as a Slang value (e.g., OCaml strings are converted to Slang strings, etc.)
+
+
+     value_constructor
+       converts OCaml value to Slang value, see Value.Mk module
+     id
+       name of the function; used in error message
+     func
+       function running in evaluation context that returns OCaml value that
+       needs converting
+     
+   *)
   let to_type
       (value_constructor : 'a -> Value.t                 )
       (id                : string                        )
-      (f                 : unit -> 'a EvaluationContext.t)
+      (func              : unit -> 'a EvaluationContext.t)
     =
     let slang_function (arguments : Value.t list) =
       match arguments with
       | [] -> begin
-          let* result = f ()
+          let* result = func ()
           in
           EvaluationContext.return @@ value_constructor result
         end
