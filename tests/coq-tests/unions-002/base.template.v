4c726971e092dc29f26bbd4b3999bb1abeeb16b9
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-30 17:13:46 +0200

    ./unions-002

--- tests/coq-tests/unions-002/base.template.v
+++ tests/coq-tests/unions-002/base.template.v
@@ -1,14 +1,16 @@
 (*<
-  (generate (full-translation))
-
-  (if (untranslated-definitions?)
-    (generate "\n\nTheorem UNTRANSLATED_DEFINITIONS : False. Qed."))
+  (generate (base-translation))
 >*)
 
 Theorem test : forall instr : instruction, exists x : Z, exists y : Z, instr = Pop x y.
 Proof.
   destruct instr as [a b]; exists a; exists b; trivial.
 Qed.
+
+(*<
+  (if (untranslated-definitions?)
+    (generate "Theorem UNTRANSLATED_DEFINITIONS : False. Qed."))
+>*)
 
 (*
 
