26b28698712a7300163db724485c6c3d3b4c777a
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-30 17:13:46 +0200

    ./value-definitions-002

--- tests/coq-tests/value-definitions-002/base.template.v
+++ tests/coq-tests/value-definitions-002/base.template.v
@@ -1,8 +1,5 @@
 (*<
-  (generate (full-translation))
-
-  (if (untranslated-definitions?)
-    (generate "\n\nTheorem UNTRANSLATED_DEFINITIONS : False. Qed."))
+  (generate (base-translation))
 >*)
 
 Theorem imm_size_correct : imm_size = 12.
@@ -11,6 +8,11 @@ Proof. trivial. Qed.
 
 Theorem imm_ex_size_correct : imm_ext_size = 20.
 Proof. trivial. Qed.
+
+(*<
+  (if (untranslated-definitions?)
+    (generate "Theorem UNTRANSLATED_DEFINITIONS : False. Qed."))
+>*)
 
 (*
 
