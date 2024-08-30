b0d91f9fb5b39400e55a59a5c61e502c38d1731f
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-30 12:19:33 +0200

    split code

--- nanosail/Templates/Templates.ml
+++ nanosail/Templates/Templates.ml
@@ -1,8 +1,8 @@
 open Base
 open Monads.Notations.Star(Slang.EvaluationContext)
-open Slang.Prelude.Shared
 
 module Blocks = Blocks
+module Prelude = Prelude
 
 module EC = Slang.EvaluationContext
 
@@ -18,123 +18,6 @@ let string_of_document document =
   in
   PPrint.ToBuffer.pretty 1.0 text_width buffer document;
   Stdlib.Buffer.contents buffer
-
-
-let template_prelude (translation : Ast.program) =
-  (* Allocate a refcell that holds a list of generated strings (in reverse order, for efficiency purposes) *)
-  let* generated_output_reference =
-    EC.heap_allocate Slang.Value.Nil
-  in
-
-  (* Adds the given string to the list of generated strings; not exported directly *)
-  let generate_string (str : string) =
-    let* current_output =
-      EC.heap_access generated_output_reference
-    in
-    let extended_output =
-      Slang.Value.Cons (Slang.Value.String str, current_output)
-    in
-    EC.heap_update generated_output_reference extended_output
-  in
-
-  (* Returns generated strings in concatenated form *)
-  let fetch_generated =
-    let* generated_string_list = EC.heap_access generated_output_reference
-    in
-    match Slang.Converters.(list string) generated_string_list with
-    | Some strings -> begin
-        EC.return @@ String.concat ~sep:"" @@ List.rev strings
-      end
-    | None -> failwith "Bug: somehow the list got corrupted"
-  in
-
-  let nullary_string_function id func =
-    (id, Slang.Helpers.Function.to_string id func)
-  and nullary_boolean_function id func =
-    (id, Slang.Helpers.Function.to_bool id func)
-  in
-
-  (* (generate string1 string2 ...) *)
-  let exported_generate =
-    let id = "generate"
-    and impl args =
-      let=?? strings = List.map ~f:Slang.Converters.string args
-      in
-      let* () = EC.iter ~f:generate_string strings
-      in
-      EC.return (Some Slang.Value.Nil)
-    in
-      (id, Slang.Value.Callable (Slang.Functions.mk_multimethod [ impl; error id ]))
-  in
-
-  let exported_full_translation =
-    let id = "full-translation"
-    in
-    let f () =
-      EC.return @@ string_of_document @@ NanosailToMicrosail.Katamaran.full_translation translation
-    in
-    nullary_string_function id f
-  in
-
-  let exported_ignored_definitions =
-    let id = "ignored-definitions"
-    in
-    let f () =
-      let ignored_definitions =
-        List.map ~f:fst @@ Ast.Definition.Select.(select ignored_definition translation.definitions)
-      in
-      let formatted_ignored_definitions =
-        (* todo improve this *)
-        let open Monads.Notations.Star(GC)
-        in
-        let result =
-          let* ignored_definitions' =
-            GC.map ~f:NanosailToMicrosail.Ignored.generate ignored_definitions
-          in
-          GC.return @@ PP.vertical ~separator:PP.(twice hardline) ignored_definitions'
-        in
-        GC.generate result
-      in
-      EC.return @@ string_of_document formatted_ignored_definitions
-    in
-    nullary_string_function id f
-  in
-
-  let exported_untranslated_definitions =
-    let id = "untranslated-definitions"
-    in
-    let f () =
-      let untranslated_definitions =
-        Ast.Definition.Select.(select untranslated_definition translation.definitions)
-      in
-      let formatted_untranslated_definitions =
-        PPrint.(separate (twice hardline) @@ List.map ~f:(Auxlib.uncurry NanosailToMicrosail.Untranslated.generate) untranslated_definitions)
-      in
-      EC.return @@ string_of_document formatted_untranslated_definitions
-    in
-    nullary_string_function id f
-  in
-
-  let exported_untranslated_definitions_predicate =
-    let id = "untranslated-definitions?"
-    in
-    let f () =
-      EC.return @@ not @@ List.is_empty @@ Ast.Definition.Select.(select untranslated_definition translation.definitions)
-    in
-    nullary_boolean_function id f
-  in
-
-  let exported : (string * Slang.Value.t) list = [
-    exported_generate;
-    exported_full_translation;
-    exported_ignored_definitions;
-    exported_untranslated_definitions;
-    exported_untranslated_definitions_predicate;
-  ]
-  in
-  let* () = EC.iter exported ~f:(fun (id, callable) -> EC.add_binding id callable)
-  in
-  EC.return fetch_generated
 
 
 let is_template_block_start line =
@@ -156,7 +39,7 @@ let run_code
       (source      : string     ) : string =
   let program =
     let* () = Slang.Prelude.initialize
-    and* fetch_generated = template_prelude translation
+    and* fetch_generated = Prelude.prelude translation
     in
     let* _ = Slang.Evaluation.evaluate_string source
     in
