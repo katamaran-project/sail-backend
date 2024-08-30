9ec468a8ea4298ccc6e3192bc3b70fe6e4aefee2
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-08-30 19:23:36 +0200

    Logging.surround

--- nanosail/Logging.ml
+++ nanosail/Logging.ml
@@ -10,18 +10,18 @@ let info message =
 
 
 (* todo move to better place *)
-let try_finally
-    (f       : unit -> 'a  )
-    (finally : unit -> unit) : 'a =
-  try
-    let result = f ()
-    in
-    finally ();
-    result
-  with e -> begin
-      finally ();
-      raise e
-    end
+(* let try_finally *)
+(*     (f       : unit -> 'a  ) *)
+(*     (finally : unit -> unit) : 'a = *)
+(*   try *)
+(*     let result = f () *)
+(*     in *)
+(*     finally (); *)
+(*     result *)
+(*   with e -> begin *)
+(*       finally (); *)
+(*       raise e *)
+(*     end *)
 
 
 let surround
@@ -31,9 +31,18 @@ let surround
   =
   let enter_block () =
     logger @@ Printf.sprintf " IN %s" caption;
-  and exit_block () =
+  and exited_block_successfully () =
     logger @@ Printf.sprintf "OUT %s" caption;
+  and exited_block_with_exception () =
+    logger @@ Printf.sprintf "XXX %s" caption;
   in
   enter_block ();
-  try_finally f exit_block      
-
+  try
+    let result = f ()
+    in
+    exited_block_successfully ();
+    result
+  with e -> begin
+      exited_block_with_exception ();
+      raise e
+    end
