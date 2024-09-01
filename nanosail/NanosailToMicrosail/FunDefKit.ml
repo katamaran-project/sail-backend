8f2e1ff23e414d02e84d6f631dcfea2e79e22cd3
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-01 19:42:06 +0200

    fixed bug in FunDefKit

--- nanosail/NanosailToMicrosail/FunDefKit.ml
+++ nanosail/NanosailToMicrosail/FunDefKit.ml
@@ -11,53 +11,55 @@ let pp_function_definition
       ((sail_function_definition : Sail.sail_definition), (function_definition : Ast.Definition.Function.t))
       (type_constraint           : (Sail.sail_definition * 'a) option                                      ) : PP.document GC.t
   =
-  let identifier = Identifier.pp @@ Ast.Identifier.add_prefix "fun_" function_definition.function_name
-  in
-  let* coq_definition =
-    let* result_type =
-      let* bindings =
-        let* parameters : (Ast.Identifier.t * PP.document) list =
-          let pp (id : Ast.Identifier.t) (t : Ast.Type.t) =
-            let* t' = Nanotype.pp_nanotype t
-            in
-            GC.return (id, t')
-          in
-          GC.map ~f:(Auxlib.uncurry pp) function_definition.function_type.parameters
-        in
-        let docs = List.map ~f:PPSail.pp_bind parameters
-        in
-        GC.return @@ Coq.pp_list docs
-      in
-      let* result_type =
-        Nanotype.pp_nanotype function_definition.function_type.return_type
-      in
-      GC.return @@ Some (
-        PP.hanging_list (PP.string "Stm") [
-          bindings;
-          PP.parens result_type
-        ]
-      )
-    in
-    let* body =
-      Statements.pp_statement function_definition.function_body
-    in
-    let* extended_function_type' = Types.ExtendedType.pp_extended_function_type function_definition.function_type function_definition.extended_function_type
-    in
-    let* () = GC.add_comment extended_function_type'
-    in
-    GC.return @@ Coq.pp_definition ~identifier ~result_type body
-  in
-  let original_sail_code =
-    Auxlib.build_list (fun { add; _ } ->
-        (
-          match type_constraint with
-          | Some (sail_type_constraint, _) -> add sail_type_constraint
-          | None                           -> ()
-        );
-        add sail_function_definition;
-      )
-  in
   GC.block begin
+    let* () = GC.log Logging.debug @@ Printf.sprintf "Generating code for function %s" (StringOf.Nanosail.identifier function_definition.function_name)
+    in
+    let identifier = Identifier.pp @@ Ast.Identifier.add_prefix "fun_" function_definition.function_name
+    in
+    let* coq_definition =
+      let* result_type =
+        let* bindings =
+          let* parameters : (Ast.Identifier.t * PP.document) list =
+            let pp (id : Ast.Identifier.t) (t : Ast.Type.t) =
+              let* t' = Nanotype.pp_nanotype t
+              in
+              GC.return (id, t')
+            in
+            GC.map ~f:(Auxlib.uncurry pp) function_definition.function_type.parameters
+          in
+          let docs = List.map ~f:PPSail.pp_bind parameters
+          in
+          GC.return @@ Coq.pp_list docs
+        in
+        let* result_type =
+          Nanotype.pp_nanotype function_definition.function_type.return_type
+        in
+        GC.return @@ Some (
+          PP.hanging_list (PP.string "Stm") [
+            bindings;
+            PP.parens result_type
+          ]
+        )
+      in
+      let* body =
+        Statements.pp_statement function_definition.function_body
+      in
+      let* extended_function_type' = Types.ExtendedType.pp_extended_function_type function_definition.function_type function_definition.extended_function_type
+      in
+      let* () = GC.add_comment extended_function_type'
+      in
+      GC.return @@ Coq.pp_definition ~identifier ~result_type body
+    in
+    let original_sail_code =
+      Auxlib.build_list (fun { add; _ } ->
+          (
+            match type_constraint with
+            | Some (sail_type_constraint, _) -> add sail_type_constraint
+            | None                           -> ()
+          );
+          add sail_function_definition;
+        )
+    in
     let* () = GC.add_original_definitions original_sail_code
     in
     GC.return @@ coq_definition
@@ -68,6 +70,8 @@ let pp_function_definitions
       (function_definitions : (Sail.sail_definition * Ast.Definition.Function.t) list)
       (top_level_type_constraint_definitions : (Sail.sail_definition * Ast.Definition.top_level_type_constraint_definition) list) : PP.document list GC.t
   =
+  let* () = GC.log Logging.debug "Generating translations for all functions"
+  in
   let type_and_function_pairs =
     let find_type_constraint function_name =
       match
@@ -90,6 +94,8 @@ let pp_function_definition_kit
       (function_definitions                  : (Sail.sail_definition * Ast.Definition.Function.t) list                          )
       (top_level_type_constraint_definitions : (Sail.sail_definition * Ast.Definition.top_level_type_constraint_definition) list) : PP.document GC.t
   =
+  let* () = GC.log Logging.debug "Generation FunDefKit"
+  in
   let fundef =
     let identifier = Identifier.pp @@ Ast.Identifier.mk "FunDef"
     and implicit_parameters = [
@@ -130,4 +136,4 @@ let pp_function_definition_kit
   let section =
     Coq.pp_section (Ast.Identifier.mk "FunDefKit") contents
   in
-  GC.return section
+  GC.return @@ section
