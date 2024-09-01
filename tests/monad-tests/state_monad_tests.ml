eb9c96b581b48a516a83b4445a8226efcb7f0efc
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-01 18:56:18 +0200

    state monad test for act

--- tests/monad-tests/state_monad_tests.ml
+++ tests/monad-tests/state_monad_tests.ml
@@ -14,6 +14,7 @@ let increment =
   in
   put @@ n + 1
 
+
 let get_and_inc =
   let open IntState
   in
@@ -31,7 +32,29 @@ let test_collect =
     assert_equal result [0; 1; 2; 3; 4];
     assert_equal state 5
 
+
+let test_act =
+  let open IntState
+  in
+  let flag = ref false
+  in
+  let set_flag () =
+    flag := true
+  in  
+  "act" >:: fun _ ->
+    let monadic =
+      let* () = act set_flag
+      in
+      return 0
+    in
+    assert_equal false (!flag);
+    let _ = IntState.run monadic 0
+    in
+    assert_equal true (!flag)
+      
+
 let test_suite =
   "state monad test suite" >::: [
-    test_collect
+    test_collect;
+    test_act;
   ]
