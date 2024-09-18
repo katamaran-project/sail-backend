af389258215b801e01e19c2ecba361a5f6ad8723
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-18 14:55:37 +0200

    test.sh outputs number of successes and failures

--- tests/coq-tests/test.sh
+++ tests/coq-tests/test.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+pass_count=0
+fail_count=0
+
 for dir in `find -mindepth 1 -maxdepth 1 -type d | sort`; do
     (
         cd $dir;
         echo "Testing $(basename `pwd`)";
-        ./test.sh;
+        ./test.sh > /dev/null 2> /dev/null;
         if [ $? != 0 ]; then
-            echo "FAILED $?";
-            exit -1
+            echo "FAILED $dir";
+            fail_count=$((fail_count+1));
+        else
+            pass_count=$((pass_count+1));
         fi
     )
 done
+
+
+echo PASS $pass_count
+echo FAIL $fail_count
