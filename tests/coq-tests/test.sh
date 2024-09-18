cb87bce22afd6d828e2e58affad16ff8fffc0f2b
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-18 15:02:49 +0200

    fixed test.sh

--- tests/coq-tests/test.sh
+++ tests/coq-tests/test.sh
@@ -4,19 +4,17 @@ pass_count=0
 fail_count=0
 
 for dir in `find -mindepth 1 -maxdepth 1 -type d | sort`; do
-    (
-        cd $dir;
-        echo "Testing $(basename `pwd`)";
-        ./test.sh > /dev/null 2> /dev/null;
-        if [ $? != 0 ]; then
-            echo "FAILED $dir";
-            fail_count=$((fail_count+1));
-        else
-            pass_count=$((pass_count+1));
-        fi
-    )
+    pushd $dir > /dev/null;
+    echo "Testing $(basename `pwd`)";
+    ./test.sh > /dev/null 2> /dev/null;
+    if [ $? != 0 ]; then
+        echo "FAILED $dir";
+        fail_count=$(($fail_count+1));
+    else
+        pass_count=$(($pass_count+1));
+    fi
+    popd > /dev/null;
 done
-
 
 echo PASS $pass_count
 echo FAIL $fail_count
