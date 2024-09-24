ef5275996de7958467432af5b4d3ef7627a82734
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-24 18:20:18 +0200

    module reorganization to avoid cyclical dependencies

--- nanosail/SailToNanosail/Translate/Translate.ml
+++ nanosail/SailToNanosail/Translate/Translate.ml
@@ -12,3 +12,4 @@ module TypeQuantifier         = TypeQuantifier
 module ValueDefinition        = ValueDefinition
 module Variant                = Variant
 module ExtendedType           = ExtendedType
+module Recursive              = Recursive
