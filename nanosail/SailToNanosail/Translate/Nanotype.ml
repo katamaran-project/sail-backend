f329c8a3c83297355e5c1712f7b66225d8fdb93d
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-24 18:03:04 +0200

    cleanup

--- nanosail/SailToNanosail/Translate/Nanotype.ml
+++ nanosail/SailToNanosail/Translate/Nanotype.ml
@@ -41,9 +41,9 @@ let rec nanotype_of_sail_type (S.Typ_aux (typ, location)) : Ast.Type.t TC.t =
             let _ = identifier (* todo remove this *)
             in
             match abbreviation with
-            | Ast.Definition.Type.Abbreviation.NumericExpression (_, _)     -> TC.not_yet_implemented [%here] location
-            | Ast.Definition.Type.Abbreviation.NumericConstraint (_, _)     -> TC.not_yet_implemented [%here] location
-            | Ast.Definition.Type.Abbreviation.Alias (type_quantifier, typ) -> begin
+            | NumericExpression (_, _)     -> TC.not_yet_implemented [%here] location
+            | NumericConstraint (_, _)     -> TC.not_yet_implemented [%here] location
+            | Alias (type_quantifier, typ) -> begin
                 match type_quantifier with
                 | []   -> TC.return @@ Ast.Type.Alias (identifier', typ)
                 | _::_ -> TC.not_yet_implemented [%here] location
