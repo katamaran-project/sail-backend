58440d4e25d97c1b18d9ed28a63311bb32e7dc43
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-25 15:54:36 +0200

    Revert "refactored test.sh"
    This reverts commit 04b9cb806fb9c411882f8e7d1391565f5d244129.

--- tests/coq-tests/test.sh
+++ tests/coq-tests/test.sh
@@ -1,30 +1,22 @@
-s#!/usr/bin/env bash
+#!/usr/bin/env bash
 
 pass_count=0
 fail_count=0
 
-
-run_test() {
-    dir=$1
-    
+for dir in `find -mindepth 1 -maxdepth 1 -type d | sort`; do
     pushd $dir > /dev/null;
     ./test.sh > /dev/null 2> /dev/null;
     if [ $? != 0 ]; then
-        echo "FAIL $dir";
+        echo "FAILED $dir";
         fail_count=$(($fail_count+1));
     else
-        echo "PASS $dir";
         pass_count=$(($pass_count+1));
     fi
     popd > /dev/null;
-}
-
-
-for dir in `find -mindepth 1 -maxdepth 1 -type d | sort`; do
-    run_test $dir
 done
 
-echo PASS $pass_count */
-echo FAIL $fail_count */
+echo PASS $pass_count
+echo FAIL $fail_count
+
 
 printf "%(%Y-%m-%d)T PASS:$pass_count FAIL:$fail_count\n" >> results.txt
