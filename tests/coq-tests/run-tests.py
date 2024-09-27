eebb7332a5944ef4523bf076e195c99619cb9faa
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-27 17:40:19 +0200

    run-tests.py

--- tests/coq-tests/run-tests.py
+++ tests/coq-tests/run-tests.py
@@ -5,36 +5,53 @@ from pathlib import Path
 import subprocess
 import psutil
 import time
+from datetime import date
 
 MAXIMUM_QUEUE_SIZE = 20
 
 
-def get_free_memory():
-    vm = psutil.virtual_memory()
-    return 100 - vm.percent
+pass_count = 0
+fail_count = 0
 
 
-def wait_for_test(directory, process):
+def run_test(directory_name, path):
+    script_path = path / 'test.sh'
+    return subprocess.Popen(script_path, cwd=str(path), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+def wait_for_test(directory_name, path, process):
+    global pass_count, fail_count
+    
     result = process.wait()
     if result == 0:
         print(f"PASS {directory}")
+        pass_count += 1
     else:
         print(f"FAIL {directory}")
+        fail_count += 1
 
 
-directories = sorted([Path(entry).absolute() for entry in os.listdir() if os.path.isdir(entry)])
+directory_names = sorted([entry for entry in os.listdir() if os.path.isdir(entry)])        
 
 queue = []
 
-for directory in directories:
+for directory_name in directory_names:
+    path = Path(directory_name).absolute()
+    
     if len(queue) >= MAXIMUM_QUEUE_SIZE:
-        directory, process = queue.pop(0)
-        wait_for_test(directory, process)
+        directory, path, process = queue.pop(0)
+        wait_for_test(directory, path, process)
 
-    script_path = directory / 'test.sh'
-    process = subprocess.Popen(script_path, cwd=str(directory), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    queue.append((directory, process))
+    process = run_test(directory_name, path)
+    queue.append((directory_name, path, process))
 
 
-for directory, process in queue:
-    wait_for_test(directory, process)
+for directory, path, process in queue:
+    wait_for_test(directory, path, process)
+
+with open('results.txt', 'a') as file:
+    today = date.today()
+    formatted_today = today.strftime('%Y-%m-%d')
+    print(f"{formatted_today} PASS:{pass_count} FAIL:{fail_count}", file=file)
+
+print(f"PASS:{pass_count} FAIL:{fail_count}")
