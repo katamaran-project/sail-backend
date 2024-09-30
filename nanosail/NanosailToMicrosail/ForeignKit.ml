566719703abbcd53d239fceaa2a026402794e2d5
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-30 19:00:14 +0200

    foreign kit generation block

--- nanosail/NanosailToMicrosail/ForeignKit.ml
+++ nanosail/NanosailToMicrosail/ForeignKit.ml
@@ -4,15 +4,17 @@ module GC = GenerationContext
 
 
 let pp_foreign_kit () : PP.document GC.t =
-  let title = Ast.Identifier.mk "ForeignKit"
-  and contents =
-    PP.vertical_strings [
-      "Definition Memory : Set := unit.";
-      "Definition ForeignCall {σs σ} (f : 𝑭𝑿 σs σ) (args : NamedEnv Val σs)";
-      "  (res : string + Val σ) (γ γ' : RegStore) (μ μ' : Memory) : Prop := False.";
-      "Lemma ForeignProgress {σs σ} (f : 𝑭𝑿 σs σ) (args : NamedEnv Val σs) γ μ :";
-      "  exists γ' μ' res, ForeignCall f args res γ γ' μ μ'.";
-      "Proof. destruct f. Qed."
-    ]
-  in
-  GC.return @@ Coq.pp_section title contents
+  GC.generation_block' [%here] (PP.string "Foreign kit") begin
+    let title = Ast.Identifier.mk "ForeignKit"
+    and contents =
+      PP.vertical_strings [
+        "Definition Memory : Set := unit.";
+        "Definition ForeignCall {σs σ} (f : 𝑭𝑿 σs σ) (args : NamedEnv Val σs)";
+        "  (res : string + Val σ) (γ γ' : RegStore) (μ μ' : Memory) : Prop := False.";
+        "Lemma ForeignProgress {σs σ} (f : 𝑭𝑿 σs σ) (args : NamedEnv Val σs) γ μ :";
+        "  exists γ' μ' res, ForeignCall f args res γ γ' μ μ'.";
+        "Proof. destruct f. Qed."
+      ]
+    in
+    GC.return @@ Coq.pp_section title contents
+  end
