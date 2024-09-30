0e2418306f7fcdb4c77e5c18f2b74617e29e98ba
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-30 17:58:57 +0200

    rename

--- nanosail/NanosailToMicrosail/Types/TypeAbbreviations.ml
+++ nanosail/NanosailToMicrosail/Types/TypeAbbreviations.ml
@@ -28,7 +28,7 @@ let pp_type_abbreviation (type_abbreviation : Ast.Definition.Type.Abbreviation.t
       end
 
     | Alias (quantifier, typ) -> begin
-        let* coq_alias =
+        let* pp_alias =
           let  identifier  = Identifier.pp identifier
           in
           let* body        = Nanotype.coq_type_of_nanotype typ
@@ -36,6 +36,6 @@ let pp_type_abbreviation (type_abbreviation : Ast.Definition.Type.Abbreviation.t
           in
           GC.return @@ Coq.pp_definition ~identifier ~parameters body;
         in
-        GC.return coq_alias
+        GC.return pp_alias
       end
   end
