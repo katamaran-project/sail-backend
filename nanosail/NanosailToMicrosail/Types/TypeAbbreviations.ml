0dc30a521339177cd6162385a6d812c3e2d26c43
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-09-30 17:38:10 +0200

    added generation block to type abbreviations

--- nanosail/NanosailToMicrosail/Types/TypeAbbreviations.ml
+++ nanosail/NanosailToMicrosail/Types/TypeAbbreviations.ml
@@ -5,35 +5,37 @@ module GC = GenerationContext
 
 
 let generate (type_abbreviation : Ast.Definition.Type.Abbreviation.t) : PP.document GC.t =
-  let { identifier; abbreviation } : Ast.Definition.Type.Abbreviation.t = type_abbreviation
-  in
-  match abbreviation with
-  | NumericExpression (quantifier, numexpr) -> begin
-      let  identifier  = Identifier.pp identifier
-      in
-      let* body        = Numeric.Expression.pp numexpr
-      and* parameters  = TypeQuantifier.pp_type_quantifier quantifier
-      in
-      GC.return @@ Coq.pp_definition ~identifier ~parameters body
-    end
-
-  | NumericConstraint (quantifier, numconstraint) -> begin
-      let  identifier  = Identifier.pp identifier
-      in
-      let* body        = Numeric.Constraint.pp numconstraint
-      and* parameters  = TypeQuantifier.pp_type_quantifier quantifier
-      in
-      GC.return @@ Coq.pp_definition ~identifier ~parameters body
-    end
-
-  | Alias (quantifier, typ) -> begin
-      let* coq_alias =
+  GC.generation_block' [%here] (PP.string "Type abbreviation") begin
+    let { identifier; abbreviation } : Ast.Definition.Type.Abbreviation.t = type_abbreviation
+    in
+    match abbreviation with
+    | NumericExpression (quantifier, numexpr) -> begin
         let  identifier  = Identifier.pp identifier
         in
-        let* body        = Nanotype.coq_type_of_nanotype typ
+        let* body        = Numeric.Expression.pp numexpr
         and* parameters  = TypeQuantifier.pp_type_quantifier quantifier
         in
-        GC.return @@ Coq.pp_definition ~identifier ~parameters body;
-      in
-      GC.return coq_alias
-    end
+        GC.return @@ Coq.pp_definition ~identifier ~parameters body
+      end
+
+    | NumericConstraint (quantifier, numconstraint) -> begin
+        let  identifier  = Identifier.pp identifier
+        in
+        let* body        = Numeric.Constraint.pp numconstraint
+        and* parameters  = TypeQuantifier.pp_type_quantifier quantifier
+        in
+        GC.return @@ Coq.pp_definition ~identifier ~parameters body
+      end
+
+    | Alias (quantifier, typ) -> begin
+        let* coq_alias =
+          let  identifier  = Identifier.pp identifier
+          in
+          let* body        = Nanotype.coq_type_of_nanotype typ
+          and* parameters  = TypeQuantifier.pp_type_quantifier quantifier
+          in
+          GC.return @@ Coq.pp_definition ~identifier ~parameters body;
+        in
+        GC.return coq_alias
+      end
+  end
