12198e013779aa2fb3c001ccd124c68653dd7cc7
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-10-02 20:24:17 +0200

    BinaryOperator.to_fexpr

--- nanosail/Ast/BinaryOperator.ml
+++ nanosail/Ast/BinaryOperator.ml
@@ -13,3 +13,21 @@ type t =
   | LessThan
   | GreaterThanOrEqualTo
   | GreaterThan
+
+
+let to_fexpr (operator : t) : FExpr.t =
+  match operator with
+   | Plus                 -> FExpr.mk_symbol "Plus"
+   | Times                -> FExpr.mk_symbol "Times"
+   | Minus                -> FExpr.mk_symbol "Minus"
+   | And                  -> FExpr.mk_symbol "And"
+   | Or                   -> FExpr.mk_symbol "Or"
+   | Pair                 -> FExpr.mk_symbol "Pair"
+   | Cons                 -> FExpr.mk_symbol "Cons"
+   | Append               -> FExpr.mk_symbol "Append"
+   | EqualTo              -> FExpr.mk_symbol "EqualTo"
+   | NotEqualTo           -> FExpr.mk_symbol "NotEqualTo"
+   | LessThanOrEqualTo    -> FExpr.mk_symbol "LessThanOrEqualTo"
+   | LessThan             -> FExpr.mk_symbol "LessThan"
+   | GreaterThanOrEqualTo -> FExpr.mk_symbol "GreaterThanOrEqualTo"
+   | GreaterThan          -> FExpr.mk_symbol "GreaterThan"
