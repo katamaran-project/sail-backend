634e3203c603ab57ce644bf6fdce12c06c20c742
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-10-02 17:07:41 +0200

    Value.to_fexpr

--- nanosail/Ast/Value.ml
+++ nanosail/Ast/Value.ml
@@ -16,3 +16,12 @@ let rec type_of_value (value : t) : Nanotype.t =
   | Int _         -> Int
   | String _      -> String
   | Prod (v1, v2) -> Tuple [type_of_value v1; type_of_value v2]
+
+
+let rec to_fexpr (value : t) : FExpr.t =
+  match value with
+   | Unit          -> FExpr.Application { head="Unit"; positional=[]; keyword=[] }
+   | Bool b        -> FExpr.Bool b
+   | Int n         -> FExpr.Integer (Z.to_int n)
+   | String s      -> FExpr.String s
+   | Prod (v1, v2) -> FExpr.Application { head="Pair"; positional=[to_fexpr v1; to_fexpr v2]; keyword=[] }
