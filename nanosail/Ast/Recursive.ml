83e964d7177cb9324566e24391aa8e73c8bc90dd
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-10-03 09:53:28 +0200

    NumericExpression.to_fexpr added prefix

--- nanosail/Ast/Recursive.ml
+++ nanosail/Ast/Recursive.ml
@@ -42,12 +42,12 @@ module NumericExpression = struct
   let rec to_fexpr (numeric_expression : t) : FExpr.t =
     match numeric_expression with
      | Constant n     -> FExpr.Integer (Z.to_int n)
-     | Add (e1, e2)   -> FExpr.mk_application ~positional:[to_fexpr e1; to_fexpr e2] "Add"
-     | Minus (e1, e2) -> FExpr.mk_application ~positional:[to_fexpr e1; to_fexpr e2] "Minus"
-     | Times (e1, e2) -> FExpr.mk_application ~positional:[to_fexpr e1; to_fexpr e2] "Times"
-     | Neg e          -> FExpr.mk_application ~positional:[to_fexpr e] "Neg"
-     | Id identifier  -> FExpr.mk_application ~positional:[Identifier.to_fexpr identifier] "Id"
-     | Var identifier -> FExpr.mk_application ~positional:[Identifier.to_fexpr identifier] "Var"
+     | Add (e1, e2)   -> FExpr.mk_application ~positional:[to_fexpr e1; to_fexpr e2] "NumExpr:Add"
+     | Minus (e1, e2) -> FExpr.mk_application ~positional:[to_fexpr e1; to_fexpr e2] "NumExpr:Minus"
+     | Times (e1, e2) -> FExpr.mk_application ~positional:[to_fexpr e1; to_fexpr e2] "NumExpr:Times"
+     | Neg e          -> FExpr.mk_application ~positional:[to_fexpr e] "NumExpr:Neg"
+     | Id identifier  -> FExpr.mk_application ~positional:[Identifier.to_fexpr identifier] "NumExpr:Id"
+     | Var identifier -> FExpr.mk_application ~positional:[Identifier.to_fexpr identifier] "NumExpr:Var"
 end
 
 
