17c007ccb7dcdf1ab5845621e4ea9592d038ba50
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-10-07 14:38:19 +0200

    pp_regdeclkit

--- nanosail/NanosailToMicrosail/Registers.ml
+++ nanosail/NanosailToMicrosail/Registers.ml
@@ -181,7 +181,7 @@ let pp_regdeclkit (register_definitions : (Sail.sail_definition * Ast.Definition
       pp_reg_finite register_names
     ]
     in
-    GC.return @@ PP.vertical ~separator:PP.(twice hardline) items
+    GC.return @@ PP.vertical @@ List.intersperse ~sep:(PP.string "") items
   in
   GC.return @@ Coq.pp_section (Ast.Identifier.mk "RegDeclKit") section_contents
 
