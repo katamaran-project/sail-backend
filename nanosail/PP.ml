9c9fe86a0f11af4385a0a9bc0827121136a3ad14
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-10-07 22:36:13 +0200

    PP.annotate

--- nanosail/PP.ml
+++ nanosail/PP.ml
@@ -79,3 +79,10 @@ let measure_height        = Doc.measure_height
 let pad_right             = Doc.pad_right
 let paragraphs            = Doc.paragraphs
 let pp_binary_operation   = Doc.pp_binary_operation
+
+
+let annotate
+    (ocaml_location : Lexing.position)
+    (document       : document       ) : document
+  =
+  Doc.annotate (StringOf.OCaml.position ocaml_location) document
