b3a8a7cf8b1e7f0232aaa4f8e298d2fedd75c7af
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-10-08 15:34:00 +0200

    ValueDefinitions annotations

--- nanosail/NanosailToMicrosail/ValueDefinitions.ml
+++ nanosail/NanosailToMicrosail/ValueDefinitions.ml
@@ -10,23 +10,23 @@ end
 
 let pp_value (value : Ast.Value.t) : PP.document GC.t =
   match value with
-  | Unit        -> GC.return @@ PP.(string "tt")
-  | Int n       -> GC.return @@ PP.(string @@ Z.to_string n)
+  | Unit        -> GC.return @@ PP.annotate [%here] @@ PP.(string "tt")
+  | Int n       -> GC.return @@ PP.annotate [%here] @@ PP.(string @@ Z.to_string n)
+  | Bool b      -> GC.return @@ PP.annotate [%here] @@ PP.string @@ if b then "true" else "false"
   | String _    -> GC.not_yet_implemented [%here]
   | Prod (_, _) -> GC.not_yet_implemented [%here]
-  | Bool b      -> GC.return @@ PP.string @@ if b then "true" else "false"
 
 
 let pp_value_definition (value_definition : Ast.Definition.value_definition) : PP.document GC.t =
   let { identifier; value } : Ast.Definition.value_definition = value_definition
   in
   let definition =
-    let identifier = Identifier.pp identifier
+    let identifier  = PP.annotate [%here] @@ Identifier.pp identifier
     and result_type = None
     in
     let* body = pp_value value
     in
-    GC.return @@ Coq.pp_definition ~identifier ~result_type body
+    GC.return @@ PP.annotate [%here] @@ Coq.pp_definition ~identifier ~result_type body
   in
   definition
 
@@ -38,4 +38,4 @@ let generate (definitions : (Sail.sail_definition * Ast.Definition.t) list) : PP
     in
     GC.map ~f:pp_value_definition value_definitions
   in
-  GC.return @@ PP.vertical coq_lines
+  GC.return @@ PP.annotate [%here] @@ PP.vertical coq_lines
