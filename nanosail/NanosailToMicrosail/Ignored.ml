0f3591d07e9a87f21030070f50edcc88e3d52f47
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-10-08 14:17:20 +0200

    Ignored annotations

--- nanosail/NanosailToMicrosail/Ignored.ml
+++ nanosail/NanosailToMicrosail/Ignored.ml
@@ -4,4 +4,4 @@ module GC = GenerationContext
 
 
 let generate (ignored_definition : Sail.sail_definition) : PP.document GC.t =
-  GC.return @@ PPSail.pp_sail_definition ignored_definition
+  GC.return @@ PP.annotate [%here] @@ PPSail.pp_sail_definition ignored_definition
