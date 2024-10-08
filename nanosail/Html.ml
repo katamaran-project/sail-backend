1ab24ccd43ab04fdf11103a0208d246addb904e6
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-10-08 11:08:06 +0200

    Html.t

--- nanosail/Html.ml
+++ nanosail/Html.ml
@@ -1,28 +1,38 @@
 open Base
 
 
-let escape_char (char : char) : string =
-  match char with
-  | '<' -> "&lt;"
-  | '>' -> "&gt;"
-  | '&' -> "&amp;"
-  | ' ' -> "&nbsp;"
-  | _   -> String.make 1 char
+(*
+  We use a separate type to represent HTML strings
+  This helps us prevent escaping the same string twice
+*)
+type t = Html of string
 
 
-let escape_string (string : string) : string =
+let to_string (Html s) =
+  s
+
+let escape_string (string : string) : t =
+  let escape_char (char : char) : string =
+    match char with
+    | '<' -> "&lt;"
+    | '>' -> "&gt;"
+    | '&' -> "&amp;"
+    | ' ' -> "&nbsp;"
+    | _   -> String.make 1 char
+  in
   let chars = String.to_list string
   in
   let escaped = List.map ~f:escape_char chars
   in
-  String.concat ~sep:"" escaped
+  Html (String.concat ~sep:"" escaped)
 
 
-let unordered_list (items : string list) : string =
-  let html_items =
-    let html_item item =
+let unordered_list (items : t list) : t =
+  let html_items : string =
+    let html_item (Html item) : string =
       Printf.sprintf "<li>%s</li>" item
     in
     String.concat ~sep:"" @@ List.map ~f:html_item items
   in
-  Printf.sprintf "<ul>%s</ul>" html_items
+  Html (Printf.sprintf "<ul>%s</ul>" html_items)
+
