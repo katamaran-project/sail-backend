4b9986a12b1e87b47c453cf1d058d4666c3e27ff
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-10-08 14:24:01 +0200

    Kind annotations

--- nanosail/NanosailToMicrosail/Kind.ml
+++ nanosail/NanosailToMicrosail/Kind.ml
@@ -7,5 +7,5 @@ module GC = GenerationContext
 let pp_kind (kind : Ast.Kind.t) : PP.document GC.t =
   match kind with
   | Type -> GC.not_yet_implemented [%here]
-  | Int  -> GC.return @@ PP.string @@ "nat"
+  | Int  -> GC.return @@ PP.annotate [%here] @@ PP.string @@ "nat"
   | Bool -> GC.not_yet_implemented [%here]
