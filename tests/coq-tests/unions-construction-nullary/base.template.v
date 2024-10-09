964e5c233a281c14e60b3b59bb548810b8e6921a
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-10-09 14:52:45 +0200

    fixed bug in tests

--- tests/coq-tests/unions-construction-nullary/base.template.v
+++ tests/coq-tests/unions-construction-nullary/base.template.v
@@ -2,11 +2,6 @@
   (generate (base-translation))
 >*)
 
-Theorem test : forall instr : instruction, exists x : Z, exists y : Z, instr = Pop x y.
-Proof.
-  destruct instr as [a b]; exists a; exists b; trivial.
-Qed.
-
 (*<
   (if (untranslated-definitions?)
     (generate "Theorem UNTRANSLATED_DEFINITIONS : False. Qed."))
