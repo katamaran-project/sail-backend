7491fb936a987ebfc9d74d0f08193c42ee735e53
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-10-10 16:20:39 +0200

    improved support for variant values

--- nanosail/NanosailToMicrosail/Expressions.ml
+++ nanosail/NanosailToMicrosail/Expressions.ml
@@ -228,6 +228,22 @@ let rec pp_expression (expression : Ast.Expression.t) : PP.document GC.t =
       match arguments with
       | [] -> GC.fail "Should not occur"
       | [argument] -> GC.lift ~f:PP.(surround parens) @@ pp_expression argument
+      | [argument1; argument2] -> begin
+          let* pp_argument1 =
+            pp_expression argument1
+          and* pp_argument2 =
+            pp_expression argument2
+          in
+          GC.return begin
+            Coq.pp_application
+              (PP.string "exp_binop")
+              [
+                PP.string "bop.pair";
+                PP.(surround parens) pp_argument1;
+                PP.(surround parens) pp_argument2;
+              ]
+          end
+        end
       | _   -> GC.not_yet_implemented [%here]
     in
     GC.return @@ Coq.pp_application
