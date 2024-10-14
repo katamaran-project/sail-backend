4f693daad3a327bff61303d887682b7901c9695a
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-10-14 11:22:41 +0200

    bits are now bits

--- nanosail/Ast/Value.ml
+++ nanosail/Ast/Value.ml
@@ -3,21 +3,21 @@ module Big_int       = Nat_big_num
 
 type t =
   | Unit
-  | Bool   of bool
-  | Int    of Big_int.num
-  | String of string
-  | Prod   of t * t
-  | Bit    of bool
+  | Bool      of bool
+  | Int       of Big_int.num
+  | String    of string
+  | Prod      of t * t
+  | Bit       of bool
 
 
 let rec type_of_value (value : t) : Nanotype.t =
   match value with
-  | Unit          -> Unit
-  | Bool _        -> Bool
-  | Int _         -> Int
-  | String _      -> String
-  | Prod (v1, v2) -> Tuple [type_of_value v1; type_of_value v2]
-  | Bit _         -> Int (* todo introduce separate Bit type? *)
+  | Unit          -> Nanotype.Unit
+  | Bool _        -> Nanotype.Bool
+  | Int _         -> Nanotype.Int
+  | String _      -> Nanotype.String
+  | Prod (v1, v2) -> Nanotype.Tuple [type_of_value v1; type_of_value v2]
+  | Bit _         -> Nanotype.Bit
 
 
 let rec to_fexpr (value : t) : FExpr.t =
