e8bc0a7e05cacb191ec8866f1bc4cc101a689df8
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-10-14 12:48:59 +0200

    added Bitvector value

--- nanosail/Ast/Value.ml
+++ nanosail/Ast/Value.ml
@@ -1,3 +1,5 @@
+open Base
+
 module Big_int       = Nat_big_num
 
 
@@ -8,6 +10,7 @@ type t =
   | String    of string
   | Prod      of t * t
   | Bit       of bool
+  | Bitvector of bool list
 
 
 let rec type_of_value (value : t) : Nanotype.t =
@@ -18,6 +21,7 @@ let rec type_of_value (value : t) : Nanotype.t =
   | String _      -> Nanotype.String
   | Prod (v1, v2) -> Nanotype.Tuple [type_of_value v1; type_of_value v2]
   | Bit _         -> Nanotype.Bit
+  | Bitvector bs  -> Nanotype.Bitvector (Numeric.Expression.Constant (Z.of_int @@ List.length bs))
 
 
 let rec to_fexpr (value : t) : FExpr.t =
@@ -28,3 +32,9 @@ let rec to_fexpr (value : t) : FExpr.t =
    | String s      -> FExpr.mk_string s
    | Prod (v1, v2) -> FExpr.mk_application ~positional:[to_fexpr v1; to_fexpr v2] "Prod"
    | Bit b         -> FExpr.mk_application ~positional:[FExpr.mk_bool b] "Bit"
+   | Bitvector bs  -> begin
+       let positional =
+         List.map ~f:(fun b -> FExpr.mk_int @@ if b then 1 else 0) bs
+       in
+       FExpr.mk_application ~positional "Bitvector"
+     end
