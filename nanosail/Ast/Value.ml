acf4457f83343637ed2b36207e58e33f6dc35bdf
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-10-14 10:39:07 +0200

    added Bit as possible value in Ast

--- nanosail/Ast/Value.ml
+++ nanosail/Ast/Value.ml
@@ -7,6 +7,7 @@ type t =
   | Int    of Big_int.num
   | String of string
   | Prod   of t * t
+  | Bit    of bool
 
 
 let rec type_of_value (value : t) : Nanotype.t =
@@ -16,6 +17,7 @@ let rec type_of_value (value : t) : Nanotype.t =
   | Int _         -> Int
   | String _      -> String
   | Prod (v1, v2) -> Tuple [type_of_value v1; type_of_value v2]
+  | Bit _         -> Int (* todo introduce separate Bit type? *)
 
 
 let rec to_fexpr (value : t) : FExpr.t =
@@ -25,3 +27,4 @@ let rec to_fexpr (value : t) : FExpr.t =
    | Int n         -> FExpr.mk_int @@ Z.to_int n
    | String s      -> FExpr.mk_string s
    | Prod (v1, v2) -> FExpr.mk_application ~positional:[to_fexpr v1; to_fexpr v2] "Prod"
+   | Bit b         -> FExpr.mk_application ~positional:[FExpr.mk_bool b] "Bit"
