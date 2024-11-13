ff5d5bc84d44415e2b9ba1077c931a7bd9644034
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-11-13 15:23:56 +0100

    location

---
+++ slang/Location.ml
@@ -0,0 +1,16 @@
+open Base
+
+
+class virtual location =
+  object (_self)
+    method virtual to_string : string
+  end
+
+
+class string_location (index : int) =
+  object (_self)
+    inherit location
+
+    method to_string =
+      Int.to_string index    
+  end
