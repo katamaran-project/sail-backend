4fbb738a5934fc5e5d64e64b087e01f0b29ace56
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-11-14 16:21:09 +0100

    Kind.to_fexpr

--- nanosail/Ast/Kind.ml
+++ nanosail/Ast/Kind.ml
@@ -9,3 +9,10 @@ let to_string (kind : t) =
   | Type -> "Kind.Type"
   | Int  -> "Kind.Int"
   | Bool -> "Kind.Bool"
+
+
+let to_fexpr (kind : t) : FExpr.t =
+  match kind with
+  | Type -> FExpr.mk_symbol "KindType"
+  | Int  -> FExpr.mk_symbol "KindInt"
+  | Bool -> FExpr.mk_symbol "KindBool"
