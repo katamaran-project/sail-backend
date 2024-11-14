32042da2f0e876085eb4f1a8c9f793bd989241ea
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-11-14 16:21:16 +0100

    TypeQuantifier.to_fexpr

--- nanosail/Ast/TypeQuantifier.ml
+++ nanosail/Ast/TypeQuantifier.ml
@@ -1,1 +1,19 @@
+open Base
+
+
 type t = TypeQuantifier of (Identifier.t * Kind.t) list
+
+
+let to_fexpr (type_quantifier : t) : FExpr.t =
+  let item_to_fexpr
+        (id   : Identifier.t)
+        (kind : Kind.t      ) : FExpr.t
+    =
+    FExpr.mk_list [ Identifier.to_fexpr id; Kind.to_fexpr kind ]
+  in
+  let TypeQuantifier items = type_quantifier
+  in
+  let positional =
+    List.map ~f:(Auxlib.uncurry item_to_fexpr) items
+  in
+  FExpr.mk_application ~positional "TypeQuantifier"
