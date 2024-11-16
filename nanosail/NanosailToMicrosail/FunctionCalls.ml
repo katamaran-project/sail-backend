5b73051e3bb558595c1f6101b1ff69ad2331fca8
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-16 16:03:17 +0100

    refactoring

--- nanosail/NanosailToMicrosail/FunctionCalls.ml
+++ nanosail/NanosailToMicrosail/FunctionCalls.ml
@@ -4,6 +4,37 @@ open Monads.Notations.Star(GenerationContext)
 module GC = GenerationContext
 
 
+
+let report_incorrect_argument_count
+    (original_function_name : Ast.Identifier.t )
+    (expected_operand_count : int              )
+    (operands               : PP.document list ) : PP.document GC.t
+  =
+  let message =
+    PP.annotate [%here] @@ PP.string @@ Printf.sprintf
+      "%s should receive %d argument(s) but instead received %d; falling back on default translation for function calls"
+      (Ast.Identifier.string_of original_function_name)
+      expected_operand_count
+      (List.length operands)
+  in
+  let* annotation_index =
+    GC.add_annotation message
+  in
+  let translation =
+    PP.annotate [%here] begin
+      MuSail.Statement.pp_call original_function_name operands
+    end
+  in
+  GC.return begin
+    PP.annotate [%here] begin
+      PP.(separate_horizontally ~separator:space [
+          translation;
+          Coq.pp_inline_comment (string @@ Int.to_string annotation_index)
+        ])
+    end
+  end
+
+
 let translate_as_unary_operator
     (original_function_name : Ast.Identifier.t )
     (operator               : string           )
@@ -21,30 +52,7 @@ let translate_as_unary_operator
         end
       end
     end
-  | _ -> begin
-      let message =
-        PP.annotate [%here] @@ PP.string @@ Printf.sprintf
-          "%s should receive 1 argument but instead received %d; falling back on default translation for function calls"
-          (Ast.Identifier.string_of original_function_name)
-          (List.length operands)
-      in
-      let* annotation_index =
-        GC.add_annotation message
-      in
-      let translation =
-        PP.annotate [%here] begin
-            MuSail.Statement.pp_call original_function_name operands
-          end
-      in
-      GC.return begin
-          PP.annotate [%here] begin
-              PP.(separate_horizontally ~separator:space [
-                      translation;
-                      Coq.pp_inline_comment (string @@ Int.to_string annotation_index)
-              ])
-            end
-        end
-    end
+  | _ -> report_incorrect_argument_count original_function_name 1 operands
 
 
 let translate_as_binary_operator_using_infix_notation
@@ -60,31 +68,7 @@ let translate_as_binary_operator_using_infix_notation
             end
         end
     end
-
-  | _      -> begin
-      let message =
-        PP.annotate [%here] @@ PP.string @@ Printf.sprintf
-          "%s should receive 2 arguments but instead received %d; falling back on default translation for function calls"
-          (Ast.Identifier.string_of original_function_name)
-          (List.length operands)
-      in
-      let* annotation_index =
-        GC.add_annotation message
-      in
-      let translation =
-        PP.annotate [%here] begin
-            MuSail.Statement.pp_call original_function_name operands
-          end
-      in
-      GC.return begin
-          PP.annotate [%here] begin
-              PP.(separate_horizontally ~separator:space [
-                      translation;
-                      Coq.pp_inline_comment (string @@ Int.to_string annotation_index)
-              ])
-            end
-        end
-    end
+  | _ -> report_incorrect_argument_count original_function_name 2 operands
 
 
 let translate_as_binary_operator_using_function_notation
@@ -104,30 +88,7 @@ let translate_as_binary_operator_using_function_notation
         end
       end
     end
-  | _ -> begin
-      let message =
-        PP.annotate [%here] @@ PP.string @@ Printf.sprintf
-          "%s should receive 2 arguments but instead received %d; falling back on default translation for function calls"
-          (Ast.Identifier.string_of original_function_name)
-          (List.length operands)
-      in
-      let* annotation_index =
-        GC.add_annotation message
-      in
-      let translation =
-        PP.annotate [%here] begin
-            MuSail.Statement.pp_call original_function_name operands
-          end
-      in
-      GC.return begin
-          PP.annotate [%here] begin
-              PP.(separate_horizontally ~separator:space [
-                      translation;
-                      Coq.pp_inline_comment (string @@ Int.to_string annotation_index)
-              ])
-            end
-        end
-    end
+  | _ -> report_incorrect_argument_count original_function_name 2 operands    
 
 
 let translate_as_binary_operator 
