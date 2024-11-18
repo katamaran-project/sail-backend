afdfed7f6b084159189afd9e30f6fc99508057b3
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-18 19:37:05 +0100

    exported Logging.warn

--- nanosail/Logging.mli
+++ nanosail/Logging.mli
@@ -1,3 +1,4 @@
 val info     : string lazy_t -> unit
 val debug    : string lazy_t -> unit
+val warning  : string lazy_t -> unit
 val surround : (string lazy_t -> unit) -> Lexing.position -> string -> (unit -> 'a) -> 'a
