bc33ff467f1bd06fd5d9ff99d197f3261c0cda08
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-18 12:44:59 +0100

    & on bitvectors

--- nanosail/NanosailToMicrosail/FunctionCalls.ml
+++ nanosail/NanosailToMicrosail/FunctionCalls.ml
@@ -166,6 +166,7 @@ let translate
       end
     end
   | "add_bits"     -> GC.pp_annotate [%here] @@ translate_as_binary_operator function_identifier "!=" "(bop.bvadd)" pp_arguments
+  | "and_vec"      -> GC.pp_annotate [%here] @@ translate_as_binary_operator function_identifier "!=" "(bop.bvand)" pp_arguments
   | "not_bool"     -> GC.pp_annotate [%here] @@ translate_as_unary_operator function_identifier "uop.not" pp_arguments
   | "eq_bool"      -> GC.pp_annotate [%here] @@ translate_as_binary_operator function_identifier "=" "(bop.relop bop.eq)" pp_arguments
   | "neq_bool"     -> GC.pp_annotate [%here] @@ translate_as_binary_operator function_identifier "!=" "(bop.relop bop.neq)" pp_arguments
