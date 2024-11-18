bbd20aa902b5bab8e3450122775ee651ea6c4857
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-18 11:21:08 +0100

    StringOf.OCaml.list

--- nanosail/StringOf/OCaml.ml
+++ nanosail/StringOf/OCaml.ml
@@ -1,2 +1,15 @@
+open Base
+
+
 let position ({ pos_fname; pos_lnum; pos_bol; pos_cnum } : Lexing.position) =
   Printf.sprintf "Pos(%s:%d:%d:%d)" pos_fname pos_lnum pos_bol pos_cnum
+
+
+let list
+    (string_of_element : 'a -> string)
+    (elements          : string list ) : string
+  =
+  let elements' =
+    List.map ~f:string_of_element elements
+  in
+  "[" ^ String.concat ~sep:"; " elements' ^ "]"
