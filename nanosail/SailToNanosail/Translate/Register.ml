ffc7e71a5359f776cd5664ab206ac4ceba2a62a2
Frederic Vogels <frederic.vogels@kuleuven.be>
2024-11-19 12:36:34 +0100

    improved logging

--- nanosail/SailToNanosail/Translate/Register.ml
+++ nanosail/SailToNanosail/Translate/Register.ml
@@ -29,7 +29,7 @@ let translate_register
     match initial_value with
     | Some unwrapped_initial_value -> begin
         let on_failed_translation (error : TC.Error.t) =
-          Logging.info @@ lazy (Printf.sprintf "Failed to translate initial value for register %s:\n  %s" (Ast.Identifier.string_of identifier') (TC.Error.to_string error));
+          Logging.warning @@ lazy (Printf.sprintf "Failed to translate initial value for register %s:\n  %s" (Ast.Identifier.string_of identifier') (TC.Error.to_string error));
           TC.return @@ Ast.Definition.Register.RawSpecified (StringOf.Sail.exp unwrapped_initial_value)
         in
         TC.recover
