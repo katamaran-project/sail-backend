5da665f46b69c43ed14d0302a4068650e06b9e56
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-22 08:55:24 +0100

    layout

--- nanosail/NanosailToMicrosail/FunctionCalls.ml
+++ nanosail/NanosailToMicrosail/FunctionCalls.ml
@@ -228,13 +228,14 @@ let translate
   | "not_bool"       -> GC.pp_annotate [%here] @@ translate_unary_operator  function_identifier "uop.not" pp_arguments
   | "signed"         -> GC.pp_annotate [%here] @@ translate_unary_operator  function_identifier "uop.signed" pp_arguments
   | "unsigned"       -> GC.pp_annotate [%here] @@ translate_unary_operator  function_identifier "uop.unsigned" pp_arguments
-  | "eq_bits"        -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~infix:(Some "=") pp_arguments
-  | "add_bits"       -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~infix:(Some "+ᵇ") ~name:(Some "bop.bvadd") pp_arguments
-  | "sub_bits"       -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~infix:(Some "-ᵇ") ~name:(Some "bop.bvsub") pp_arguments
-  | "and_vec"        -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~name:(Some "bop.bvand") pp_arguments
-  | "or_vec"         -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~name:(Some "bop.bvor") pp_arguments
-  | "xor_vec"        -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~name:(Some "bop.bvxor") pp_arguments
-  | "eq_bool"        -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~infix:(Some "=") ~name:(Some "(bop.relop bop.eq)") pp_arguments
+      
+  | "eq_bits"        -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~infix:(Some "=")                                     pp_arguments
+  | "add_bits"       -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~infix:(Some "+ᵇ") ~name:(Some "bop.bvadd")           pp_arguments
+  | "sub_bits"       -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~infix:(Some "-ᵇ") ~name:(Some "bop.bvsub")           pp_arguments
+  | "and_vec"        -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier                    ~name:(Some "bop.bvand")           pp_arguments
+  | "or_vec"         -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier                    ~name:(Some "bop.bvor")            pp_arguments
+  | "xor_vec"        -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier                    ~name:(Some "bop.bvxor")           pp_arguments
+  | "eq_bool"        -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~infix:(Some "=")  ~name:(Some "(bop.relop bop.eq)")  pp_arguments
   | "neq_bool"       -> GC.pp_annotate [%here] @@ translate_binary_operator function_identifier ~infix:(Some "!=") ~name:(Some "(bop.relop bop.neq)") pp_arguments
   | "eq_unit"        -> GC.pp_annotate [%here] @@ translate_unit_equality ()
   | "add_bits_int"   -> GC.pp_annotate [%here] @@ translate_add_bits_int arguments
