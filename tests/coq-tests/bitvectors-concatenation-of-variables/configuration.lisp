268490fe42bb7945d6942d9a268823b1ce07b194
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-28 16:12:24 +0100

    removed test scripts

--- tests/coq-tests/bitvectors-concatenation-of-variables/configuration.lisp
+++ tests/coq-tests/bitvectors-concatenation-of-variables/configuration.lisp
@@ -12,8 +12,6 @@
                                          (string-ends-with? "_of_num" identifier)
                                          (string-starts-with? "num_of_" identifier)
                                          (string-starts-with? "undefined_" identifier)
-                                         (string-starts-with? "regval_of_" identifier)
-                                         (string-ends-with? "_of_regval" identifier)
                                          (contains? '(
                                                       "eq_unit"
                                                       "neq_int"
@@ -36,9 +34,7 @@
 (ignore-type-definition-predicate (lambda (identifier)
                                (contains? '(
                                             "option"
-                                            "register_value"
-                                            "bits"
-                                            "regstate")
+                                            "bits")
                                           identifier)))
 
 (ignore-value-definition-predicate
