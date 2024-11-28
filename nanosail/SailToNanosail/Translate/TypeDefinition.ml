e592118359b015f4d4f22c793179ed143b689693
Frederic Vogels <frederic.vogels+github@gmail.com>
2024-11-28 13:52:33 +0100

    refactoring

--- nanosail/SailToNanosail/Translate/TypeDefinition.ml
+++ nanosail/SailToNanosail/Translate/TypeDefinition.ml
@@ -1,6 +1,10 @@
-open Variant (* must be opened before Base due to name clash *)
-
+(*
+   Base contains its own Variant module, which we are not interested in
+   We want Variant to refer to our own module
+*)
+module V = Variant
 open Base
+module Variant = V
 
 module Big_int = Nat_big_num
 
@@ -13,9 +17,6 @@ end
 
 module TC = TranslationContext
 open Monads.Notations.Star(TC)
-open TypeAbbreviation
-open Enum
-open Record
 
 
 let translate_type_definition
@@ -32,9 +33,9 @@ let translate_type_definition
     TC.return @@ Ast.Definition.TypeDefinition result
   in
   match type_definition with
-  | TD_abbrev (identifier, quantifier, arg)                      -> register @@ translate_type_abbreviation definition_annotation type_annotation identifier quantifier arg
-  | TD_variant (identifier, type_quantifier, constructors, flag) -> register @@ translate_variant definition_annotation type_annotation identifier type_quantifier constructors flag
-  | TD_enum (identifier, cases, _)                               -> register @@ translate_enum definition_annotation type_annotation identifier cases
-  | TD_record (identifier, quantifier, fields, _)                -> register @@ translate_record definition_annotation type_annotation identifier quantifier fields
+  | TD_abbrev (identifier, quantifier, arg)                      -> register @@ TypeAbbreviation.translate_type_abbreviation definition_annotation type_annotation identifier quantifier arg
+  | TD_variant (identifier, type_quantifier, constructors, flag) -> register @@ Variant.translate_variant definition_annotation type_annotation identifier type_quantifier constructors flag
+  | TD_enum (identifier, cases, _)                               -> register @@ Enum.translate_enum definition_annotation type_annotation identifier cases
+  | TD_record (identifier, quantifier, fields, _)                -> register @@ Record.translate_record definition_annotation type_annotation identifier quantifier fields
   | TD_bitfield (_, _, _)                                        -> TC.not_yet_implemented [%here] location
   | TD_abstract (_, _)                                           -> TC.not_yet_implemented [%here] location
