f7f69c9cbf2b099cacb63bed746c854d6939e2af
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-01-06 13:10:52 +0100

    simplification

--- nanosail/SailToNanosail/Translate/Function.ml
+++ nanosail/SailToNanosail/Translate/Function.ml
@@ -792,15 +792,15 @@ let rec statement_of_aexp (expression : S.typ S.aexp) : Ast.Statement.t TC.t =
                       let S.AP_aux (tuple_pattern, _env, tuple_pattern_location) = tuple_pattern
                       in
                       match tuple_pattern with
-                      | Libsail.Anf.AP_id (identifier, _typ) -> Identifier.translate_identifier [%here] identifier
-                      | Libsail.Anf.AP_tuple _               -> TC.not_yet_implemented [%here] tuple_pattern_location
-                      | Libsail.Anf.AP_global (_, _)         -> TC.not_yet_implemented [%here] tuple_pattern_location
-                      | Libsail.Anf.AP_app (_, _, _)         -> TC.not_yet_implemented [%here] tuple_pattern_location
-                      | Libsail.Anf.AP_cons (_, _)           -> TC.not_yet_implemented [%here] tuple_pattern_location
-                      | Libsail.Anf.AP_as (_, _, _)          -> TC.not_yet_implemented [%here] tuple_pattern_location
-                      | Libsail.Anf.AP_struct (_, _)         -> TC.not_yet_implemented [%here] tuple_pattern_location
-                      | Libsail.Anf.AP_nil _                 -> TC.not_yet_implemented [%here] tuple_pattern_location
-                      | Libsail.Anf.AP_wild _                -> TC.not_yet_implemented [%here] tuple_pattern_location
+                      | AP_id (identifier, _typ) -> Identifier.translate_identifier [%here] identifier
+                      | AP_tuple _               -> TC.not_yet_implemented [%here] tuple_pattern_location
+                      | AP_global (_, _)         -> TC.not_yet_implemented [%here] tuple_pattern_location
+                      | AP_app (_, _, _)         -> TC.not_yet_implemented [%here] tuple_pattern_location
+                      | AP_cons (_, _)           -> TC.not_yet_implemented [%here] tuple_pattern_location
+                      | AP_as (_, _, _)          -> TC.not_yet_implemented [%here] tuple_pattern_location
+                      | AP_struct (_, _)         -> TC.not_yet_implemented [%here] tuple_pattern_location
+                      | AP_nil _                 -> TC.not_yet_implemented [%here] tuple_pattern_location
+                      | AP_wild _                -> TC.not_yet_implemented [%here] tuple_pattern_location
                     in
                     let* identifiers = TC.map ~f:extract_identifiers tuple_patterns
                     in
@@ -816,8 +816,11 @@ let rec statement_of_aexp (expression : S.typ S.aexp) : Ast.Statement.t TC.t =
                     in
                     TC.return @@ Ast.Identifier.Map.add_exn acc ~key:variant_tag_identifier ~data:([identifier], translated_clause)
                   end
+                | S.AP_app (_identifier, _pattern, _typ) -> begin
+                    Stdio.printf "AP_app(%s, %s, %s)\n" (StringOf.Sail.id _identifier) (StringOf.Sail.apat _pattern) (StringOf.Sail.typ _typ);
+                    TC.not_yet_implemented [%here] subpattern_location
+                  end
                 | S.AP_global (_, _) -> TC.not_yet_implemented [%here] subpattern_location
-                | S.AP_app (_, _, _) -> TC.not_yet_implemented [%here] subpattern_location
                 | S.AP_cons (_, _)   -> TC.not_yet_implemented [%here] subpattern_location
                 | S.AP_as (_, _, _)  -> TC.not_yet_implemented [%here] subpattern_location
                 | S.AP_struct (_, _) -> TC.not_yet_implemented [%here] subpattern_location
