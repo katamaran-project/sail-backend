b478aa3c9190bf9544f9c00c9e5474679b5d968c
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-27 16:04:25 +0100

    test_build_match_for_enum_1

--- tests/nanosail-tests/TuplePatternMatchingTests.ml
+++ tests/nanosail-tests/TuplePatternMatchingTests.ml
@@ -58,7 +58,7 @@ let run_tc (tc : 'a TC.t) : 'a =
 
 let build_tuple_pattern_chain = TM.build_tuple_pattern_chain dummy_location
 let categorize                = TM.categorize_case dummy_location
-
+let build_match               = TM.build_leveled_match_statements
 
 let test_build_chain_enum_1 =
   let test _ =
@@ -542,7 +542,6 @@ let test_categorize_enum_6 =
       TC.return ()
     in
     ignore @@ run_tc tc
-
   in
   {|
       enum A { A1, A2 }
@@ -550,6 +549,63 @@ let test_categorize_enum_6 =
       match a {
         A1 => read_register r1,
         x  => read_register r2,
+      }
+  |} >:: test
+
+
+let test_build_match_for_enum_1 =
+  let test _ =
+    let tc =
+      let* enum_type =
+        define_enum_str "A" ["A1"]
+      in
+      let a1_statement =        
+        Ast.Statement.ReadRegister (mkid "r1")
+      in
+      let* chain =
+        let* chain = build_tuple_pattern_chain [ enum_type ]
+        in
+        let* chain = categorize
+            chain
+            [
+              Pattern.EnumCase (mkid "A1")
+            ]
+            a1_statement
+            false
+        in
+        TC.return chain
+      in
+      let* actual_match_statement =
+        build_match [mkid "value1"] chain
+      in
+      let expected_match_statement =
+        Ast.Statement.Match begin
+          Ast.Statement.MatchEnum {
+            matched = mkid "value1";
+            matched_type = mkid "A";
+            cases = Ast.Identifier.Map.of_alist_exn [
+                (
+                  mkid "A1",
+                  a1_statement
+                )
+              ]
+          }
+        end
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string Ast.Statement.to_fexpr)
+        ~cmp:Ast.Statement.equal
+        expected_match_statement
+        actual_match_statement;
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+  in
+  {|
+      enum A { A1 }
+  
+      match value1 {
+        A1 => read_register r1,
       }
   |} >:: test
 
@@ -573,8 +629,14 @@ let test_categorizing_suite =
   ]
 
 
+let test_generate_match_suite =
+  "match generation" >::: [
+    test_build_match_for_enum_1;
+  ]
+
 let test_suite =
   "tuple pattern matching test suite" >::: [
     test_chain_building_suite;
     test_categorizing_suite;
+    test_generate_match_suite;
   ]
