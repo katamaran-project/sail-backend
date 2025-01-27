b12f9aaec28528abbf89020325fc184699f4b9a6
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-27 15:39:33 +0100

    test_categorize_enum_6

--- tests/nanosail-tests/TuplePatternMatchingTests.ml
+++ tests/nanosail-tests/TuplePatternMatchingTests.ml
@@ -481,7 +481,75 @@ let test_categorize_enum_5 =
   
       match a {
         A1 => read_register r1,
-        A2 => read_register r2,
+        _  => read_register r2,
+      }
+  |} >:: test
+
+
+let test_categorize_enum_6 =
+  let test _ =
+    let tc =
+      let* enum_type =
+        define_enum_str "A" ["A1"; "A2"]
+      in
+      let a1_statement =        
+        Ast.Statement.ReadRegister (mkid "r1")
+      in
+      let a2_statement =
+        Ast.Statement.ReadRegister (mkid "r2")
+      in
+      let* chain =
+        let* chain = build_tuple_pattern_chain [ enum_type ]
+        in
+        let* chain = categorize
+            chain
+            [
+              Pattern.EnumCase (mkid "A1")
+            ]
+            a1_statement
+            false
+        in
+        let* chain = categorize
+            chain
+            [
+              Pattern.Binder { identifier = mkid "x"; wildcard = false }
+            ]
+            a2_statement
+            false
+        in            
+        TC.return chain
+      in
+      let expected_chain =
+        TM.PatternNode.Enum {
+          enum_identifier = mkid "A";
+          table = Ast.Identifier.Map.of_alist_exn [
+              (
+                mkid "A1",
+                TM.PatternNode.Terminal (Some a1_statement)
+              );
+              (
+                mkid "A2",
+                TM.PatternNode.Terminal (Some a2_statement)
+              );
+            ];
+          binder_identifier = Some (mkid "x");
+        }
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string TM.PatternNode.to_fexpr)
+        ~cmp:TM.PatternNode.equal
+        expected_chain chain;
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+
+  in
+  {|
+      enum A { A1, A2 }
+  
+      match a {
+        A1 => read_register r1,
+        x  => read_register r2,
       }
   |} >:: test
 
@@ -501,6 +569,7 @@ let test_categorizing_suite =
     test_categorize_enum_3;
     test_categorize_enum_4;
     test_categorize_enum_5;
+    test_categorize_enum_6;
   ]
 
 
