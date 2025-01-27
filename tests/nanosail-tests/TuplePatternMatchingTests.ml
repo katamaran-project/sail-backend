8a3ad1edd0774272c20df4da6bf3423e38e900a7
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-27 21:01:29 +0100

    test_build_match_for_enum_int

--- tests/nanosail-tests/TuplePatternMatchingTests.ml
+++ tests/nanosail-tests/TuplePatternMatchingTests.ml
@@ -1427,6 +1427,69 @@ let test_build_match_for_int_int =
   |} >:: test
 
 
+let test_build_match_for_enum_int =
+  let test _ =
+    let tc =
+      let* enum_type =
+        define_enum_str "A" ["A1"]
+      in
+      let statement =
+        mkstm 1
+      in
+      let* chain =
+        let* chain = build_tuple_pattern_chain [ enum_type; Ast.Type.Int ]
+        in
+        let* chain = categorize
+            chain
+            [
+              Pattern.EnumCase (mkid "A1");
+              Pattern.Binder { identifier = mkid "k"; wildcard = true };
+            ]
+            statement
+            false
+        in
+        TC.return chain
+      in
+      let* actual_match_statement =
+        build_match [mkid "enum_value"; mkid "int_value"] chain
+      in
+      let expected_match_statement =
+        Ast.Statement.Match begin
+          Ast.Statement.MatchEnum {
+            matched = mkid "enum_value";
+            matched_type = mkid "A";
+            cases = Ast.Identifier.Map.of_alist_exn [
+                (
+                  mkid "A1",
+                  Ast.Statement.Let {
+                    variable_identifier    = mkid "k";
+                    binding_statement_type = Ast.Type.Int;
+                    binding_statement      = Ast.Statement.Expression (Ast.Expression.Variable (mkid "int_value", Ast.Type.Int));
+                    body_statement         = statement
+                  }
+                )
+              ]
+          }
+        end
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string Ast.Statement.to_fexpr)
+        ~cmp:Ast.Statement.equal
+        expected_match_statement
+        actual_match_statement;
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+  in
+  {|
+      enum A = { A1 }
+    
+      match (enumval, intval) {
+        (A1, k) => read_register r1,
+      }
+  |} >:: test
+
+
 let test_chain_building_suite =
   "chain building test suite" >::: [
     test_build_chain_enum_1;
@@ -1459,6 +1522,8 @@ let test_generate_match_suite =
     
     test_build_match_for_int;
     test_build_match_for_int_int;
+
+    test_build_match_for_enum_int;
   ]
 
 
