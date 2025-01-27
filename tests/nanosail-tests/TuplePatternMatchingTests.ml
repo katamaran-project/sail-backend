dafd251018149da37227dcb04e633a4c39e29ccc
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-27 19:45:07 +0100

    test_build_match_for_enum_5

--- tests/nanosail-tests/TuplePatternMatchingTests.ml
+++ tests/nanosail-tests/TuplePatternMatchingTests.ml
@@ -12,8 +12,8 @@ module TM      = SailToNanosail.Translate.Match.TupleMatching
 let dummy_location : Libsail.Ast.l =
   Libsail.Parse_ast.Unknown
 
-let mkid = Ast.Identifier.mk
-
+let mkid    = Ast.Identifier.mk
+let mkstm n = Ast.Statement.ReadRegister (mkid @@ Printf.sprintf "r%d" n)
 
 let define_enum
     (identifier : Ast.Identifier.t     )
@@ -842,6 +842,134 @@ let test_build_match_for_enum_4 =
   |} >:: test
 
 
+let test_build_match_for_enum_5 =
+  let test _ =
+    let tc =
+      let* enum_type =
+        define_enum_str "A" ["A1"; "A2"]
+      in
+      let a1_a1_statement =
+        mkstm 1
+      and a1_a2_statement =
+        mkstm 2
+      and a2_a1_statement =
+        mkstm 3
+      and a2_a2_statement =
+        mkstm 4
+      in
+      let* chain =
+        let* chain = build_tuple_pattern_chain [ enum_type; enum_type ]
+        in
+        let* chain = categorize
+            chain
+            [
+              Pattern.EnumCase (mkid "A1");
+              Pattern.EnumCase (mkid "A1");
+            ]
+            a1_a1_statement
+            false
+        in
+        let* chain = categorize
+            chain
+            [
+              Pattern.EnumCase (mkid "A1");
+              Pattern.EnumCase (mkid "A2");
+            ]
+            a1_a2_statement
+            false
+        in        
+        let* chain = categorize
+            chain
+            [
+              Pattern.EnumCase (mkid "A2");
+              Pattern.EnumCase (mkid "A1");
+            ]
+            a2_a1_statement
+            false
+        in        
+        let* chain = categorize
+            chain
+            [
+              Pattern.EnumCase (mkid "A2");
+              Pattern.EnumCase (mkid "A2");
+            ]
+            a2_a2_statement
+            false
+        in        
+        TC.return chain
+      in
+      let* actual_match_statement =
+        build_match [mkid "value1"; mkid "value2"] chain
+      in
+      let expected_match_statement =
+        Ast.Statement.Match begin
+          Ast.Statement.MatchEnum {
+            matched = mkid "value1";
+            matched_type = mkid "A";
+            cases = Ast.Identifier.Map.of_alist_exn [
+                (
+                  mkid "A1",
+                  Ast.Statement.Match begin
+                    Ast.Statement.MatchEnum {
+                      matched = mkid "value2";
+                      matched_type = mkid "A";
+                      cases = Ast.Identifier.Map.of_alist_exn [
+                          (
+                            mkid "A1",
+                            a1_a1_statement
+                          );
+                          (
+                            mkid "A2",
+                            a1_a2_statement
+                          );
+                        ]
+                    }
+                  end
+                );
+                (
+                  mkid "A2",
+                  Ast.Statement.Match begin
+                    Ast.Statement.MatchEnum {
+                      matched = mkid "value2";
+                      matched_type = mkid "A";
+                      cases = Ast.Identifier.Map.of_alist_exn [
+                          (
+                            mkid "A1",
+                            a2_a1_statement
+                          );
+                          (
+                            mkid "A2",
+                            a2_a2_statement
+                          );
+                        ]
+                    }
+                  end
+                );
+              ]
+          }
+        end
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string Ast.Statement.to_fexpr)
+        ~cmp:Ast.Statement.equal
+        expected_match_statement
+        actual_match_statement;
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+  in
+  {|
+      enum A { A1, A2 }
+  
+      match value1, value2 {
+        A1, A1 => r1,
+        A1, A2 => r2,
+        A2, A1 => r3,
+        A2, A2 => r4,
+      }
+  |} >:: test
+
+
 let test_chain_building_suite =
   "chain building test suite" >::: [
     test_build_chain_enum_1;
@@ -867,6 +995,7 @@ let test_generate_match_suite =
     test_build_match_for_enum_2;
     test_build_match_for_enum_3;
     test_build_match_for_enum_4;
+    test_build_match_for_enum_5;
   ]
 
 
