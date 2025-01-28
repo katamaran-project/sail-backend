5d8025ecc661142119d0216c254c4044723ecea6
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-01-28 17:12:44 +0100

    variant matching tests

--- tests/nanosail-tests/TuplePatternMatchingTests/BuildStatementTests/BuildStatementTests.ml
+++ tests/nanosail-tests/TuplePatternMatchingTests/BuildStatementTests/BuildStatementTests.ml
@@ -4,5 +4,6 @@ open OUnit2
 let test_suite = "build statement" >::: [
     Enum.test_suite;
     Int.test_suite;
+    Variant.test_suite;
     Mix.test_suite;
   ]
---
+++ tests/nanosail-tests/TuplePatternMatchingTests/BuildStatementTests/Variant.ml
@@ -0,0 +1,83 @@
+open Base
+open OUnit2
+open Nanosail
+
+module BuildChainTests = BuildChainTests
+
+
+module TC = SailToNanosail.TranslationContext
+open Monads.Notations.Star(TC)
+
+
+module Pattern = SailToNanosail.Translate.Match.Pattern
+module TM      = SailToNanosail.Translate.Match.TupleMatching
+
+open Shared
+
+
+let test_build_match_for_variant_1 =
+  let test _ =
+    let tc =
+      let* enum_type =
+        define_variant "A" [("A1", [])]
+      in
+      let a1_statement =        
+        Ast.Statement.ReadRegister (mkid "r1")
+      in
+      let* pattern_tree =
+        let* pattern_tree = build_tuple_pattern_chain [ enum_type ]
+        in
+        let* pattern_tree = categorize
+            pattern_tree
+            [
+              Pattern.EnumCase (mkid "A1")
+            ]
+            a1_statement
+            false
+        in
+        TC.return pattern_tree
+      in
+      let* actual_match_statement =
+        build_match [mkid "value1"] pattern_tree
+      in
+      let expected_match_statement =
+        Ast.Statement.Match begin
+          Ast.Statement.MatchVariant {
+            matched = mkid "value1";
+            matched_type = mkid "A";
+            cases = Ast.Identifier.Map.of_alist_exn [
+                (
+                  mkid "A1",
+                  (
+                    [],
+                    a1_statement
+                  )
+                )
+              ]
+          }
+        end
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string Ast.Statement.to_fexpr)
+        ~cmp:Ast.Statement.equal
+        expected_match_statement
+        actual_match_statement;
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+  in
+  {|
+      union A = {
+        A1 : unit
+      }
+  
+      match value1 {
+        A1() => read_register r1,
+      }
+  |} >:: test
+
+
+let test_suite =
+  "match generation" >::: [
+    test_build_match_for_variant_1;
+  ]
