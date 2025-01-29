c996bd8743e38118205aae3ce94d048d487a8d30
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-01-29 16:12:26 +0100

    test_build_match_for_variant_nary_constructor_field_wildcards

--- tests/nanosail-tests/TuplePatternMatchingTests/BuildStatementTests/Variant.ml
+++ tests/nanosail-tests/TuplePatternMatchingTests/BuildStatementTests/Variant.ml
@@ -568,6 +568,76 @@ let test_build_match_for_variant_two_constructors =
   |} >:: test
 
 
+let test_build_match_for_variant_nary_constructor_field_wildcards =
+  let test _ =
+    let tc =
+      let* enum_type =
+        define_variant "A" [("A1", [Ast.Type.Int; Ast.Type.Int])]
+      in
+      let a1_statement =
+        Ast.Statement.ReadRegister (mkid "r1")
+      in
+      let* pattern_tree =
+        let* pattern_tree = build_tuple_pattern_tree [ enum_type ]
+        in
+        let* pattern_tree = categorize
+            pattern_tree
+            [
+              Pattern.(
+                VariantCase (
+                  mkid "A1",
+                  Tuple [
+                    Binder { identifier = mkid "x"; wildcard = false };
+                    Binder { identifier = mkid "y"; wildcard = true  };
+                  ]
+                )
+              )
+            ]
+            a1_statement
+            false
+        in
+        TC.return pattern_tree
+      in
+      let* actual_match_statement =
+        build_match [mkid "value1"] pattern_tree
+      in
+      let expected_match_statement =
+        Ast.Statement.Match begin
+          Ast.Statement.MatchVariant {
+            matched = mkid "value1";
+            matched_type = mkid "A";
+            cases = Ast.Identifier.Map.of_alist_exn [
+                (
+                  mkid "A1",
+                  (
+                    [ mkid "x"; mkid "y" ],
+                    a1_statement
+                  )
+                )
+              ]
+          }
+        end
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string Ast.Statement.to_fexpr)
+        ~cmp:Ast.Statement.equal
+        expected_match_statement
+        actual_match_statement;
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+  in
+  {|
+      union A = {
+        A1 : (int, int)
+      }
+
+      match value1 {
+        A1(x, y) => read_register r1,
+      }
+  |} >:: test
+
+
 let test_suite =
   "match generation" >::: [
     test_build_match_for_variant_single_nullary_constructor;
@@ -578,4 +648,5 @@ let test_suite =
     test_build_match_for_variant_single_unary_constructor_field_wildcard;
     test_build_match_for_variant_single_binary_constructor;
     test_build_match_for_variant_two_constructors;
+    test_build_match_for_variant_nary_constructor_field_wildcards;
   ]
