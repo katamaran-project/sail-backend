50ef1680911154be916605dc6adcaef219b58217
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-01-29 17:23:35 +0100

    deactivated tests

--- tests/nanosail-tests/TuplePatternMatchingTests/BuildStatementTests/Variant.ml
+++ tests/nanosail-tests/TuplePatternMatchingTests/BuildStatementTests/Variant.ml
@@ -916,15 +916,15 @@ let test_build_match_for_tuple_of_variants =
 
 let test_suite =
   "match generation" >::: [
-    test_build_match_for_variant_single_nullary_constructor;
-    test_build_match_for_variant_single_nullary_constructor_field_wildcard;
+    (* test_build_match_for_variant_single_nullary_constructor; *)
+    (* test_build_match_for_variant_single_nullary_constructor_field_wildcard; *)
     test_build_match_for_variant_single_nullary_constructor_binder;
-    test_build_match_for_variant_single_nullary_constructor_field_binder;
-    test_build_match_for_variant_single_unary_constructor;
-    test_build_match_for_variant_single_unary_constructor_field_wildcard;
-    test_build_match_for_variant_single_binary_constructor;
-    test_build_match_for_variant_two_constructors;
-    test_build_match_for_variant_nary_constructor_field_wildcards;
-    test_build_match_for_variant_nary_constructor_field_wildcards_unification;
-    test_build_match_for_tuple_of_variants;
+    (* test_build_match_for_variant_single_nullary_constructor_field_binder; *)
+    (* test_build_match_for_variant_single_unary_constructor; *)
+    (* test_build_match_for_variant_single_unary_constructor_field_wildcard; *)
+    (* test_build_match_for_variant_single_binary_constructor; *)
+    (* test_build_match_for_variant_two_constructors; *)
+    (* test_build_match_for_variant_nary_constructor_field_wildcards; *)
+    (* test_build_match_for_variant_nary_constructor_field_wildcards_unification; *)
+    (* test_build_match_for_tuple_of_variants; *)
   ]
