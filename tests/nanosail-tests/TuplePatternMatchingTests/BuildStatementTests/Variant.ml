80aa1e68b924e7f9b99a4b18939efb9e651ca9b2
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-01-29 10:30:24 +0100

    test_build_match_for_variant_single_nullary_constructor_wildcard

--- tests/nanosail-tests/TuplePatternMatchingTests/BuildStatementTests/Variant.ml
+++ tests/nanosail-tests/TuplePatternMatchingTests/BuildStatementTests/Variant.ml
@@ -75,6 +75,68 @@ let test_build_match_for_variant_single_nullary_constructor =
   |} >:: test
 
 
+let test_build_match_for_variant_single_nullary_constructor_wildcard =
+  let test _ =
+    let tc =
+      let* enum_type =
+        define_variant "A" [("A1", [])]
+      in
+      let a1_statement =
+        Ast.Statement.ReadRegister (mkid "r1")
+      in
+      let* pattern_tree =
+        let* pattern_tree = build_tuple_pattern_tree [ enum_type ]
+        in
+        let* pattern_tree = categorize
+            pattern_tree
+            [
+              Pattern.(VariantCase (mkid "A1", Pattern.Binder { identifier = mkid "x"; wildcard = true }))
+            ]
+            a1_statement
+            false
+        in
+        TC.return pattern_tree
+      in
+      let* actual_match_statement =
+        build_match [mkid "value1"] pattern_tree
+      in
+      let expected_match_statement =
+        Ast.Statement.Match begin
+          Ast.Statement.MatchVariant {
+            matched = mkid "value1";
+            matched_type = mkid "A";
+            cases = Ast.Identifier.Map.of_alist_exn [
+                (
+                  mkid "A1",
+                  (
+                    [],
+                    a1_statement
+                  )
+                )
+              ]
+          }
+        end
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string Ast.Statement.to_fexpr)
+        ~cmp:Ast.Statement.equal
+        expected_match_statement
+        actual_match_statement;
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+  in
+  {|
+      union A = {
+        A1 : unit
+      }
+
+      match value1 {
+        A1(_) => read_register r1,
+      }
+  |} >:: test
+
+
 let test_build_match_for_variant_single_unary_constructor =
   let test _ =
     let tc =
@@ -369,6 +431,7 @@ let test_build_match_for_variant_two_constructors =
 let test_suite =
   "match generation" >::: [
     test_build_match_for_variant_single_nullary_constructor;
+    test_build_match_for_variant_single_nullary_constructor_wildcard;
     test_build_match_for_variant_single_unary_constructor;
     test_build_match_for_variant_single_unary_constructor_wildcard;
     test_build_match_for_variant_single_binary_constructor;
