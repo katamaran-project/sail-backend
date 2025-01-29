81f84d3f31a2a255c00f67fac12ff3f157c35a42
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-01-29 14:59:26 +0100

    test_build_pattern_tree_variant_single_unary_constructor

--- tests/nanosail-tests/TuplePatternMatchingTests/BuildTreeTests/BuildTreeTests.ml
+++ tests/nanosail-tests/TuplePatternMatchingTests/BuildTreeTests/BuildTreeTests.ml
@@ -2,5 +2,6 @@ open OUnit2
 
 
 let test_suite = "build pattern tree" >::: [
-    Enum.test_suite
+    Enum.test_suite;
+    Variant.test_suite;
   ]
---
+++ tests/nanosail-tests/TuplePatternMatchingTests/BuildTreeTests/Variant.ml
@@ -0,0 +1,52 @@
+open Base
+open OUnit2
+open Nanosail
+
+module TC = SailToNanosail.TranslationContext
+open Monads.Notations.Star(TC)
+
+module TM = SailToNanosail.Translate.Match.TupleMatching
+module PN = TM.PatternNode
+
+open Shared
+
+
+let test_build_pattern_tree_variant_single_unary_constructor =
+  let test _ =
+    let tc =
+      let* enum_type =
+        define_variant "A" [("A1", [Ast.Type.Int])]
+      in
+      let* actual_pattern_tree : PN.t =
+        build_tuple_pattern_tree [ enum_type ]
+      in
+      let expected_pattern_tree : PN.t =
+        PN.Variant {
+          variant_identifier = mkid "A";
+          table = Ast.Identifier.Map.of_alist_exn [
+              (
+                mkid "A1",
+                PN.UnaryConstructor (None, PN.Terminal None)
+              );
+            ]
+        }
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string PN.to_fexpr)
+        ~cmp:TM.PatternNode.equal
+        expected_pattern_tree
+        actual_pattern_tree;
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+  in
+  {|
+      union A = {
+        A1 : int
+      }
+  |} >:: test
+
+
+let test_suite = "variant" >::: [
+    test_build_pattern_tree_variant_single_unary_constructor;
+]
