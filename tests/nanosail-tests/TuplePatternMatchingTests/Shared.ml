eee988fbc86964f2e422bf5d604514f426826f3a
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-01-30 13:17:56 +0100

    create_identifier_generator in tests

--- tests/nanosail-tests/TuplePatternMatchingTests/Shared.ml
+++ tests/nanosail-tests/TuplePatternMatchingTests/Shared.ml
@@ -91,3 +91,14 @@ let run_failing_tc (tc : 'a TC.t) : unit =
 let build_tuple_pattern_tree = TM.build_tuple_pattern_tree dummy_location
 let categorize               = TM.categorize_case dummy_location
 let build_match              = TM.build_leveled_match_statements
+
+let create_identifier_generator () =
+  let counter = ref 0
+  in
+  let next () =
+    let number = !counter
+    in
+    counter := !counter + 1;
+    Ast.Identifier.mk_generated @@ Int.to_string number
+  in
+  next
