c836cba3c3a118a70d80d7c8a10e967aaec5ca7c
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-01-30 16:05:50 +0100

    extra Identifier functionality

--- nanosail/Ast/Identifier.ml
+++ nanosail/Ast/Identifier.ml
@@ -20,8 +20,13 @@ module Impl = struct
 
   let mk x = Id x
 
+  let generated_prefix = "ж"
+  
   let mk_generated name =
-    mk @@ "ж" ^ name
+    mk @@ generated_prefix ^ name
+
+  let is_generated name =
+    String.is_prefix name ~prefix:generated_prefix
   
   let to_string (Id x) = x
 
