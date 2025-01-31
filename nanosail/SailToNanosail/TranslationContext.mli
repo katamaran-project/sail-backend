fa4077a2a7c76ea3e23bc16893ea247b2c9a7a06
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-01 00:30:26 +0100

    TranslationContext.repeat

--- nanosail/SailToNanosail/TranslationContext.mli
+++ nanosail/SailToNanosail/TranslationContext.mli
@@ -47,6 +47,7 @@ val fold_left                      : f:('a -> 'b -> 'a t) -> init:'a -> 'b list 
 val iter                           : f:('a -> unit t) -> 'a list -> unit t
 val lift                           : f:('a -> 'b) -> 'a t -> 'b t
 val lift_option                    : 'a t option -> 'a option t
+val repeat                         : int -> f:('a t) -> 'a list t
 
 val log                            : Lexing.position -> (Lexing.position -> string lazy_t -> unit) -> string lazy_t -> unit t
 val translation_block              : Lexing.position -> string -> 'a t -> 'a t
