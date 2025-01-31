c1500ea8d0ee2ac39eedf103d35fd1b5e2e5c873
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-01-31 15:40:05 +0100

    UnaryOperator.equal

--- nanosail/Ast/UnaryOperator.ml
+++ nanosail/Ast/UnaryOperator.ml
@@ -7,3 +7,14 @@ let to_fexpr (operator : t) : FExpr.t =
   match operator with
   | Neg -> FExpr.mk_symbol "Neg"
   | Not -> FExpr.mk_symbol "Not"
+
+
+let equal
+    (operator_1 : t)
+    (operator_2 : t) : bool
+  =
+  match operator_1, operator_2 with
+  | Neg, Neg -> true
+  | Neg, _   -> false
+  | Not, Not -> true
+  | Not, _   -> false
