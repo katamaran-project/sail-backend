49e6304adef35b21ffbc7c593d68566c8c15fe3b
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-02 20:55:23 +0100

    test_adorn_bool_1

--- tests/nanosail-tests/PatternMatchingTests/AdornTests/AdornTests.ml
+++ tests/nanosail-tests/PatternMatchingTests/AdornTests/AdornTests.ml
@@ -4,4 +4,5 @@ open OUnit2
 let test_suite = "adorn" >::: [
     Enum.test_suite;
     Variant.test_suite;
+    Bool.test_suite;
   ]
---
+++ tests/nanosail-tests/PatternMatchingTests/AdornTests/Bool.ml
@@ -0,0 +1,71 @@
+open Base
+open OUnit2
+open Nanosail
+
+module BuildTreeTests = BuildTreeTests
+
+
+module TC = SailToNanosail.TranslationContext
+open Monads.Notations.Star(TC)
+
+
+module Pattern = SailToNanosail.Translate.Match.Pattern
+module TM      = SailToNanosail.Translate.Match
+
+open Shared
+
+
+let test_adorn_bool_1 =
+  let test _ =
+    let _gen = new generator
+    in
+    let tc =
+      let true_statement =
+        Ast.Statement.ReadRegister (mkid "r1")
+      and false_statement =
+        Ast.Statement.ReadRegister (mkid "r2")
+      in
+      let* tree =
+        let* tree = build_empty_pattern_tree [ Ast.Type.Bool ]
+        in
+        let* tree = adorn
+          tree
+          [ Pattern.BoolCase true ]
+          true_statement
+        in
+        let* tree = adorn
+          tree
+          [ Pattern.BoolCase false ]
+          true_statement
+        in
+        TC.return tree
+      in
+      let expected_tree =
+        TM.PatternTree.Bool {
+          binder     = gen#wildcard;
+          when_true  = None;
+          when_false = None
+        }
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string TM.PatternTree.to_fexpr)
+        ~cmp:TM.PatternTree.equal
+        (Normalize.normalize_pattern_tree expected_tree)
+        (Normalize.normalize_pattern_tree tree);
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+
+  in
+  {|
+    match b {
+      true  => r1,
+      false => r2
+    }
+  |} >:: test
+
+
+let test_suite =
+  "bool" >::: [
+    test_adorn_bool_1;
+  ]
