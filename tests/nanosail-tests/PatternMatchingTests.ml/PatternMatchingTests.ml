1122ad9b7856b791240aed0b8d1ae63655c777e1
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-02 19:11:53 +0100

    rename

--- tests/nanosail-tests/PatternMatchingTests.ml/PatternMatchingTests.ml
+++ tests/nanosail-tests/PatternMatchingTests.ml/PatternMatchingTests.ml
@@ -2,7 +2,7 @@ open OUnit2
 
 
 let test_suite =
-  "tuple pattern matching" >::: [
+  "pattern matching" >::: [
     BuildTreeTests.test_suite;
     AdornTests.test_suite;
     BuildStatementTests.test_suite;
