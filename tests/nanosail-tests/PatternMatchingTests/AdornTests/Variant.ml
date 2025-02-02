ecb1f39b79890c62950b16bb12a2de9a07c38e2a
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-02 19:17:51 +0100

    test_adorn_variant_binder

--- tests/nanosail-tests/PatternMatchingTests/AdornTests/Variant.ml
+++ tests/nanosail-tests/PatternMatchingTests/AdornTests/Variant.ml
@@ -70,6 +70,58 @@ let test_adorn_variant_single_unary_constructor =
   |} >:: test
 
 
+let test_adorn_variant_binder =
+  let test _ =
+    let tc =
+      let* enum_type =
+        define_variant "A" [("A1", [Ast.Type.Int])]
+      in
+      let a1_statement =
+        Ast.Statement.ReadRegister (mkid "r1")
+      in
+      let* actual_pattern_tree =
+        let* pattern_tree = build_empty_pattern_tree [ enum_type ]
+        in
+        let* pattern_tree = adorn
+            pattern_tree
+            [
+              Pattern.Binder (mkbinder "x");
+            ]
+            a1_statement
+        in
+        TC.return pattern_tree
+      in
+      let expected_pattern_tree =
+        PN.Variant {
+          variant_identifier = mkid "A";
+          table = Ast.Identifier.Map.of_alist_exn [
+              (
+                mkid "A1",
+                (mkbinder "x", PN.UnaryConstructor (mkbinder "x"), PN.Terminal (Some a1_statement))
+              );
+            ]
+        }
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string PN.to_fexpr)
+        ~cmp:TM.PatternTree.equal
+        (Normalize.normalize_pattern_tree expected_pattern_tree)
+        (Normalize.normalize_pattern_tree actual_pattern_tree);
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+  in
+  {|
+      union A = {
+        A1 : int
+      }
+
+      match value1 {
+        x => read_register r1,
+      }
+  |} >:: test
+
+
 let test_failure_due_to_clashing_field_binders =
   let test _ =
     let tc =
@@ -120,6 +172,7 @@ let test_failure_due_to_clashing_field_binders =
 let test_suite =
   "enum" >::: [
     test_adorn_variant_single_unary_constructor;
+    test_adorn_variant_binder;
 
     test_failure_due_to_clashing_field_binders;
   ]
