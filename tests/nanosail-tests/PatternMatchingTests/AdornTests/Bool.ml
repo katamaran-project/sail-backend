913ebcd6616b8c356f53b542b553675e8cf2be84
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-03 08:24:59 +0100

    test_adorn_bool_2

--- tests/nanosail-tests/PatternMatchingTests/AdornTests/Bool.ml
+++ tests/nanosail-tests/PatternMatchingTests/AdornTests/Bool.ml
@@ -66,7 +66,59 @@ let test_adorn_bool_1 =
   |} >:: test
 
 
+let test_adorn_bool_2 =
+  let test _ =
+    let _gen = new generator
+    in
+    let tc =
+      let true_statement =
+        Ast.Statement.ReadRegister (mkid "r2")
+      and false_statement =
+        Ast.Statement.ReadRegister (mkid "r1")
+      in
+      let* tree =
+        let* tree = build_empty_pattern_tree [ Ast.Type.Bool ]
+        in
+        let* tree = adorn
+          tree
+          [ Pattern.BoolCase false ]
+          false_statement
+        in
+        let* tree = adorn
+          tree
+          [ Pattern.BoolCase true ]
+          true_statement
+        in
+        TC.return tree
+      in
+      let expected_tree =
+        TM.PatternTree.Bool begin
+          TM.PatternTree.SeparateBoolCases {
+            when_true  = TM.PatternTree.Terminal (Some true_statement);
+            when_false = TM.PatternTree.Terminal (Some false_statement)
+          }
+        end
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string TM.PatternTree.to_fexpr)
+        ~cmp:TM.PatternTree.equal
+        (Normalize.normalize_pattern_tree expected_tree)
+        (Normalize.normalize_pattern_tree tree);
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+
+  in
+  {|
+    match b {
+      false => r1,
+      true  => r2
+    }
+  |} >:: test
+
+
 let test_suite =
   "bool" >::: [
     test_adorn_bool_1;
+    test_adorn_bool_2;
   ]
