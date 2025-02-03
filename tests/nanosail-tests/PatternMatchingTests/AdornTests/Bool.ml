91785c2f22c1a27dcd9f310fbfe9493ebfe275f3
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-03 10:03:19 +0100

    test_adorn_bool_wildcard

--- tests/nanosail-tests/PatternMatchingTests/AdornTests/Bool.ml
+++ tests/nanosail-tests/PatternMatchingTests/AdornTests/Bool.ml
@@ -117,8 +117,49 @@ let test_adorn_bool_false_true =
   |} >:: test
 
 
+let test_adorn_bool_wildcard =
+  let test _ =
+    let gen = new generator
+    in
+    let tc =
+      let statement =
+        Ast.Statement.ReadRegister (mkid "r1")
+      in
+      let* tree =
+        let* tree = build_empty_pattern_tree [ Ast.Type.Bool ]
+        in
+        let* tree = adorn
+          tree
+          [ Pattern.Binder gen#wildcard ]
+          statement
+        in
+        TC.return tree
+      in
+      let expected_tree =
+        TM.PatternTree.Bool begin
+          TM.PatternTree.SingleBoolCase (gen#wildcard, TM.PatternTree.Terminal (Some statement))
+        end
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string TM.PatternTree.to_fexpr)
+        ~cmp:TM.PatternTree.equal
+        (Normalize.normalize_pattern_tree expected_tree)
+        (Normalize.normalize_pattern_tree tree);
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+
+  in
+  {|
+    match b {
+      _ => r1
+    }
+  |} >:: test
+
+
 let test_suite =
   "bool" >::: [
     test_adorn_bool_true_false;
     test_adorn_bool_false_true;
+    test_adorn_bool_wildcard;
   ]
