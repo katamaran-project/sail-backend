a2637e6ab097e3fa1fb8f0066b628a2cca7aabd9
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-03 17:10:15 +0100

    updated tests

--- tests/nanosail-tests/NumericExpressionPrettyPrintingTests.ml
+++ tests/nanosail-tests/NumericExpressionPrettyPrintingTests.ml
@@ -27,45 +27,46 @@ let test_formatting =
       end
   in
   let test_cases =
-    let open Nanosail.Ast.Numeric.Expression in
+    let open Nanosail.Ast.Numeric.Expression
+    in
+    let add x y =
+      BinaryOperation (Add, x, y)
+    and sub x y =
+      BinaryOperation (Sub, x, y)
+    and mul x y =
+      BinaryOperation (Mul, x, y)
+    in
     [
       (Constant (z 1), "1");
       (Constant (z 5), "5");
       (Constant (z 12345), "12345");
-      (Add (c 1, c 2), "1 + 2");
-      (Add (c 12, c 23), "12 + 23");
-      (Sub (c 1, c 2), "1 - 2");
-      (Sub (c 13, c 10), "13 - 10");
-      (Mul (c 2, c 9), "2 * 9");
+      (add (c 1) (c 2), "1 + 2");
+      (add (c 12) (c 23), "12 + 23");
+      (sub (c 1) (c 2), "1 - 2");
+      (sub (c 13) (c 10), "13 - 10");
+      (mul (c 2) (c 9), "2 * 9");
       (Neg (c 10), "-10");
       (id "x", "x");
       (var "yx", "yx");
       (PowerOf2 (id "x"), "2^x");
-      (Add (Add (c 1, c 2), Add (c 3, c 4)), "1 + 2 + 3 + 4");
-      (Sub (c 1, Sub (c 2, c 3)), "1 - (2 - 3)");
-      (Sub (Sub (c 1, c 2), c 3), "1 - 2 - 3");
-      (Mul (Mul (c 1, c 2), Mul (c 3, c 4)), "1 * 2 * 3 * 4");
-      (Mul (Add (c 1, c 2), Add (c 3, c 4)), "(1 + 2) * (3 + 4)");
-      (Mul (Sub (c 1, c 2), Sub (c 3, c 4)), "(1 - 2) * (3 - 4)");
-      (Mul
-         (
-           Add
-             (
-               Mul
-                 (
-                   Add (c 1, c 2),
-                   Add (c 3, c 4)
-                 ),
-               Mul
-                 (c 5, c 6)
-             ),
-           Add (c 7, c 8)
-         ),
+      (add (add (c 1) (c 2)) (add (c 3) (c 4)), "1 + 2 + 3 + 4");
+      (sub (c 1) (sub (c 2) (c 3)), "1 - (2 - 3)");
+      (sub (sub (c 1) (c 2)) (c 3), "1 - 2 - 3");
+      (mul (mul (c 1) (c 2)) (mul (c 3) (c 4)), "1 * 2 * 3 * 4");
+      (mul (add (c 1) (c 2)) (add (c 3) (c 4)), "(1 + 2) * (3 + 4)");
+      (mul (sub (c 1) (c 2)) (sub (c 3) (c 4)), "(1 - 2) * (3 - 4)");
+      (mul
+         (add
+            (mul
+               (add (c 1) (c 2))
+               (add (c 3) (c 4)))
+            (mul (c 5) (c 6)))
+         (add (c 7) (c 8)),
        "((1 + 2) * (3 + 4) + 5 * 6) * (7 + 8)"
       );
-      (Neg (Add (c 1, c 2)), "-(1 + 2)");
-      (PowerOf2 (Add (c 1, c 2)), "2^(1 + 2)");
-      (PowerOf2 (Mul (c 1, c 2)), "2^(1 * 2)");
+      (Neg (add (c 1) (c 2)), "-(1 + 2)");
+      (PowerOf2 (add (c 1) (c 2)), "2^(1 + 2)");
+      (PowerOf2 (mul (c 1) (c 2)), "2^(1 * 2)");
       (PowerOf2 (PowerOf2 (c 10)), "2^(2^10)");
     ]
   in
