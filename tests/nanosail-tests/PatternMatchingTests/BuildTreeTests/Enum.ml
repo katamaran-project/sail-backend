a69998855cee5a1a3674831a2d42513c35b20ce7
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-02-04 16:13:34 +0100

    test_build_pattern_tree_enum_single_case

--- tests/nanosail-tests/PatternMatchingTests/BuildTreeTests/Enum.ml
+++ tests/nanosail-tests/PatternMatchingTests/BuildTreeTests/Enum.ml
@@ -11,6 +11,40 @@ module TM      = SailToNanosail.Translate.Match
 open Shared
 
 
+let test_build_pattern_tree_enum_single_case =
+  let test _ =
+    let gen = new generator
+    in
+    let tc =
+      let* enum_type =
+        define_enum_str "A" ["A1"]
+      in
+      let* actual_tree =
+        build_empty_pattern_tree [ enum_type ]
+      in
+      let expected_tree =
+        TM.PatternTree.Binder {
+          matched_type = enum_type;
+          binder       = gen#wildcard;
+          subtree      = TM.PatternTree.Terminal None
+        }
+      in
+      assert_equal
+        ~printer:(Fn.compose FExpr.to_string TM.PatternTree.to_fexpr)
+        ~cmp:TM.PatternTree.equal
+        (Normalize.normalize_pattern_tree expected_tree)
+        (Normalize.normalize_pattern_tree actual_tree);
+      TC.return ()
+    in
+    ignore @@ run_tc tc
+  in
+  {|
+    enum A = { A1, A2 }
+
+    tuple (A)
+  |} >:: test
+
+
 let test_build_pattern_tree_enum_1 =
   let test _ =
     let gen = new generator
@@ -219,4 +253,5 @@ let test_suite = "enum" >::: [
     test_build_pattern_tree_enum_2;
     test_build_pattern_tree_enum_3;
     test_build_pattern_tree_enum_4;
+    test_build_pattern_tree_enum_single_case;    
 ]
