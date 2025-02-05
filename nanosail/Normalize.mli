88d32c0c888131c3e82e8d842b08b8d7c08841c6
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-05 18:16:27 +0100

    Normalize.mli

---
+++ nanosail/Normalize.mli
@@ -0,0 +1,3 @@
+val normalize_statement    : Ast.Statement.t -> Ast.Statement.t
+val normalize_expression   : Ast.Expression.t -> Ast.Expression.t
+val normalize_pattern_tree : SailToNanosail.Translate.Match.PatternTree.t -> SailToNanosail.Translate.Match.PatternTree.t
