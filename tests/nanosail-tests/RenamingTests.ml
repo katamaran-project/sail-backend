adba8f28bc76cf134da1918319d15806beab5adc
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-11 22:24:14 +0100

    test_rename_match_product_4

--- tests/nanosail-tests/RenamingTests.ml
+++ tests/nanosail-tests/RenamingTests.ml
@@ -163,9 +163,62 @@ let test_rename_match_product_3 =
   |} >:: test
 
 
+let test_rename_match_product_4 =
+  let test _ =
+    let statement : Ast.Statement.t =
+      Match begin
+        MatchProduct {
+          matched  = mkid "x";
+          type_fst = Ast.Type.Int;
+          type_snd = Ast.Type.Int;
+          id_fst   = mkid "left";
+          id_snd   = mkid "right";
+          body     = Ast.Statement.Expression (Ast.Expression.Tuple [ evar "x"; evar "y"; evar "left"; evar "right" ])
+        }
+      end
+    in
+    let renamer =
+      Ast.Renaming.create_renamer
+        (mkid "right")
+        (mkid "renamed")
+    in
+    let actual =
+      Ast.Renaming.rename_in_statement renamer statement
+    and expected : Ast.Statement.t =
+      Match begin
+        MatchProduct {
+          matched  = mkid "x";
+          type_fst = Ast.Type.Int;
+          type_snd = Ast.Type.Int;
+          id_fst   = mkid "left";
+          id_snd   = mkid "right";
+          body     = Ast.Statement.Expression (Ast.Expression.Tuple [ evar "x"; evar "y"; evar "left"; evar "right" ])
+        }
+      end
+    in
+    assert_equal
+      ~cmp:Ast.Statement.equal
+      ~printer:(Fn.compose FExpr.to_string Ast.Statement.to_fexpr)
+      expected
+      actual
+  in
+  {|
+      match x {
+        (left, right) => (x, y, left, right)
+      }
+
+    Renaming right -> renamed gives
+
+      match renamed {
+        (left, right) => (x, y, left, right)
+      }
+  |} >:: test
+
+
 let test_suite =
   "renaming" >::: [
     test_rename_match_product_1;
     test_rename_match_product_2;
     test_rename_match_product_3;
+    test_rename_match_product_4;
   ]
