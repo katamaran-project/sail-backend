3bbc80289de6c71d59355f5473f4ee82cb52091f
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-17 22:43:42 +0100

    cleanup

--- nanosail/SailToNanosail/Translate/TypeQuantifier.ml
+++ nanosail/SailToNanosail/Translate/TypeQuantifier.ml
@@ -17,9 +17,9 @@ let translate_kind (kind : S.kind) : Ast.Kind.t TC.t =
   let S.K_aux (kind, _location) = kind
   in
   match kind with
-  | S.K_type -> TC.return @@ Ast.Kind.Type
-  | S.K_int  -> TC.return @@ Ast.Kind.Int
-  | S.K_bool -> TC.return @@ Ast.Kind.Bool
+  | K_type -> TC.return @@ Ast.Kind.Type
+  | K_int  -> TC.return @@ Ast.Kind.Int
+  | K_bool -> TC.return @@ Ast.Kind.Bool
 
 
 let translate_kind_id (kid : S.kid) : Ast.Identifier.t TC.t =
@@ -32,7 +32,7 @@ let translate_type_quantifier_item (quantifier_item : Libsail.Ast.quant_item) :(
   let S.QI_aux (unwrapped_quantifier_item, _location) = quantifier_item
   in
   match unwrapped_quantifier_item with
-  | S.QI_constraint _numeric_constraint -> begin
+  | QI_constraint _numeric_constraint -> begin
       let* () =
         let message = lazy begin
           PP.format "Ignoring constraint %s" (StringOf.Sail.quant_item quantifier_item)
@@ -42,7 +42,7 @@ let translate_type_quantifier_item (quantifier_item : Libsail.Ast.quant_item) :(
       in
       TC.return None
     end
-  | S.QI_id (KOpt_aux (KOpt_kind (kind, kind_id), _loc)) -> begin
+  | QI_id (KOpt_aux (KOpt_kind (kind, kind_id), _loc)) -> begin
       let* kind'    = translate_kind kind
       and* kind_id' = translate_kind_id kind_id
       in
@@ -54,9 +54,9 @@ let translate_type_quantifier (type_quantifier : Libsail.Ast.typquant) : Ast.Typ
   let S.TypQ_aux (unwrapped_type_quantifier, _location) = type_quantifier
   in
   match unwrapped_type_quantifier with
-  | S.TypQ_tq items  -> begin
+  | TypQ_tq items  -> begin
       let* items = TC.filter_map ~f:translate_type_quantifier_item items
       in
       TC.return @@ Ast.TypeQuantifier.TypeQuantifier items
     end
-  | S.TypQ_no_forall -> TC.return @@ Ast.TypeQuantifier.TypeQuantifier []
+  | TypQ_no_forall -> TC.return @@ Ast.TypeQuantifier.TypeQuantifier []
