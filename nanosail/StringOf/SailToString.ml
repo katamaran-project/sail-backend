3ec0fba3d8bc7f84cceef6893ad9e87a2e8b4c2d
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-02-18 15:30:44 +0100

    simplified StringOf.Sail.location

--- nanosail/StringOf/SailToString.ml
+++ nanosail/StringOf/SailToString.ml
@@ -110,6 +110,21 @@ let pat (pattern : Sail.tannot Libsail.Ast.pat) : string =
 
 
 let location (location : Libsail.Parse_ast.l) : string =
+  let string_of_ocaml_position ({ pos_fname; pos_lnum; pos_bol = _; pos_cnum = _ } : Lexing.position) =
+    Printf.sprintf "%s:%d" pos_fname pos_lnum
+  in
+  let rec string_of_location (location : Libsail.Parse_ast.l) =
+    match location with
+    | Unknown                 -> "UnknownLocation"
+    | Unique (k, loc)         -> Printf.sprintf "UniqueLocation(%d, %s)" k (string_of_location loc)
+    | Generated loc           -> Printf.sprintf "GeneratedLocation(%s)" (string_of_location loc)
+    | Hint (hint, loc1, loc2) -> Printf.sprintf "HintLocation(%s, %s, %s)" hint (string_of_location loc1) (string_of_location loc2)
+    | Range (pos1, _pos2)     -> string_of_ocaml_position pos1
+  in
+  string_of_location location
+
+
+let location_full (location : Libsail.Parse_ast.l) : string =
   let string_of_ocaml_position ({ pos_fname; pos_lnum; pos_bol; pos_cnum } : Lexing.position) =
     Printf.sprintf "Pos(%s:%d:%d:%d)" pos_fname pos_lnum pos_bol pos_cnum
   in
