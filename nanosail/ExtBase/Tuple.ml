ca25e6b3adbf568e95871efa2befe86e5afdd46d
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-02-19 09:00:06 +0100

    Tuple.Triple.equal

--- nanosail/ExtBase/Tuple.ml
+++ nanosail/ExtBase/Tuple.ml
@@ -22,4 +22,18 @@ module Triple = struct
   let first  (x, _, _) = x
   let second (_, x, _) = x
   let third  (_, _, x) = x
+
+  let equal
+      (fst_equal : 'a -> 'a -> bool)
+      (snd_equal : 'b -> 'b -> bool)
+      (trd_equal : 'c -> 'c -> bool)
+      (triple_1  : 'a * 'b * 'c    )
+      (triple_2  : 'a * 'b * 'c    ) : bool
+    =
+    let x1, x2, x3 = triple_1
+    and y1, y2, y3 = triple_2
+    in
+    fst_equal x1 y1 &&
+    snd_equal x2 y2 &&
+    trd_equal x3 y3      
 end
