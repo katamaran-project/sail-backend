6e05cd1b1bd4d75837fffe8c11a1c54c0268f4d3
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-21 19:34:04 +0100

    coq-tests shared-configuration

--- tests/coq-tests/shared-configuration.lisp
+++ tests/coq-tests/shared-configuration.lisp
@@ -8,34 +8,36 @@
                 "sail_internal")
 
 (define (should-be-ignored? identifier)
-  (or
-   (string-ends-with? "_of_num" identifier)
-   (string-starts-with? "num_of_" identifier)
-   (string-starts-with? "undefined_" identifier)
-   (string-starts-with? "regval_of_" identifier)
-   (string-ends-with? "_of_regval" identifier)
-   (contains? '(
-                "eq_unit"
-                "neq_int"
-                "neq_bool"
-                "neq_bits"
-                "slice_mask"
-                "_shl_int_general"
-                "_shr_int_general"
-                "fdiv_int"
-                "fmod_int"
-                "is_none"
-                "is_some"
-                "__id"
-                "sail_mask"
-                "sail_ones"
-                "concat_str_bits"
-                "concat_str_dec"
-                "__deref")
-              identifier)))
+  )
 
-(ignore-function-definition-predicate should-be-ignored?)
-(ignore-top-level-type-constraint-predicate should-be-ignored?)
+(ignore-function-definition-and-top-level-type-constraints-predicate
+ (lambda (identifier)
+   (or
+    (string-ends-with? "_of_num" identifier)
+    (string-starts-with? "num_of_" identifier)
+    (string-starts-with? "undefined_" identifier)
+    (string-starts-with? "regval_of_" identifier)
+    (string-ends-with? "_of_regval" identifier)
+    (contains? '(
+                 "eq_unit"
+                 "neq_int"
+                 "neq_bool"
+                 "neq_bits"
+                 "slice_mask"
+                 "_shl_int_general"
+                 "_shr_int_general"
+                 "fdiv_int"
+                 "fmod_int"
+                 "is_none"
+                 "is_some"
+                 "__id"
+                 "sail_mask"
+                 "sail_ones"
+                 "concat_str_bits"
+                 "concat_str_dec"
+                 "__deref")
+               identifier))))
+                                        
 
 (ignore-type-definition-predicate (lambda (identifier)
                                     (contains? '(
