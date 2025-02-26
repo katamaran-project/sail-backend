f760cbeff3dfaa5abb6202ed4026112169efba74
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-02-26 15:55:41 +0100

    removed redundant module

--- nanosail/SailToNanosail/SailToNanosail.ml
+++ nanosail/SailToNanosail/SailToNanosail.ml
@@ -3,6 +3,5 @@ module TranslationContext     = TranslationContext
 module Prelude                = Prelude
 module StringOf               = StringOf
 module Translate              = Translate
-module Monomorphization       = Monomorphization
 
 let translate                 = Translation.translate
