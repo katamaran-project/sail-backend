b6e572587a35e6fe8f002da29860924783275af9
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-28 00:45:03 +0100

    fixed log level

--- nanosail/SailToNanosail/Translate/TopLevelTypeConstraint.ml
+++ nanosail/SailToNanosail/Translate/TopLevelTypeConstraint.ml
@@ -120,7 +120,7 @@ let translate_top_level_type_constraint
         ]
       end
       in
-      TC.log [%here] Logging.warning message
+      TC.log [%here] Logging.info message
     in
     TC.return begin
       Ast.Definition.TopLevelTypeConstraintDefinition {
