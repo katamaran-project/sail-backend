dfc80dac7aea08b080e6685da073170cc98c7c1d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-02-27 21:11:34 +0100

    updated coq-tests/monomorphize

--- tests/coq-tests/monomorphize/configuration.lisp
+++ tests/coq-tests/monomorphize/configuration.lisp
@@ -1,5 +1,9 @@
 $include ../shared-configuration.lisp
 
+(monomorphize "foo" "foo_8" '(("'n" 8)))
+(monomorphize "foo" "foo_16" '(("'n" 16)))
+(monomorphize "foo" "foo_32" '(("'n" 32)))
+
 (template "base.template.v")
 (template "machine.template.v")
 (template "base.template.html")
