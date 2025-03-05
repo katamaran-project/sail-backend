2224dd3cada7c5e271f24be20b9e9d5e4b39a61d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-05 20:43:22 +0100

    layout

--- nanosail/FExpr.mli
+++ nanosail/FExpr.mli
@@ -1,17 +1,17 @@
 type t
     
-val equal : t -> t -> bool
-val pp : t -> PP.t
-val pp_diff : (PP.t -> PP.t) -> compared_with : t -> printed : t -> PP.t
-val to_string : t -> string
+val equal             : t -> t -> bool
+val pp                : t -> PP.t
+val pp_diff           : (PP.t -> PP.t) -> compared_with : t -> printed : t -> PP.t
+val to_string         : t -> string
 
-val mk_int : int -> t
-val mk_bool : bool -> t
-val mk_string : string -> t
-val mk_application : ?positional:t list -> ?keyword:(string * t) list -> string -> t
-val mk_symbol : string -> t
-val mk_list : t list -> t
-val mk_nil : t
-val mk_option : t option -> t
+val mk_int            : int -> t
+val mk_bool           : bool -> t
+val mk_string         : string -> t
+val mk_application    : ?positional:t list -> ?keyword:(string * t) list -> string -> t
+val mk_symbol         : string -> t
+val mk_list           : t list -> t
+val mk_nil            : t
+val mk_option         : t option -> t
 val mk_ocaml_location : Lexing.position -> t
-val mk_sail_location : Libsail.Parse_ast.l -> t
+val mk_sail_location  : Libsail.Parse_ast.l -> t
