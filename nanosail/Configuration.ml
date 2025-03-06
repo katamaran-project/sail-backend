7bdd6432c88e0e585caef84eb794d8ae999638e4
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-06 17:04:29 +0100

    improved example usage of VERBOSE

--- nanosail/Configuration.ml
+++ nanosail/Configuration.ml
@@ -51,8 +51,8 @@ module Implementation = struct
 
        Example Usage
        -------------
-       VERBOSE=0 sail ...        # Suppress all logging
-       VERBOSE=4 sail ...        # Enable all logging (check LogLib to make sure this is still valid)
+       $ VERBOSE=0 sail ...        # Suppress all logging
+       $ VERBOSE=4 sail ...        # Enable all logging (check LogLib to make sure this is still valid)
     *)
     let verbosity_level = environment_variable "VERBOSE" LogLib.VerbosityLevel.default (Fn.compose LogLib.VerbosityLevel.of_int Int.of_string)
 
