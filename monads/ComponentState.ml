f7ebffcc645baf0715b9e592d081145d8641bb40
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-06 21:19:43 +0100

    layout

--- monads/ComponentState.ml
+++ monads/ComponentState.ml
@@ -1,15 +1,21 @@
+(*
+   State monad where the state consists of multiple components
+   which can be retrieved and updated separately.
+*)
+
 module type S = sig
   include Sig.Monad
 
   type state
 
   type 'a accessor = (state -> 'a) * (state -> 'a -> state)
-
+  
   val get : 'a accessor -> 'a t
   val put : 'a accessor -> 'a -> unit t
   val act : (unit -> 'a) -> 'a t
   val run : 'a t -> state -> ('a * state)
 end
+
 
 module Make (S : sig type t end) : (S with type state = S.t) = struct
   module M = State.Make(S)
@@ -22,14 +28,17 @@ module Make (S : sig type t end) : (S with type state = S.t) = struct
   let return = M.return
   let bind   = M.bind
 
-  let get (f, _) =
+  let get ((f, _) : 'a accessor) : 'a t =
     let open Notations.Star(M)
     in
     let* s = M.get
     in
     return @@ f s
 
-  let put (_, f) x =
+  let put
+      ((_, f) : 'a accessor)
+      (x      : 'a         ) : unit t
+    =
     let open Notations.Star(M)
     in
     let* s = M.get
