edbca2f44b72d8c212a56dfead1619cc8cf6b878
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-06 13:31:03 +0100

    coq-tests/print-pretty-binary-operation

---
+++ tests/coq-tests/print-pretty-binary-operation/configuration.lisp
@@ -0,0 +1,8 @@
+$include ../shared-configuration.lisp
+
+(pretty-print-binary-operators)
+
+(template "base.template.v")
+(template "machine.template.v")
+(template "base.template.html")
+(template "machine.template.html")
