d6b61aaebae817cb69cfbac8d4b526be81fb64c0
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-06 12:28:32 +0100

    coq-tests/match-enum-pretty-notation

---
+++ tests/coq-tests/match-enum-pretty-notation/configuration.lisp
@@ -0,0 +1,8 @@
+$include ../shared-configuration.lisp
+
+(pretty-print-match-enum)
+
+(template "base.template.v")
+(template "machine.template.v")
+(template "base.template.html")
+(template "machine.template.html")  
