54348cb72afc690e1301d9b1e4ef77abac5296f6
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-06 17:22:58 +0100

    coq-tests/program-name

---
+++ tests/coq-tests/program-name/configuration.lisp
@@ -0,0 +1,8 @@
+$include ../shared-configuration.lisp
+
+(program-name "MyLittleProgram")
+
+(template "base.template.v")
+(template "machine.template.v")
+(template "base.template.html")
+(template "machine.template.html")  
