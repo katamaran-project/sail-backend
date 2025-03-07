e508cad08673e3e6e1c78016e7d3ef51e3695cc2
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-07 19:08:05 +0100

    removed redundant code

--- nanosail/SailToNanosail/SailToNanosail.ml
+++ nanosail/SailToNanosail/SailToNanosail.ml
@@ -8,7 +8,6 @@
 module Translation            = Translation
 module TranslationContext     = TranslationContext
 module Prelude                = Prelude
-module StringOf               = StringOf
 module Translate              = Translate
 
 let translate                 = Translation.translate
