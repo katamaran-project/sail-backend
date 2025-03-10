5a6b71c3d22ea098107cc6bc82be42aba6f31f34
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-10 11:14:09 +0100

    counter_test

--- tests/nanosail-tests/SlangTests/HeapTests.ml
+++ tests/nanosail-tests/SlangTests/HeapTests.ml
@@ -70,8 +70,28 @@ let heap_update_tests =
   "updating" >::: List.map ~f:(Fn.uncurry test_run) test_cases
 
 
+let counter_test =
+  let open Slang.Value
+  in
+  let program =
+    {|
+      (define (make-counter)
+        (define counter (ref 0))
+        (lambda ()
+          (@= (+ 1 (@ counter)))
+          (@ counter)))
+
+      (define counter (make-counter))
+
+      (list (counter) (counter) (counter))
+    |}
+  in
+  test_run program (Slang.Value.list_to_cons [ Integer 1; Integer 2; Integer 3 ])
+
+
 let tests =
   "heap tests" >::: [
     heap_access_tests;
     heap_update_tests;
+    counter_test;
   ]
