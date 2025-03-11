e863c45f3c9eb7b260bc9b78e4d394e62a205ce4
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-11 11:33:48 +0100

    docs

--- nanosail/StringOf/OCaml.ml
+++ nanosail/StringOf/OCaml.ml
@@ -5,6 +5,12 @@ let position ({ pos_fname; pos_lnum; pos_bol; pos_cnum } : Lexing.position) =
   Printf.sprintf "Pos(%s:%d:%d:%d)" pos_fname pos_lnum pos_bol pos_cnum
 
 
+(*
+   Converts all elements to strings using string_of_element,
+   separates them by a semicolon and encloses the result in brackets:
+
+      [ string_of_element elt1; string_of_element elt2; ...; string_of_element eltn ]
+*)
 let list
     (string_of_element : 'a -> string)
     (elements          : 'a list     ) : string
