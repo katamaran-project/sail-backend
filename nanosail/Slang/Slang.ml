c0023c5bd12c68a2eba7c88c177101fdfb6bc5ab
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-11 15:18:38 +0100

    typing

--- nanosail/Slang/Slang.ml
+++ nanosail/Slang/Slang.ml
@@ -26,7 +26,11 @@ module Error               = Error
 let run_asts state asts =
   EvaluationContext.run_with_state (Evaluation.evaluate_sequentially asts) state
 
-let run_string state string =
+
+let run_string
+    (state  : State.t)
+    (string : string ) : Value.t list EvaluationContext.result * State.t
+  =
   let asts = Parser.parse_string string
   in
   run_asts state asts
