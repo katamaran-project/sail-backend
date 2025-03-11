80acf40ccae762e3d367a629bd1c64673a422621
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-11 11:37:54 +0100

    Address updates

--- nanosail/Slang/Address.ml
+++ nanosail/Slang/Address.ml
@@ -1,16 +1,21 @@
-open! ExtBase
+(*
+   An address is essentially the same as a ref-cell in OCaml:
+   it is an opaque value that represents something in memory
+   that can be read from and written to.
+*)
+open ExtBase
 
 
 type t = Address of int
 
 
-let equal (Address p) (Address q) =
+let equal (Address p) (Address q) : bool =
   Int.equal p q
 
 
-let to_int (Address p) =
+let to_int (Address p) : int =
   p
 
 
-let mk n =
+let mk (n : int) : t =
   Address n
