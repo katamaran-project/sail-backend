c509ad2ba1ebcb19dabefa5ffd59bfca0b1f5ad1
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-11 14:34:39 +0100

    layout

--- nanosail/Slang/EnvironmentBuilder.ml
+++ nanosail/Slang/EnvironmentBuilder.ml
@@ -3,13 +3,21 @@ type extend_environment_context = {
   callable  : string -> Value.callable -> unit;
 }
 
-let extend_environment env (f : extend_environment_context -> unit) =
-  let current               = ref env
+
+let extend_environment
+    (environment : State.environment                 )
+    (f           : extend_environment_context -> unit) : State.environment
+  =
+  let current =
+    ref environment
   in
-  let update env            = current := env
+  let update environment =
+    current := environment
   in
-  let callable identifier f = update @@ Environment.bind !current identifier @@ Value.Callable f
-  and extend   f            = update @@ f !current
+  let callable identifier f =
+    update @@ Environment.bind !current identifier @@ Value.Callable f
+  and extend f =
+    update @@ f !current
   in
   let context = { extend; callable; }
   in
