6c6fdd66e12f5d698d55bc89ee3b3d3ede54764d
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-15 19:21:27 +0100

    coq-tests/statements-vs-expressions

---
+++ tests/coq-tests/statements-vs-expressions/configuration.lisp
@@ -0,0 +1,9 @@
+$include ../shared-configuration.lisp
+
+(pretty-print-let)
+(pretty-print-function-calls)
+
+(template "base.template.v")
+(template "machine.template.v")
+(template "base.template.html")
+(template "machine.template.html")  
