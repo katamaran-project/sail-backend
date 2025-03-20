7570a7222306be435955a13f534bb1ae3338bb77
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-20 20:48:57 +0100

    updated itself-002 configuration

--- tests/coq-tests/itself-002/configuration.lisp
+++ tests/coq-tests/itself-002/configuration.lisp
@@ -1,5 +1,11 @@
 $include ../shared-configuration.lisp
 
+(monomorphize "index"
+              "index_5_0"
+              '(
+                ("'n" 5)
+                ("'i" 1)))
+
 (template "base.template.v")
 (template "machine.template.v")
 (template "base.template.html")
