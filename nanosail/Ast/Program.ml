6beebf0306586caf8329fab37746047f1e9a969b
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-21 13:21:05 +0100

    docs

--- nanosail/Ast/Program.ml
+++ nanosail/Ast/Program.ml
@@ -1,6 +1,28 @@
 type t = {
-  definitions          : (Sail.sail_definition * Definition.t) list;  (* list of all translated definitions; original order preserved *)
-  polymorphic_argtypes : Type.t list list Identifier.Map.t;           (* maps polymorphic functions to the argument types *)
+  (*
+     List of all translated definitions.
+     The definitions appear in the same order as their corresponding Sail-definitions were provided by Sail.
+  *)
+  definitions : (Sail.sail_definition * Definition.t) list;
+
+  (*
+     This map keeps track of the types of the arguments passed in calls to polymorphic functions.
+
+     For example, take the polymorphic function
+
+       val xor : forall 'n. bits('n) -> bits('n) -> bits('n)
+
+     and it is called with the following arguments:
+
+       xor(0b0, 0b0)
+       xor(0b00, 0b00)
+
+     then this map would contain
+
+       xor -> [ [ Bitvector(1); Bitvector(1) ];
+                [ Bitvector(2); Bitvector(2) ] ]
+  *)
+  polymorphic_argtypes : Type.t list list Identifier.Map.t;
 }
 
 
