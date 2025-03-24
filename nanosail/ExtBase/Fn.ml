fabd632e36a39263c713287fa7359379f7f2cc5e
Frederic Vogels <frederic.vogels+github@gmail.com>
2025-03-24 22:49:00 +0100

    ExtBase.Fn.fixed_point

--- nanosail/ExtBase/Fn.ml
+++ nanosail/ExtBase/Fn.ml
@@ -3,3 +3,21 @@ include Base.Fn
 
 let uncurry  f (x, y)    = f x y
 let uncurry3 f (x, y, z) = f x y z
+
+
+let fixed_point
+    ~(f     :'a -> 'a        )
+    ~(equal :'a -> 'a -> bool)
+     (value : 'a             ): 'a
+  =
+  let rec aux (value : 'a) : 'a =
+    let next_value = f value
+    in
+    if
+      equal value next_value
+    then
+      value
+    else
+      aux next_value
+  in
+  aux value
