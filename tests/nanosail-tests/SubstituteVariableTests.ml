6063b4af5783644a9e8f69eddd0c1202adde61b5
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-03-25 13:34:50 +0100

    test_binary_operation_3

--- tests/nanosail-tests/SubstituteVariableTests.ml
+++ tests/nanosail-tests/SubstituteVariableTests.ml
@@ -128,10 +128,42 @@ let test_binary_operation_2 =
   |} >:: test
 
 
+let test_binary_operation_3 =
+  let test _ =
+    let statement : Ast.Statement.t =
+      Expression (BinaryOperation (Ast.BinaryOperator.Plus, evar (mkid "x"), evar (mkid "y")))
+    in
+    let substitution (id : Ast.Identifier.t) : Ast.Expression.t option =
+      match Ast.Identifier.to_string id with
+      | "x" -> Some (Value (Ast.Value.mk_int 1))
+      | "y" -> Some (Value (Ast.Value.mk_int 2))
+      | _   -> None
+    in
+    let actual : Ast.Statement.t =
+      Ast.Statement.substitute_variable substitution statement
+    and expected : Ast.Statement.t =
+      Expression (BinaryOperation (Ast.BinaryOperator.Plus, Ast.Expression.Value (Ast.Value.mk_int 1), Ast.Expression.Value (Ast.Value.mk_int 2)))
+    in
+    assert_equal
+      ~cmp:Ast.Statement.equal
+      ~printer:(Fn.compose FExpr.to_string Ast.Statement.to_fexpr)
+      expected
+      actual
+  in
+  {|
+      (x + y) [1/x][2/y]
+
+    should become
+
+      (1 + 2)
+  |} >:: test
+
+
 let test_suite =
   "substitute variables" >::: [
     test_variable;
     test_variable_2;
     test_binary_operation;
     test_binary_operation_2;
+    test_binary_operation_3;
   ]
