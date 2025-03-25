b42614f330642316a33a9644434ff148cbdd49f2
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-03-25 13:30:23 +0100

    test_binary_operation

--- tests/nanosail-tests/SubstituteVariableTests.ml
+++ tests/nanosail-tests/SubstituteVariableTests.ml
@@ -3,13 +3,15 @@ open OUnit2
 open Nanosail
 
 
-let mkid  = Ast.Identifier.mk
+let mkid    = Ast.Identifier.mk
+let evar id = Ast.Expression.Variable (id, Int None)
+let svar id = Ast.Statement.Expression (evar id)                
 
 
 let test_variable =
   let test _ =
     let statement : Ast.Statement.t =
-      Expression (Variable (mkid "x", Int None))
+      svar (mkid "x")
     in
     let substitution (id : Ast.Identifier.t) : Ast.Expression.t option =
       match Ast.Identifier.to_string id with
@@ -39,7 +41,7 @@ let test_variable =
 let test_variable_2 =
   let test _ =
     let statement : Ast.Statement.t =
-      Expression (Variable (mkid "y", Int None))
+      svar (mkid "y")
     in
     let substitution (id : Ast.Identifier.t) : Ast.Expression.t option =
       match Ast.Identifier.to_string id with
@@ -49,7 +51,7 @@ let test_variable_2 =
     let actual : Ast.Statement.t =
       Ast.Statement.substitute_variable substitution statement
     and expected : Ast.Statement.t =
-      Expression (Variable (mkid "y", Int None))
+      svar (mkid "y")
     in
     assert_equal
       ~cmp:Ast.Statement.equal
@@ -66,8 +68,39 @@ let test_variable_2 =
   |} >:: test
 
 
+let test_binary_operation =
+  let test _ =
+    let statement : Ast.Statement.t =
+      Expression (BinaryOperation (Ast.BinaryOperator.Plus, evar (mkid "x"), evar (mkid "y")))
+    in
+    let substitution (id : Ast.Identifier.t) : Ast.Expression.t option =
+      match Ast.Identifier.to_string id with
+      | "x" -> Some (Value (Ast.Value.mk_int 1))
+      | _   -> None
+    in
+    let actual : Ast.Statement.t =
+      Ast.Statement.substitute_variable substitution statement
+    and expected : Ast.Statement.t =
+      Expression (BinaryOperation (Ast.BinaryOperator.Plus, Ast.Expression.Value (Ast.Value.mk_int 1), evar (mkid "y")))
+    in
+    assert_equal
+      ~cmp:Ast.Statement.equal
+      ~printer:(Fn.compose FExpr.to_string Ast.Statement.to_fexpr)
+      expected
+      actual
+  in
+  {|
+      (x + y) [1/x]
+
+    should become
+
+      (1 + x)
+  |} >:: test
+
+
 let test_suite =
   "substitute variables" >::: [
     test_variable;
     test_variable_2;
+    test_binary_operation;
   ]
