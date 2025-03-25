1b295eccf074fa831c44356a38c00e1791161a54
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-03-25 11:54:35 +0100

    test_simplify_aliases

--- tests/nanosail-tests/SimplificationTests.ml
+++ tests/nanosail-tests/SimplificationTests.ml
@@ -101,6 +101,37 @@ let test_simplify_seq_unit =
   |} >:: test
 
 
+let test_simplify_aliases =
+  let test _ =
+    let statement : Ast.Statement.t =
+      Let {
+        binder                 = Ast.Identifier.mk "x";
+        binding_statement_type = Unit;
+        binding_statement      = Expression (Variable (Ast.Identifier.mk "y", String));
+        body_statement         = Expression (Variable (Ast.Identifier.mk "x", String))
+      }
+    in
+    let actual : Ast.Statement.t =
+      Ast.Statement.simplify_aliases statement
+    and expected : Ast.Statement.t =
+      Expression (Variable (Ast.Identifier.mk "y", String));
+    in
+    assert_equal
+      ~cmp:Ast.Statement.equal
+      ~printer:(Fn.compose FExpr.to_string Ast.Statement.to_fexpr)
+      expected
+      actual
+  in
+  {|
+      let x = y
+      in
+      x
+
+    should become
+
+      y
+  |} >:: test
+
 let test_simplify_statement_1 =
   let test _ =
     let statement : Ast.Statement.t =
@@ -171,6 +202,7 @@ let test_suite =
     test_simplify_unused_let_binder;
     test_simplify_unused_let_binder_2;
     test_simplify_seq_unit;
+    test_simplify_aliases;
     test_simplify_statement_1;
     test_simplify_statement_2;
   ]
