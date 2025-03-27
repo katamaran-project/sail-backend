0a5c5dabf7081b4a01ca39fb849fb4e78b1650b7
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-03-27 13:21:09 +0100

    test_simplify_aliases_product_5

--- tests/nanosail-tests/SimplificationTests.ml
+++ tests/nanosail-tests/SimplificationTests.ml
@@ -1151,6 +1151,60 @@ let test_simplify_aliases_product_4 =
   |} >:: test
 
 
+let test_simplify_aliases_product_5 =
+  let test _ =
+    let statement : Ast.Statement.t =
+      Let {
+        binder                 = mkid "x";
+        binding_statement_type = Unit;
+        binding_statement      = svar "y";
+        body_statement         = Match begin
+            MatchProduct {
+              matched          = mkid "product";
+              type_fst         = Int None;
+              type_snd         = Int None;
+              id_fst           = mkid "x";
+              id_snd           = mkid "snd";
+              body             = svar "x";
+            }
+          end
+      }
+    in
+    let actual : Ast.Statement.t =
+      Ast.Statement.simplify_aliases statement
+    and expected : Ast.Statement.t =
+      Match begin
+        MatchProduct {
+          matched          = mkid "product";
+          type_fst         = Int None;
+          type_snd         = Int None;
+          id_fst           = mkid "x";
+          id_snd           = mkid "snd";
+          body             = svar "x";
+        }
+      end
+    in
+    assert_equal
+      ~cmp:Ast.Statement.equal
+      ~pp_diff:(pp_diff Ast.Statement.to_fexpr)
+      expected
+      actual
+  in
+  {|
+      let x = y
+      in
+      match product {
+        (x, snd) => x
+      }
+    
+    should become
+
+      match product {
+        (x, snd) => x
+      }
+  |} >:: test
+
+
 let test_suite =
   "simplification" >::: [
     test_simplify_unused_let_binder;
@@ -1176,6 +1230,7 @@ let test_suite =
     test_simplify_aliases_product_2;
     test_simplify_aliases_product_3;
     test_simplify_aliases_product_4;
+    test_simplify_aliases_product_5;
     test_simplify_statement;
     test_simplify_statement_2;
   ]
