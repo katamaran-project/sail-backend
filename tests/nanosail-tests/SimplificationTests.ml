bc226a7d8d56e863548e7ff745e9548c2d180d65
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-03-27 13:29:32 +0100

    test_simplify_match_enum

--- tests/nanosail-tests/SimplificationTests.ml
+++ tests/nanosail-tests/SimplificationTests.ml
@@ -1313,6 +1313,62 @@ let test_simplify_aliases_product_7 =
   |} >:: test
 
 
+let test_simplify_match_enum =
+  let test _ =
+    let statement : Ast.Statement.t =
+      Let {
+        binder                 = mkid "x";
+        binding_statement_type = Unit;
+        binding_statement      = svar "y";
+        body_statement         = Match begin
+            MatchEnum {
+              matched          = mkid "value";
+              matched_type     = mkid "MyEnum";
+              cases            = Ast.Identifier.Map.of_alist_exn [
+                  (mkid "Foo", svar "a");
+                  (mkid "Bar", svar "b");
+                ]
+            }
+          end
+      }
+    in
+    let actual : Ast.Statement.t =
+      Ast.Statement.simplify_aliases statement
+    and expected : Ast.Statement.t =
+      Match begin
+        MatchEnum {
+          matched          = mkid "value";
+          matched_type     = mkid "MyEnum";
+          cases            = Ast.Identifier.Map.of_alist_exn [
+              (mkid "Foo", svar "a");
+              (mkid "Bar", svar "b");
+            ]
+        }
+      end
+    in
+    assert_equal
+      ~cmp:Ast.Statement.equal
+      ~pp_diff:(pp_diff Ast.Statement.to_fexpr)
+      expected
+      actual
+  in
+  {|
+      let x = y
+      in
+      match value {
+        Foo => a,
+        Bar => b,
+      }
+    
+    should become
+
+      match value {
+        Foo => a,
+        Bar => b,
+      }
+  |} >:: test
+
+
 let test_suite =
   "simplification" >::: [
     test_simplify_unused_let_binder;
@@ -1341,6 +1397,7 @@ let test_suite =
     test_simplify_aliases_product_5;
     test_simplify_aliases_product_6;
     test_simplify_aliases_product_7;
+    test_simplify_match_enum;
     test_simplify_statement;
     test_simplify_statement_2;
   ]
