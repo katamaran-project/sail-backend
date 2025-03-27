79ebbf10674c37b30553fb5e7028ce20d6121546
Frederic Vogels <frederic.vogels@kuleuven.be>
2025-03-27 12:22:27 +0100

    test_simplify_aliases_destructure_record_2

--- tests/nanosail-tests/SimplificationTests.ml
+++ tests/nanosail-tests/SimplificationTests.ml
@@ -677,6 +677,55 @@ let test_simplify_aliases_destructure_record =
       result
   |} >:: test
 
+
+let test_simplify_aliases_destructure_record_2 =
+  let test _ =
+    let statement : Ast.Statement.t =
+      Let {
+        binder                 = mkid "x";
+        binding_statement_type = Unit;
+        binding_statement      = svar "y";
+        body_statement         = DestructureRecord {
+            record_type_identifier = (mkid "foo");
+            field_identifiers      = [ mkid "field1"; mkid "field2" ];
+            binders                = [ mkid "a"; mkid "b" ];
+            destructured_record    = svar "x";
+            body                   = svar "result";
+          }
+      }
+    in
+    let actual : Ast.Statement.t =
+      Ast.Statement.simplify_aliases statement
+    and expected : Ast.Statement.t =
+      DestructureRecord {
+        record_type_identifier = (mkid "foo");
+        field_identifiers      = [ mkid "field1"; mkid "field2" ];
+        binders                = [ mkid "a"; mkid "b" ];
+        destructured_record    = svar "y";
+        body                   = svar "result";
+      }
+    in
+    assert_equal
+      ~cmp:Ast.Statement.equal
+      ~pp_diff:(pp_diff Ast.Statement.to_fexpr)
+      expected
+      actual
+  in
+  {|
+      let x = y
+      in
+      let { field1 = a; field2 = b } = x
+      in
+      result
+
+    should become
+
+      let { field1 = a; field2 = b } = y
+      in
+      result
+  |} >:: test
+
+
 let test_simplify_statement =
   let test _ =
     let statement : Ast.Statement.t =
@@ -759,6 +808,7 @@ let test_suite =
     test_simplify_aliases_match_list_7;
     test_simplify_aliases_match_list_8;
     test_simplify_aliases_destructure_record;
+    test_simplify_aliases_destructure_record_2;
     test_simplify_statement;
     test_simplify_statement_2;
   ]
